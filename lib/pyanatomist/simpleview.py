93ef4066778251c3a4e758a4d24f3683a6f25af8
yann.cointepas <>
2006-02-28 19:11:22 -0700

    specified minimum pyanatomist usage

---
+++ lib/pyanatomist/simpleview.py
@@ -0,0 +1,20 @@
+class SimpleWindow:
+  def addImage( self, imageData, affineTransformation=None ):
+    pass
+  
+  def addMesh( self, mesh ):
+    pass
+  
+  def setMaterial( self, tag, material ):
+    pass
+  
+  def addImageFusion( self, *imageTags ):
+    pass
+
+# Mesh creation
+
+# ISO surface
+def createIsoSurface( image, threshold ):
+  pass
+
+  
