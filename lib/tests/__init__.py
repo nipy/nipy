dfb6213aec27df1c6d247e1802b79934fbbb1779
brian.hawthorne <>
2006-04-13 19:09:52 -0700

    autodetect TestCases defined under test packages

--- lib/tests/__init__.py
+++ lib/tests/__init__.py
@@ -1,4 +1,7 @@
-import unittest, shutil, tempfile, os
+import shutil, tempfile, os, types
+from unittest import TestSuite, makeSuite, TestCase, TextTestRunner
+from glob import glob
+from os.path import join, dirname, basename
 
 packages = (
     'neuroimaging',
@@ -13,12 +16,16 @@ packages = (
     'neuroimaging.visualization',
     'neuroimaging.visualization.cmap')
 
-
+#-----------------------------------------------------------------------------
 def import_from(modulename, objectname):
     "Import and return objectname from modulename."
-    module = __import__(modulename, globals(), locals(), (objectname,))
-    return getattr(module, objectname)
+    try:
+        module = __import__(modulename, globals(), locals(), (objectname,))
+        return getattr(module, objectname)
+    except (ImportError, AttributeError):
+        return None
 
+#-----------------------------------------------------------------------------
 def get_package_suite(packname):
     """
     Retrieve the test suite for the named package.  Expects to find under the
@@ -31,25 +38,48 @@ def get_package_suite(packname):
     except ImportError:
         raise ValueError("Could not find expected test package '%s'."%packname)
 
+#-----------------------------------------------------------------------------
 def run_suite(suite):
     tmpdir = tempfile.mkdtemp(suffix='nipy_unittest')
     curdir = os.path.abspath(os.curdir)
     os.chdir(tmpdir)
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    TextTestRunner(verbosity=2).run(suite)
     os.chdir(curdir)
     shutil.rmtree(tmpdir, ignore_errors=False)
+
+#-----------------------------------------------------------------------------
+def get_package_modules(package):
+    modfiles = glob(join(dirname(package.__file__), "*.py"))
+    modules = []
+    for modfile in modfiles:
+        modname = basename(modfile).split(".")[0]
+        full_modname = "%s.%s"%(package.__name__,modname)
+        modules.append(__import__(full_modname,{},{},[modname]))
+    return modules
+
+#-----------------------------------------------------------------------------
+def get_package_tests(packname):
+    """
+    Retrieve all TestCases defined for the given package.
+    @returns {testname:testclass,...}
+    """
+    package = import_from(packname, "tests")
+    if not package: return {}
+    modules = get_package_modules(package)
+    tests = []
+    for module in modules:
+        tests += [(key,val) for key,val in module.__dict__.items() \
+                  if type(val)==types.TypeType and issubclass(val, TestCase)]
+    return dict(tests)
     
-def test_package(packname):
-    try:
-        suite = get_package_suite(packname)
-    except Exception, e:
-        print "Failed to run test suite for package '%s' because: %s"\
-          %(packname,e)
-        return 
-    run_suite(get_package_suite(packname))
+#-----------------------------------------------------------------------------
+def test_package(packname, testname=None):
+    "Run all tests for the given package"
+    testdict = get_package_tests(packname)
+    tests = [val for key,val in testdict.items() \
+             if testname==None or testname==key]
+    run_suite(TestSuite([makeSuite(test) for test in tests]))
 
+#-----------------------------------------------------------------------------
 def test_all():
-    for packname in packages:
-        test_package(packname)
-
-def suite(): return unittest.TestSuite()
+    for packname in packages: test_package(packname)
