2e504282699f494a07b8a8724f6e52ca4051384b
jonathan.taylor <>
2006-04-16 00:42:18 -0700

    fixed example3 -- labels has to be a numpy array...

--- examples/quickstart/example3.py
+++ examples/quickstart/example3.py
@@ -4,18 +4,23 @@ import pylab
 
 def mask_and_func(subject=0, run=1, offset=5):
     M = ni.image.Image('http://kff.stanford.edu/FIAC/fiac%d/fonc%d/fsl/mask.img' % (subject,run))    
-    m = N.zeros(M.grid.shape, N.Float)
+    m = N.zeros(M.grid.shape)
     middle = [slice(i/2-offset, i/2+offset, 1) for i in m.shape]
     m[middle] = 1
+    other = [slice(i/2-offset+1, i/2+offset+1, 1) for i in m.shape]
+    m[other] = 2
     mm = ni.image.Image(m, grid=M.grid)
     f = ni.fmri.fMRIImage('http://kff.stanford.edu/FIAC/fiac%d/fonc%d/fsl/filtered_func_data.img' % (subject, run))
     return mm, f
 
 m, f = mask_and_func()
 f.grid.itertype = 'parcel'
-f.grid.labels = m
-f.grid.labelset = [0.,1.]
+f.grid.labels = m.readall()
+f.grid.labelset = [1, 2]
 
-print m.image.data.sum()
+means = {}
 for d in f:
-    print d.shape
+    means[f.label] = N.mean(d, axis=1)
+
+pylab.plot(means[1], means[2], 'bo')
+pylab.show()
