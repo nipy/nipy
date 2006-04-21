23d6a9181544b69dd321a44c07f1ddb7ddac0aaf
brian.hawthorne <>
2006-04-20 19:18:23 -0700

    explicit imports now taken care of by preloader

--- lib/neuroimaging/reference/__init__.py
+++ lib/neuroimaging/reference/__init__.py
@@ -1,9 +1,9 @@
-import grid
-import axis
-import coordinate_system
-import mapping
-import grid_iterators
-import slices
+#import grid
+#import axis
+#import coordinate_system
+#import mapping
+#import grid_iterators
+#import slices
 
 
 from slices import squeezeshape
