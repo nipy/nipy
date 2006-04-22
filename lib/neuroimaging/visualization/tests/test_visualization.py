005da16560a63fb244c925ea4a1aae794e6ed85b
brian.hawthorne <>
2006-04-22 09:16:10 -0700

    refactored datasource

--- lib/neuroimaging/visualization/tests/test_visualization.py
+++ lib/neuroimaging/visualization/tests/test_visualization.py
@@ -1,7 +1,8 @@
 import unittest, os, scipy, glob, sets
 import numpy as N
 from neuroimaging.image import Image, interpolation
-from neuroimaging.visualization import viewer, slices, arrayview
+from neuroimaging.visualization import viewer, slices
+from neuroimaging.visualization import arrayview
 from neuroimaging.reference import slices as rslices
 import pylab
 
@@ -10,9 +11,6 @@ class VisualizationTest(unittest.TestCase):
     def setUp(self):
         url = 'http://kff.stanford.edu/~jtaylo/BrainSTAT/rho.img'
         self.img = Image(url)
-        self.interpolator = interpolation.ImageInterpolator(self.img)
-        self.m = float(self.img.readall().min())
-        self.M = float(self.img.readall().max())
 
     def test_view(self):
         view = viewer.BoxViewer(self.img, z_pix=80.)
@@ -20,39 +18,36 @@ class VisualizationTest(unittest.TestCase):
         pylab.show()
 
     def test_transversal_slice(self):
-
-        _slice = slices.transversal(self.img, z=0., xlim=[-20,20.], ylim=[0,40.])
-
-        x = slices.PylabDataSlice(self.interpolator,
-                                  _slice,
-                                  vmax=self.M,
-                                  vmin=self.m,
-                                  colormap='spectral',
-                                  interpolation='nearest')
-        x.height = 0.8
-        x.width = 0.8
-
+        self.interpolator = interpolation.ImageInterpolator(self.img)
+        min = float(self.img.readall().min())
+        max = float(self.img.readall().max())
+        _slice = slices.transversal(self.img, z=0.,
+          xlim=[-20,20.], ylim=[0,40.])
+        x = slices.PylabDataSlice(self.interpolator, _slice, vmax=max, vmin=min,
+          colormap='spectral', interpolation='nearest')
+        x.width = 0.8; x.height = 0.8
         pylab.figure(figsize=(3,3))
         x.getaxes()
-
-
         pylab.imshow(x.RGBA(), origin=x.origin)
         pylab.show()
       
     def test_transversal_slice2(self):
-
         x = slices.PylabTransversal(self.img, y=3., xlim=[-49.,35.])
-        x.height = 0.8
-        x.width = 0.8
+        x.width = 0.8; x.height = 0.8
         pylab.figure(figsize=(3,3))
         x.getaxes()
-
         pylab.imshow(x.RGBA(), origin=x.origin)
         pylab.show()
+
+    def test_arrayview(self):
+        from neuroimaging.visualization import arrayview
+        arrayview.arrayview(self.img.data)
+
 
 class ArrayviewTest(unittest.TestCase):
 
     def test_arrayview(self):
+        from neuroimaging.visualization import arrayview
         from pylab import randn
         arrayview.arrayview(randn(20,20))
 
