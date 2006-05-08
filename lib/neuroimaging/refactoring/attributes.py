8626440557bd436176d281ea0ea9b2d9e6e317fb
brian.hawthorne <>
2006-05-08 00:57:34 -0700

    continuing development of attribute framework

--- lib/neuroimaging/refactoring/attributes.py
+++ lib/neuroimaging/refactoring/attributes.py
@@ -1,32 +1,46 @@
-import sets
+from copy import copy
+from sets import Set
 
-class InterfaceOmission (Exception): pass
+class ProtocolOmission (Exception): pass
 
 ##############################################################################
 class attribute (property):
     _attvals_name = "__attribute_values__"
-    makeclass = False
+    classdef = False
     valtype = None
+    default = None
+    readonly = False
+    doc = ""
     implements = ()
 
     #-------------------------------------------------------------------------
     class __metaclass__ (type):
-        def __new__(metaclass, classname, bases, classdict, makeclass=False):
+        def __new__(metaclass, classname, bases, classdict, classdef=False):
             # return the attribute class
-            if bases == (property,) or classdict.get("makeclass") or makeclass:
+            if bases == (property,) or classdict.get("classdef") or classdef:
                 return type.__new__(metaclass, classname, bases, classdict)
             # return a attribute instance
-            return metaclass(classname, bases, classdict, makeclass=True)(
-              classname,
-              valtype=classdict.get("valtype"),
-              default=classdict.get("default"),
-              doc=classdict.get("__doc__"))
+            return metaclass(classname, bases, classdict, classdef=True)(
+              classname, doc=classdict.get("__doc__"))
 
     #-------------------------------------------------------------------------
-    def __init__(self, name, valtype=None, default=None, doc=None):
+    @staticmethod
+    def clone(att, **kwargs):
+        """
+        Static factory method for constructing a new attribute from an
+        existing one, with modifications passed as keyword arguments.
+        """
+        newatt = copy(att)
+        newatt.__dict__.update(kwargs)
+        return newatt
+
+    #-------------------------------------------------------------------------
+    def __init__(self,
+      name, valtype=None, default=None, readonly=None, doc=None):
+        for argname in ("valtype","default","readonly"):
+            argval = locals()[argname]
+            if argval is not None: setattr(self, argname, argval)
         self.name = name
-        self.valtype = valtype
-        self.default = default
         if self.valtype is None and default is not None:
             self.valtype = type(default)
         property.__init__(self,
@@ -56,18 +70,18 @@ class attribute (property):
               (self.name,`value`,self.valtype)
 
         # protocol check
-        defined = sets.Set(dir(value))
+        defined = Set(dir(value))
         for protocol in self.implements:
-            required = sets.Set(dir(protocol))
+            required = Set(dir(protocol))
             if not required.issubset(defined):
-                raise InterfaceOmission, list(required - defined)
+                raise ProtocolOmission, list(required - defined)
 
     #-------------------------------------------------------------------------
     def isvalid(self, host, value):
         try:
             self.validate(host, value)
             return True
-        except TypeError, InterfaceOmission:
+        except TypeError, ProtocolOmission:
             return False
 
     #-------------------------------------------------------------------------
@@ -81,6 +95,9 @@ class attribute (property):
 
     #-------------------------------------------------------------------------
     def set(self, host, value):
+        if self.readonly and self.isinitialized(host):
+            raise AttributeError(
+              "attribute %s is read-only has already been set"%self.name)
         self.validate(host, value)
         self._get_attvals(host)[self.name] = value
         
@@ -89,20 +106,37 @@ class attribute (property):
         attvals = self._get_attvals(host)
         if attvals.has_key(self.name): del attvals[self.name]
 
-class readonly (attribute):
-    "A attribute which is read-only."
-    makeclass = True
-    def set(self, host, value):
-        raise AttributeError("attribute %s is read only"%self.name)
- 
-class setonce (attribute):
-    makeclass = True
-    def set(self, host, value):
-        if self.isinitialized(host):
-            raise AttributeError(
-              "setonce attribute %s has already been set"%self.name)
-        else: self.super().set(host, value)
 
+##############################################################################
+class wrapper (attribute):
+    classdef=True
+    attname=None
+    def __init__(self, name, delegate, attname=None, readonly=None):
+        attribute.__init(self, name)
+        self.delegate = delegate,
+        self.attname = self.attname or attname or name
+        if readonly is not None: self.readonly = readonly
+    def get(self, host):
+        delegate = getattr(host,self.delegate.name)
+        return getattr(delegate, self.attname or self.name)
+    def set(self, host, value):
+        if self.readonly:
+            raise AttributeError("proxy %s is read-only"%self.name)
+        delegate = getattr(host,self.delegate.name)
+        setattr(delegate, self.attname or self.name, value)
+
+def deferto(delegate):
+    # note, this won't pull in delegate superclass stuff...
+    framedict(1).update(
+      dict([(attname,wrapper(attname,delegate))\
+            for attname in delegate.__dict__.keys()]))
+
+##############################################################################
+class readonly (attribute):
+    "A attribute which cannot be changed after it is initialized."
+    classdef = True
+    readonly = True
+ 
 def _test():
     class Foo (object):
         class x (attribute):
@@ -110,10 +144,8 @@ def _test():
             def get(self, host):
                 print "Customised getter: getting",self.name,"from",host
                 return attribute.get(self, host)
-
-        class y (setonce): "test attribute y";
-
-        class z (readonly): default=10
+        class y (readonly): "test attribute y"
+        class z (readonly): "test attribute z"; default=10
 
     f = Foo()
     print f.x
