85720a4d2d0dcb81d27ede40e6cdf9198a38b4ca
brian.hawthorne <>
2006-05-10 23:40:02 -0700

    move pipes functionality into Image class

--- lib/neuroimaging/image/__init__.py
+++ lib/neuroimaging/image/__init__.py
@@ -1,17 +1,46 @@
-import types
+import types, os
 
 import enthought.traits as traits
 import numpy as N
 
-import pipes
 from neuroimaging.reference import grid, axis, mapping
+from neuroimaging.reference.grid import IdentityGrid, ParcelIterator, SliceParcelIterator
 from neuroimaging.data import DataSource
+from neuroimaging.image.formats import getreader
+
+zipexts = (".gz",".bz2")
 
 
 ##############################################################################
 class Image(traits.HasTraits):
     isfile = False
     shape = traits.ListInt()
+    fill = traits.Float(0.0)
+
+    class ArrayImage (object):
+        "A simple class to mimic an image file from an array."
+        def __init__(self, data, grid=None):
+            """
+            Create an ArrayPipe instance from an array, by default assumed to be 3d.
+
+            >>> from numpy import *
+            >>> from neuroimaging.image import Image
+            >>> z = Image.ArrayImage(zeros((10,20,20)))
+            >>> print z.ndim
+            3
+            """
+            self.data = data
+            self.shape = self.data.shape
+            self.grid = grid and grid or IdentityGrid(self.shape)
+        def getslice(self, _slice): return self.data[_slice]
+        def writeslice(self, _slice, data): self.data[_slice] = data
+
+    @staticmethod
+    def fromurl(url, datasource=DataSource(), mode="r", grid=None, clobber=False, **keywords):
+        filebase, fileext = os.path.splitext(url.strip())
+        if fileext in zipexts: filebase, fileext = os.path.splitext(filebase)
+        return getreader(fileext)(filename=url,
+          datasource=datasource, mode=mode, clobber=clobber, grid=grid, **keywords)
 
     #-------------------------------------------------------------------------
     def __init__(self, image, datasource=DataSource(), **keywords):
@@ -29,12 +58,12 @@ class Image(traits.HasTraits):
         # from array
         elif isinstance(image, N.ndarray) or isinstance(image, N.core.memmap):
             self.isfile = False
-            self.image = pipes.ArrayPipe(image, **keywords)
+            self.image = self.ArrayImage(image, **keywords)
 
         # from filename or url
         elif type(image) == types.StringType:
             self.isfile = True
-            self.image = pipes.URLPipe(image, datasource).getimage()
+            self.image = self.fromurl(image, datasource, **keywords)
 
         else: raise ValueError("Image input must be a string, array, or another image.")
             
@@ -46,51 +75,38 @@ class Image(traits.HasTraits):
         self.ndim = len(self.shape)
 
         # When possible, attach memory-mapped array or array as buffer attr
-
         if hasattr(self.image, 'memmap'):
             self.buffer = self.image.memmap
         elif isinstance(self.image.data, N.ndarray):
             self.buffer = self.image.data          
 
     #-------------------------------------------------------------------------
-    def __getitem__(self, slices):
-        return self.getslice(slices)
-
-    #-------------------------------------------------------------------------
-    def __setitem__(self, slices, data):
-        self.writeslice(slices, data)
+    def __getitem__(self, slices): return self.getslice(slices)
+    def __setitem__(self, slices, data): self.writeslice(slices, data)
 
     #-------------------------------------------------------------------------
     def __del__(self):
         if self.isfile:
-            try:
-                self.image.close()
-            except:
-                pass
-        else:
-            del(self.image)
+            try: self.image.close()
+            except: pass
+        else: del(self.image)
 
     #-------------------------------------------------------------------------
     def open(self, mode='r'):
-        if self.isfile:
-            self.image.open(mode=mode)
+        if self.isfile: self.image.open(mode=mode)
 
     #-------------------------------------------------------------------------
     def close(self):
         if self.isfile:
-            try:
-                self.image.close()
-            except:
-                pass
+            try: self.image.close()
+            except: pass
         
     #-------------------------------------------------------------------------
     def __iter__(self):
-        """
-        Create an iterator over an image based on its grid's iterator.
-        """
+        "Create an iterator over an image based on its grid's iterator."
         iter(self.grid)
-
-        if isinstance(self.grid.iterator, grid.ParcelIterator) or isinstance(self.grid.iterator, grid.SliceParcelIterator):
+        if isinstance(self.grid.iterator, ParcelIterator) or \
+           isinstance(self.grid.iterator, SliceParcelIterator):
             self.buffer.shape = N.product(self.buffer.shape)
         return self
 
@@ -98,15 +114,13 @@ class Image(traits.HasTraits):
     def compress(self, where, axis=0):
         if hasattr(self, 'buffer'):
             return self.buffer.compress(where, axis=axis)
-        else:
-            raise ValueError, 'no buffer: compress not supported'
+        else: raise ValueError, 'no buffer: compress not supported'
 
     #-------------------------------------------------------------------------
     def put(self, data, indices):
         if hasattr(self, 'buffer'):
             return self.put.compress(data, indices)
-        else:
-            raise ValueError, 'no buffer: put not supported'
+        else: raise ValueError, 'no buffer: put not supported'
 
     #-------------------------------------------------------------------------
     def next(self, value=None, data=None):
@@ -117,48 +131,28 @@ class Image(traits.HasTraits):
         the original image\'s grid, i.e. to just take the value the
         original image\'s iterator returns and use it here.
         """
-        if value is None:
-            self.itervalue = self.grid.next()
-            value = self.itervalue
-
+        if value is None: self.itervalue = value = self.grid.next()
         itertype = value.type
+        postread = getattr(self, 'postread', None) or (lambda x:x)
 
-        if itertype is 'slice':
-            if data is None:
-                return_value = N.squeeze(self.getslice(value.slice))
-                if hasattr(self, 'postread'):
-                    return self.postread(return_value)
-                else:
-                    return return_value
-            else:
-
-                self.writeslice(value.slice, data)
-
-        elif itertype is 'parcel':
-            if data is None:
+        if data is None:
+            if itertype is 'slice':
+                return postread(N.squeeze(self.getslice(value.slice)))
+            elif itertype is 'parcel':
                 value.where.shape = N.product(value.where.shape)
                 self.label = value.label
-                return_value = self.compress(value.where, axis=0)
-                if hasattr(self, 'postread'):
-                    return self.postread(return_value)
-                else:
-                    return return_value
-            else:
+                return postread(self.compress(value.where, axis=0))
+            elif itertype == 'slice/parcel':
+                return postread(self.getslice(value.slice).compress(value.where))
+        else:
+            if itertype is 'slice':
+                self.writeslice(value.slice, data)
+            elif itertype is 'parcel':
                 indices = N.nonzero(value.where)
                 self.put(data, indices)
-
-        elif itertype == 'slice/parcel':
-            if data is None:
-                tmp = self.getslice(value.slice)
-                return_value = tmp.compress(value.where)
-                if hasattr(self, 'postread'):
-                    return self.postread(return_value)
-                else:
-                    return return_value
-            else:
+            elif itertype == 'slice/parcel':
                 indices = N.nonzero(value.where)
-                self.buffer.put(data, indices)
-                
+                self.put(data, indices)
 
     #-------------------------------------------------------------------------
     def getvoxel(self, voxel):
@@ -168,7 +162,8 @@ class Image(traits.HasTraits):
 
     #-------------------------------------------------------------------------
     def toarray(self, clean=True, **keywords):
-        '''Return a Image instance that has an ArrayPipe as its image attribute.
+        """
+        Return a Image instance that has an ArrayPipe as its image attribute.
 
         >>> from numpy import *
         >>> from BrainSTAT import *
@@ -180,65 +175,46 @@ class Image(traits.HasTraits):
         >>> _test = test.toarray(slice=(2,), grid=test.grid)
         >>> print _test.shape
         (13, 128, 128)
-        '''
-
-        if self.isfile:
-            self.close()
-
+        """
+        if self.isfile: self.close()
         if clean:
             _clean = N.nan_to_num
             _data = _clean(self.readall(**keywords))
-        else:
-            _data = self.readall(**keywords)
-
-        if hasattr(self, 'postread'):
-            _data = self.postread(_data)
+        else: _data = self.readall(**keywords)
+        if hasattr(self, 'postread'): _data = self.postread(_data)
         return Image(_data, grid=self.grid, **keywords)
 
     #-------------------------------------------------------------------------
     def tofile(self, filename, array=True, **keywords):
         outimage = Image(filename, mode='w', grid=self.grid, **keywords)
-
         if array:
             tmp = self.toarray(**keywords)
             outimage.image.writeslice(slice(0,self.grid.shape[0],1), tmp.image.data)
         else:
             tmp = iter(self)
             outimage = iter(outimage)
-            for dataslice in tmp:
-                outimage.next(data=dataslice)
-                
+            for dataslice in tmp: outimage.next(data=dataslice)
         outimage.close()
-        
         return outimage
 
-    fill = traits.Float(0.0)
-    
     #-------------------------------------------------------------------------
     def readall(self, clean=False, **keywords): 
-        '''
+        """
         Read an entire Image object, returning a numpy, not another instance of
         Image. By default, it does not read 4d images. Missing values are
         filled in with the value of fill (default=self.fill=0.0).
-        '''
+        """
         try:
             _slice = self.grid.iterator.allslice
         except:
             _slice = slice(0, self.shape[0], 1)
         value = self.image.getslice(_slice)
-
-        if clean:
-            value = Image(_clean(value, fill=self.fill))
+        if clean: value = Image(_clean(value, fill=self.fill))
         return value
 
-    def getslice(self, slice):
-        return self.image.getslice(slice)
-
-    def writeslice(self, slice, data):
-        return self.image.writeslice(slice, data)
-
-    def check_grid(self, test):
-        return self.grid == test.grid
+    def getslice(self, slice): return self.image.getslice(slice)
+    def writeslice(self, slice, data): return self.image.writeslice(slice, data)
+    def check_grid(self, test): return self.grid == test.grid
 
 
 ##############################################################################
@@ -251,17 +227,13 @@ class ImageSequenceIterator(traits.HasTraits):
     """
     def __init__(self, imgs, grid=None, **keywords):
         self.imgs = imgs
-        if grid is None:
-            self.grid = iter(self.imgs[0].grid)
-        else:
-            self.grid = iter(grid)
+        if grid is None: self.grid = iter(self.imgs[0].grid)
+        else: self.grid = iter(grid)
 
-    def __iter__(self):
-        return self
+    def __iter__(self): return self
 
     def next(self, value=None):
-        if value is None:
-            value = self.grid.next()
+        if value is None: value = self.grid.next()
         v = []
         for i in range(len(self.imgs)):
             v.append(self.imgs[i].next(value=value))
