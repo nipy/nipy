7aebe3b39a1c55376679eb5046b23d2945e25e43
brian.hawthorne <>
2006-05-14 23:43:25 -0700

    merge Slicer into SliceIterator

--- lib/neuroimaging/reference/iterators.py
+++ lib/neuroimaging/reference/iterators.py
@@ -4,7 +4,8 @@ from sets import Set as set
 # External imports
 import numpy as N
 import enthought.traits as traits
-from attributes import readonly, constant
+from attributes import attribute, readonly, constant
+from protocols import Sequence
 
 # Package imports
 from neuroimaging import haslength
@@ -13,8 +14,15 @@ itertypes = ("slice", "parcel", "slice/parcel", "all")
 
 
 ##############################################################################
-class Slicer(traits.HasTraits):
-    '''
+class SliceIteratorNext (object):
+    class type (constant): default="slice"
+    class slice (readonly): pass
+    def __init__(self, slice): self.slice = slice
+
+
+##############################################################################
+class SliceIterator (object):
+    """
     This class is an iterator that steps through the slices
     of a N-dimensional array of shape shape, along a particular axis, with a
     given step and an optional start.
@@ -24,39 +32,38 @@ class Slicer(traits.HasTraits):
     defaults to [1]*(nslicedim).
 
     More than one slice can be output at a time, using nslice.
-    '''
+    """
+    class end (readonly): pass
+    class start (readonly):
+        def init(_, self): return N.array((0,)*self.nslicedim)
+    class step (readonly):
+        def init(_, self): return N.array((1,)*self.nslicedim)
+    class axis (readonly): default=0
+    class nslicedim (readonly): implements=int
+    class nslice (readonly): default=1
+    class ndim (readonly): get=lambda _,s: len(s.end)
+    class type (constant): default="slice"
 
-    axis = traits.Int(0)
-    end = traits.List()
-    step = traits.Any()
-    start = traits.Any()
-    ndim = traits.Int()
-    nslicedim = traits.Int()
-    nslice = traits.Int(1)
-    
-    type = traits.String('slice')
+    #-------------------------------------------------------------------------
+    def __init__(self, end, start=None, step=None, axis=None, nslicedim=None,
+      nslice=None):
+        self.end = tuple(end)
+        for attr in ("start","step","axis","nslice"):
+            if locals()[attr] is not None: setattr(self, attr, locals()[attr])
+        self.nslicedim = max(nslicedim, self.axis+1)
 
-    def _end_changed(self):
-        self.ndim = len(self.end)
+        self.allslice = [slice(self.start[i],
+                               self.end[i],
+                               self.step[i]) for i in range(self.nslicedim)]
 
-    def __init__(self, end, **keywords):
-        traits.HasTraits.__init__(self, **keywords)
-        self.end = list(end)
-
-        self.nslicedim = max(self.nslicedim, self.axis+1)
-
-        if self.step is None:
-            self.step = N.array([1]*self.nslicedim)
-
-        if self.start is None:
-            self.start = N.array([0]*self.nslicedim)
-
+    #-------------------------------------------------------------------------
     def __iter__(self):
         self.isend = False
         self.slice = self.start[self.axis]
         self.last = self.end[self.axis]
         return self
 
+    #-------------------------------------------------------------------------
     def next(self):
         if self.isend:
             raise StopIteration
@@ -73,48 +80,22 @@ class Slicer(traits.HasTraits):
                 _slices.append(_slice)
 
         if self.slice >= self.last: self.isend = True
-        return _slices, self.isend
+        return SliceIteratorNext(_slices)
 
 
 ##############################################################################
-class SliceIteratorNext (object):
-    class type (constant): default="slice"
-    class slice (readonly): pass
-    def __init__(self, slice): self.slice = slice
-
-
-##############################################################################
-class SliceIterator(Slicer):
+class AllSliceIterator (object):
+    class type (constant): default="all"
+    class shape (readonly): implements=tuple
+    class isend (attribute): default=False
 
     #-------------------------------------------------------------------------
-    def __init__(self, shape, **keywords):
-        traits.HasTraits.__init__(self, **keywords)
-        Slicer.__init__(self, shape, **keywords)
-        self.allslice = [slice(self.start[i],
-                               self.end[i],
-                               self.step[i]) for i in range(self.nslicedim)]
-
-    #-------------------------------------------------------------------------
-    def next(self):
-        _slice, isend = Slicer.next(self)
-        return SliceIteratorNext(_slice)
-
-
-##############################################################################
-class AllSliceIterator(Slicer):
-
-    type = traits.Str('all')
+    def __init__(self, shape): self.shape = shape
 
     #-------------------------------------------------------------------------
     def __iter__(self):
         self.isend = False
         return self
-
-    #-------------------------------------------------------------------------
-    def __init__(self, shape, **keywords):
-        traits.HasTraits.__init__(self, **keywords)
-        self.shape = shape
-        self.isend = False
 
     #-------------------------------------------------------------------------
     def next(self):
