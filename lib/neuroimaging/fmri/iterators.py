c371de075d2b3df1641581f17fac7b3c996ffe8b
brian.hawthorne <>
2006-05-19 08:08:53 -0700

    grid_iterators renamed to iterators

--- lib/neuroimaging/fmri/iterators.py
+++ lib/neuroimaging/fmri/iterators.py
@@ -1,42 +1,37 @@
+import enthought.traits as traits
+from attributes import readonly
+
 from neuroimaging.reference.iterators import SliceIterator, SliceIteratorNext,\
   ParcelIterator, SliceParcelIterator, SliceParcelIteratorNext
-import enthought.traits as traits
 
+##############################################################################
 class fMRISliceIterator(SliceIterator):
     """
     Instead of iterating over slices of a 4d file -- return slices
     of timeseries.
     """
-    nframe = traits.Int()
-
-    def __init__(self, shape, **keywords):
-        SliceIterator.__init__(self, shape, axis=1, **keywords)
-        self.nframe = shape[0]
-
-    def next(self):
-        value = iterators.SliceIterator.next(self)
-        return iterators.SliceIteratorNext(slice=value.slice, type='slice')
+    class nframe (readonly): get=lambda _,s: s.end[0]
+    def __init__(self, end, **kwargs):
+        kwargs["axis"]=1
+        SliceIterator.__init__(self, end, **kwargs)
 
 
-class fMRIParcelIterator(iterators.ParcelIterator):
-    """
-    Return parcels of timeseries.
-    """
+##############################################################################
+class fMRIParcelIterator(ParcelIterator):
+    "Return parcels of timeseries."
 
 
-class fMRISliceParcelIterator(iterators.SliceParcelIterator):
-    """
-    Return parcels of timeseries within slices.
-    """
-
-    nframe = traits.Int()
+##############################################################################
+class fMRISliceParcelIterator(SliceParcelIterator):
+    "Return parcels of timeseries within slices."
+    class nframe (readonly): implements=int
 
     def __init__(self, labels, labelset, shape, **keywords):
-        iterators.SliceParcelIterator.__init__(self, labels, labelset, **keywords)
+        SliceParcelIterator.__init__(self, labels, labelset, **keywords)
         self.nframe = shape[0]
 
     def next(self):
-        value = iterators.SliceParcelIterator.next(self)
+        value = SliceParcelIterator.next(self)
         _slice = [slice(0,self.nframe,1), value.slice]
-        return iterators.SliceParcelIteratorNext(slice=_slice, type=value.type, label=value.label, where=value.where)
-
+        return SliceParcelIteratorNext(slice=_slice, type=value.type,
+          label=value.label, where=value.where)
