27fe7bcbc0af4cd1fb3d714215c132a97e2d54d5
brian.hawthorne <>
2006-05-23 16:35:24 -0700

    bugfix protocol function to work with new Protocol classes

--- lib/protocols.py
+++ lib/protocols.py
@@ -1,26 +1,33 @@
 """
 Very lightweight system for checking protocol implementation on Python objects.
 """
+from types import ClassType
+
+class Protocol: pass
+        
 
 def protocol(*objects):
     """
     @return the tuple of names representing the complete protocol
     supported by the given objects.
     """
-    protocol = set()
-    for obj in objects:
-        protocol = protocol.union(set([name for name in dir(obj)]))
-    return protocol
+    if len(objects) == 0: return set()
+    obj = objects[0]
+    proto = set(dir(obj))
+    if type(obj) == ClassType and issubclass(obj, Protocol):
+        proto.discard("__doc__")
+        proto.discard("__module__")
+    return proto.union(protocol(*objects[1:]))
 
 def implements(proto, value): return proto.issubset(protocol(value))
 
 class ProtocolOmission (Exception):
     "Indicate that a value does not support part of its expected protocol."
 
-class Iterable:
+class Iterable (Protocol):
     def __iter__(self): pass
 
-class Iterator:
+class Iterator (Protocol):
     def next(self): pass
 
 class Sequence (Iterable):
