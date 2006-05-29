a3279d9cabf3f90c5db9feb482474cca2258e804
jarrod.millman <>
2006-05-29 08:39:44 -0700

    docstring fix

--- lib/neuroimaging/reference/__init__.py
+++ lib/neuroimaging/reference/__init__.py
@@ -1,5 +1,5 @@
 """
-Frame of reference/coordinates module.
+Frame of reference/coordinates package.
 
 Mathematical model:  The idea of a chart S{phi}: I{U} S{sub} I{M} {->} B{R}^k
 on a "manifold" I{M}.  For a chart both input (I{M}) and output coordinates
