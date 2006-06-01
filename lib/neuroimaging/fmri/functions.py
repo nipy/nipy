f0932c656dc902f397c4bf1bf3841d47cb05dacd
jonathan.taylor <>
2006-05-31 23:12:26 -0700

    moved confounds from "functions" to "protocol"

--- lib/neuroimaging/fmri/functions.py
+++ lib/neuroimaging/fmri/functions.py
@@ -17,7 +17,6 @@ from scipy.interpolate import interp1d
 
 from neuroimaging.statistics.utils import StepFunction, recipr0
 
-
 # Prototypical stimuli: "Event" (on/off) and "Stimuli" (step function)
 # -Event inherits from Stimulus so most functionality is in Stimulus
 # -changes are just in specifying parameters of self.fn
@@ -216,95 +215,4 @@ class Events(Stimulus):
 
         self.fn = StepFunction(self.times, self.values, sorted=True)
 
-class InterpolatedConfound(TimeFunction):
-
-    times = traits.Any()
-    values = traits.Any()
-
-    def __init__(self, **keywords):
-        TimeFunction.__init__(self, **keywords)
-        if len(N.asarray(self.values).shape) == 1:
-            self.f = interp1d(self.times, self.values, bounds_error=0)
-            self.nout = 1
-        else:
-            self.f = []
-            values = N.asarray(self.values)
-            for i in range(values.shape[0]):
-                f = interp1d(self.times, self.values[:,i], bounds_error=0)
-                self.f.append(f)
-            self.nout = values.shape[0]
-            
-    def __call__(self, time=None, **extra):
-
-        lt = len(N.array(time).shape)
-
-        columns = []
-
-        if self.nout == 1:
-            columns.append(self.f(time))
-        else:
-            if type(self.f) in [types.ListType, types.TupleType]:
-                for f in self.f:
-                    columns.append(f(time))
-            else:
-                columns = self.f(time)
-
-        if self.windowed:
-            _window = N.greater(time, self.window[0]) * N.less_equal(time, self.window[1])
-            columns = [column * _window for column in columns]
-                
-        return N.squeeze(N.array(columns))
-
-
-class FunctionConfound(TimeFunction):
-
-    def __init__(self, fn=[], **keywords):
-        '''
-        Argument "fn" should be a sequence of functions describing the regressor.
-        '''
-        TimeFunction.__init__(self, **keywords)
-        self.nout = len(fn)
-        if len(fn) == 1:
-            self.fn = fn[0]
-        else:
-            self.fn = fn
-
-class SplineConfound(FunctionConfound):
-
-    df = traits.Int(4)
-    knots = traits.List()
-
-    def __init__(self, **keywords):
-        '''
-        Basic spline trend confound.
-        '''
-
-        TimeFunction.__init__(self, **keywords)
-        tmax = self.window[1]
-        tmin = self.window[0]
-        trange = tmax - tmin
-
-        self.fn = []
-
-        def getpoly(j):
-            def _poly(time=None):
-                return time**j
-            return _poly
-
-        for i in range(min(self.df, 4)):
-            self.fn.append(getpoly(i))
-
-        if self.df >= 4 and not self.knots:
-            self.knots = list(trange * N.arange(1, self.df - 2) / (self.df - 3.0) + tmin)
-        self.knots[-1] = N.inf 
-
-        def _getspline(a, b):
-            def _spline(time=None):
-                return time**3 * N.greater(time, a) * N.less_equal(time, b)
-            return _spline
-
-        for i in range(len(self.knots) - 1):
-            self.fn.append(_getspline(self.knots[i], self.knots[i+1]))
-
-        self.nout = self.df
         
