a962e631b0fc997a6a5569244463c3f96da8b671
tim.leslie <>
2006-06-25 02:50:15 -0700

    add extra fwhm2sigma test

--- lib/neuroimaging/fmri/tests/test_utils.py
+++ lib/neuroimaging/fmri/tests/test_utils.py
@@ -39,7 +39,9 @@ class utilTest(unittest.TestCase):
         ensure that fwhm2sigma and sigma2fwhm are inverses of each other        
         """
         fwhm = N.arange(1.0, 5.0, 0.1)
+        sigma = N.arange(1.0, 5.0, 0.1)
         scipy.testing.assert_almost_equal(sigma2fwhm(fwhm2sigma(fwhm)), fwhm)
+        scipy.testing.assert_almost_equal(fwhm2sigma(sigma2fwhm(sigma)), sigma)
         
 
 if __name__ == '__main__':
