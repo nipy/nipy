ed3b4dac0ee4f86e053b7c5bf18a82c124c2ccfd
jonathan.taylor <>
2006-07-01 16:30:54 -0700

    missing validators module

---
+++ lib/neuroimaging/image/formats/validators.py
@@ -0,0 +1,89 @@
+from struct import pack, unpack, calcsize
+from types import StringType
+
+from neuroimaging import traits
+
+def is_tupled(packstr, value):
+    return (packstr[-1] != 's' and len(tuple(value)) > 1)
+
+def isseq(value):
+    try:
+        len(value)
+        isseq = True
+    except TypeError:
+        isseq = False
+    return type(value) != StringType and isseq
+
+class BinaryHeaderValidator(traits.TraitHandler):
+    """
+    Trait handler used in definition of ANALYZE and NIFTI-1
+    headers.
+    """
+    def __init__(self, packstr, value=None, seek=0, bytesign = '>', **keywords):
+        if len(packstr) < 1: raise ValueError("packstr must be nonempty")
+        for key, value in keywords.items(): setattr(self, key, value)
+        self.seek = seek
+        self.packstr = packstr
+        self.bytesign = bytesign
+        self.size = calcsize(self.packstr)
+
+    def write(self, value, outfile=None):
+        if isseq(value): valtup = tuple(value) 
+        else: valtup = (value,)
+        result = pack(self.bytesign+self.packstr, *valtup)
+        if outfile is not None:
+            outfile.seek(self.seek)
+            outfile.write(result)
+        return result
+
+    def validate(self, object, name, value):
+        try:
+        #if 1:
+            result = self.write(value)
+        except:
+            self.error(object, name, value)
+
+        _value = unpack(self.bytesign + self.packstr, result)
+        if is_tupled(self.packstr, _value): return _value
+        else: return _value[0]
+
+    def info(self):
+        return 'an object of type "%s", apply(struct.pack, "%s", object) must make sense' % (self.packstr, self.packstr)
+
+    def read(self, hdrfile):
+        hdrfile.seek(self.seek, 0)
+        value = unpack(self.bytesign + self.packstr,
+                       hdrfile.read(self.size))
+        if not is_tupled(self.packstr, value):
+            value = value[0]
+        return value
+
+def BinaryHeaderAtt(packstr, seek=0, value=None, **keywords):
+    """
+    Constructor for a binary header attribute for ANALYZE and NIFTI-1 files.
+    """
+    validator = BinaryHeaderValidator(packstr, value=value, seek=seek, **keywords)
+    return traits.Trait(value, validator)
+
+class BinaryFile(traits.HasTraits):
+    """
+    A simple way to specify the format of a binary file in terms
+    of strings that struct.{pack,unpack} can validate and a byte
+    position in the file.
+
+    This is used for both ANALYZE-7.5 and NIFTI-1 files.
+    """
+
+    def __init__(self, **keywords):
+        traits.HasTraits.__init__(self, **keywords)
+        self.hdrattnames = [name for name in self.trait_names() \
+          if isinstance(self.trait(name).handler, BinaryHeaderValidator)]
+
+    def readheader(self, hdrfile):
+        for traitname in self.hdrattnames:
+            trait = self.trait(traitname)
+            if hasattr(trait.handler, 'bytesign') and hasattr(self, 'bytesign'):
+                trait.handler.bytesign = self.bytesign
+            value = trait.handler.read(hdrfile)
+            setattr(self, traitname, value)
+
