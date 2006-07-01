23b8e0bcbaff8deaa6aa3d1f209c3b61c06f5c59
jonathan.taylor <>
2006-07-01 07:49:59 -0700

    URLs got changed by mistake in the comments of the enthought code

--- lib/neuroimaging/extra/enthought/traits/trait_handlers.py
+++ lib/neuroimaging/extra/enthought/traits/trait_handlers.py
@@ -5,7 +5,7 @@
 # This software is provided without warranty under the terms of the BSD
 # license included in enthought/LICENSE.txt and may be redistributed only
 # under the conditions described in the aforementioned license.  The license
-# is also available online at http://www.neuroimaging.extra.enthought.com/licenses/BSD.txt
+# is also available online at http://www.enthought.com/licenses/BSD.txt
 # Thanks for using Enthought open source!
 #
 # Author: David C. Morrill
