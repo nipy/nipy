6ac6906fa8319ec1fea0c2c4c9ad56af5f5fc091
tim.leslie <>
2006-07-08 15:31:39 -0700

    add a unit test for ConvolveFunctions

--- lib/neuroimaging/fmri/tests/test_utils.py
+++ lib/neuroimaging/fmri/tests/test_utils.py
@@ -2,7 +2,7 @@ import unittest
 import numpy as N
 import scipy
 
-from neuroimaging.fmri.utils import CutPoly, WaveFunction
+from neuroimaging.fmri.utils import CutPoly, WaveFunction, ConvolveFunctions
 
 class utilTest(unittest.TestCase):
     
@@ -33,6 +33,22 @@ class utilTest(unittest.TestCase):
         t = N.arange(0, 10.0, 0.1)
         y = f(t)
         scipy.testing.assert_almost_equal(y, [height*(x >= start and x < start + duration) for x in t])
+
+
+    def test_ConvolveFunctions(self):
+        """
+        The integral of the normalised convolution should be equal
+        to the product of the integrals of the original functions.
+        """
+        dt = 0.01
+        t = N.arange(0, 10.0, dt)
+        f1 = WaveFunction(2, 1, 1)
+        f2 = WaveFunction(4, 1, 1)
+        fa = ConvolveFunctions(f1, f2, [t[0], t[-1]], dt, normalize=[1,1])
+        int_f1 = dt*f1(t).sum()
+        int_f2 = dt*f2(t).sum()
+        int_fa = dt*fa(t).sum()
+        scipy.testing.assert_approx_equal(int_f1*int_f2, int_fa)
 
 
 if __name__ == '__main__':
