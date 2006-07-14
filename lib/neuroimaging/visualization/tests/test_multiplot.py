45a2c4a1086b8b6d897485f6cd2ec6d82ee48ac7
jonathan.taylor <>
2006-07-14 08:41:52 -0700

    tests for multiplot module

---
+++ lib/neuroimaging/visualization/tests/test_multiplot.py
@@ -0,0 +1,51 @@
+import unittest, os
+
+import numpy as N
+
+from neuroimaging.defines import pylab_def
+PYLAB_DEF, pylab = pylab_def()
+if PYLAB_DEF:
+    from neuroimaging.visualization.multiplot import MultiPlot
+
+
+class MultiPlotTest(unittest.TestCase):
+    if PYLAB_DEF:
+
+        def setUp(self):
+            def fn(x, *powers, **kw):
+                x = N.asarray(x)
+                if not powers:
+                    powers = (1,2,3)
+                v = N.array([x**i for i in powers])
+                if kw.has_key('coefs'):
+                    for i in range(v.shape[0]):
+                        v[i] *= kw['coefs'][i]
+                return v
+                        
+            self.fnplot = MultiPlot(fn, tmin=0, tmax=10., dt=0.1, title='Testing')
+            self.dataplot = MultiPlot(fn(N.linspace(0,10,101)), tmin=0, tmax=10., dt=0.1, title='Testing')
+
+        def test1(self):
+            self.fnplot.draw()
+            pylab.show()
+
+        def test2(self):
+            self.dataplot.draw()
+            pylab.show()
+
+        def test3(self):
+            self.fnplot.draw(args=(4,7))
+            pylab.show()
+
+        def test4(self):
+            print 4
+            self.fnplot.draw(args=(4,7), kw={'coefs':[2,6]})
+            pylab.show()
+
+
+def suite():
+    return unittest.makeSuite(MultiPlotTest)
+
+
+if __name__ == '__main__':
+    unittest.main()
