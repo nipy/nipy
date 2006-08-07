3b8d986f31d0466738e807e589ca1b17314ed09a
tim.leslie <>
2006-08-06 20:15:37 -0700

    some tests rely on particular files being in a particular place. these will be fixed in the future

--- lib/neuroimaging/reference/tests/test_mapping.py
+++ lib/neuroimaging/reference/tests/test_mapping.py
@@ -43,12 +43,15 @@ class MappingTest(unittest.TestCase):
 
                       
     def test_frombin(self):
+        # FIXME: this will only work on my (Tim) system. Need to sort out getting these
+        # test files either up on the web or into a standard place
         #mat = urllib.urlopen('ftp://ftp.cea.fr/pub/dsv/madic/FIAC/fiac3/fiac3_fonc3.txt')
         f = open('/home/timl/src/ni/ni/trunk/lib/neuroimaging/reference/tests/fiac3_fonc3_0089.mat')
         tstr = f.read()
         mapping.frombin(tstr)
 
     def test_matfromstr(self):
+        # FIXME: as above
         t1 = open('/home/timl/src/ni/ni/trunk/lib/neuroimaging/reference/tests/fiac3_fonc3_0089.mat').read()
         t2 = open('/home/timl/src/ni/ni/trunk/lib/neuroimaging/reference/tests/fiac3_fonc3.txt').read()
         a1 = mapping.matfromstr(t1)
@@ -56,6 +59,7 @@ class MappingTest(unittest.TestCase):
         N.testing.assert_almost_equal(a1, a2, 1e-6)
 
     def test_tofromfile(self):
+        # FIXME: This will only work on linux (at a guess)
         self._init()
         self.mapping.tofile("/tmp/mapping.csv")
         a = mapping.Mapping.fromfile("/tmp/mapping.csv")
