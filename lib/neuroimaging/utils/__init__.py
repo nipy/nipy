ba4283136ad7907d663da926674a7c74531de1a3
jarrod.millman <>
2006-08-23 07:05:29 -0700

    created utils package

---
+++ lib/neuroimaging/utils/__init__.py
@@ -0,0 +1,5 @@
+"""
+Package containging general purpose functions that are useful to a broader
+community and not restricted to the neuroimaging community. This package
+may contain third-party software included here for convience.
+"""
