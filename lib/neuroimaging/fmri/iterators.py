0b1bd5be502c6f180e92cf5fe132524856d1d142
tim.leslie <>
2006-08-25 00:14:24 -0700

    remove use of attributes

--- lib/neuroimaging/fmri/iterators.py
+++ lib/neuroimaging/fmri/iterators.py
@@ -1,24 +1,19 @@
-from attributes import readonly
-
 from neuroimaging.reference.iterators import SliceIterator, SliceParcelIterator
-
 
 class fMRISliceIterator(SliceIterator):
     """
     Instead of iterating over slices of a 4d file -- return slices of
     timeseries.
     """
-    class nframe (readonly): "number of frames"; get=lambda _,s: s.end[0]
+
     def __init__(self, end, **kwargs):
         kwargs["axis"]=1
         SliceIterator.__init__(self, end, **kwargs)
-
+        self.nframe = self.end[0]
 
 
 class fMRISliceParcelIterator(SliceParcelIterator):
     "Return parcels of timeseries within slices."
-
-    class nframe (readonly): "number of frames"; implements=int
 
     def __init__(self, parcelmap, parcelseq, nframe):
         SliceParcelIterator.__init__(self, parcelmap, parcelseq)
