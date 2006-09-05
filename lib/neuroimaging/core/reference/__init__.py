802f9ece81d3dc36dcd8ee09d41b216f89399147
jarrod.millman <>
2006-09-05 01:25:49 -0700

    fix arrow in docstring for epytext markup

--- lib/neuroimaging/core/reference/__init__.py
+++ lib/neuroimaging/core/reference/__init__.py
@@ -1,7 +1,7 @@
 """
 Frame of reference/coordinates package.
 
-Mathematical model:  The idea of a chart S{phi}: I{U} S{sub} I{M} {->} B{R}^k
+Mathematical model:  The idea of a chart S{phi}: I{U} S{sub} I{M} S{->} B{R}^k
 on a "manifold" I{M}.  For a chart both input (I{M}) and output coordinates
 (B{R}^k) must be defined and a map relating the two coordinate systems.
 """
