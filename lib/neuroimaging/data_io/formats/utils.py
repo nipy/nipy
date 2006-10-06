363cc88338a6aefc5ea07f9878d8b3a60e8a102f
tim.leslie <>
2006-10-05 22:32:15 -0700

    add docstrings and clean up

--- lib/neuroimaging/data_io/formats/utils.py
+++ lib/neuroimaging/data_io/formats/utils.py
@@ -31,20 +31,28 @@ _typemap = dict((
 
 # All allowed format strings.
 allformats = []
-for formats in _typemap.keys(): allformats.extend(formats)
+for _formats in _typemap.keys():
+    allformats.extend(_formats)
 
-format_defaults = {'i': 0,'I':0, 'h': 0, 'H':0, 'f': 0., 'c': '\0',\
-                   'l':0.,'s': '', 'B': 0}
+format_defaults = {'i': 0, 'I':0,
+                   'h': 0, 'H':0,
+                   'f': 0.,
+                   'c': '\0',
+                   'l':0.,
+                   's': '',
+                   'B': 0}
 
 ######## STRUCT PACKING/UNPACKING/INTERPRETATION ROUTINES ####################
 def numvalues(format):
+    """ The number of values for the given format. """
     numstr, fmtchar = format[:-1], format[-1]
     return (numstr and fmtchar not in ("s","p")) and int(numstr) or 1
 
 def elemtype(format):
     fmtchar = format[-1]
     for formats, typ in _typemap.items():
-        if fmtchar in formats: return typ
+        if fmtchar in formats:
+            return typ
     raise ValueError("format char %s must be one of: %s"%\
                      (fmtchar, allformats))
 
@@ -58,14 +66,22 @@ def formattype(format):
     return numvalues(format) > 1 and list or elemtype(format)
 
 def flatten_values(valseq):
-    # flattens the type of header values constructed by aggregate
-    if type(valseq) != type([]): return [valseq]
-    if valseq == []: return valseq
+    """ Flattens the type of header values constructed by aggregate. """
+    if type(valseq) != type([]):
+        return [valseq]
+    if valseq == []:
+        return valseq
     return flatten_values(valseq[0]) + flatten_values(valseq[1:])
 
-def takeval(numvalues, values):
-    if numvalues==1: return values.pop(0)
-    else: return [values.pop(0) for i in range(numvalues)]
+def takeval(numvals, values):
+    """ Take numvals from values.
+
+    Returns a single value if numvals == 1 or else a list of values.
+    """
+    if numvals == 1:
+        return values.pop(0)
+    else:
+        return [values.pop(0) for _ in range(numvals)]
 
 def struct_format(byte_order, elements):
     return byte_order+" ".join(elements)
@@ -82,8 +98,9 @@ def struct_pack(byte_order, elements, values):
     format = struct_format(byte_order, elements)
     return pack(format, *flatten_values(values))
 
-def touch(fname):
-    open(fname, 'w')
+def touch(filename):
+    """ Ensure that filename exists. """
+    open(filename, 'w')
 
 #### To filter data written to a file
 def cast_data(data, new_sctype, default_scale):
