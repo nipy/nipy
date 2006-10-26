637055f5d688a8d895b6d6ba3e38e521b397c03b
tim.leslie <>
2006-10-26 00:20:25 -0700

    typo

--- lib/neuroimaging/core/image/__init__.py
+++ lib/neuroimaging/core/image/__init__.py
@@ -1,6 +1,6 @@
 """
 The Image class provides the interface which should be used
-by users at the application level. It is build onto of a
+by users at the application level. It is build on top of a
 BaseImage object (self._source) which handles the actual
 representation of the data. A base image provides a grid,
 a data type and the data itself, while the main Image class
