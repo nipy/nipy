6a0a2fca3f7ab9884d9b76ef3f70f9e209e24247
matthew.brett <>
2006-10-27 03:18:57 -0700

    BrokenTest was expecting a t contrast when it should have been expecting an F contrast

--- lib/neuroimaging/modalities/fmri/tests/test_protocol.py
+++ lib/neuroimaging/modalities/fmri/tests/test_protocol.py
@@ -5,70 +5,6 @@ from scipy.sandbox.models import contrast
 
 from neuroimaging.modalities.fmri import hrf, protocol, functions
 
-class BrokenTest(unittest.TestCase):
-
-    def setUp(self):
-        """
-        Setup an iterator corresponding to the following .csv file:
-
-        hot,9.0,18.0
-        warm,27.0,36.0
-        hot,45.0,54.0
-        warm,63.0,72.0
-        hot,81.0,90.0
-        warm,99.0,108.0
-        hot,117.0,126.0
-        warm,135.0,144.0
-        hot,153.0,162.0
-        warm,171.0,180.0
-        hot,189.0,198.0
-        warm,207.0,216.0
-        hot,225.0,234.0
-        warm,243.0,252.0
-        hot,261.0,270.0
-        warm,279.0,288.0
-        hot,297.0,306.0
-        warm,315.0,324.0
-        hot,333.0,342.0
-        warm,351.0,360.0
-
-        """
-
-
-        on = [9.0, 27.0, 45.0, 63.0, 81.0, 99.0, 117.0,
-              135.0, 153.0, 171.0, 189.0, 207.0, 225.0,
-              243.0, 261.0, 279.0, 297.0, 315.0, 333.0, 351.0]
-        off = [18.0, 36.0, 54.0, 72.0, 90.0, 108.0, 126.0,
-               144.0, 162.0, 180.0, 198.0, 216.0, 234.0,
-               252.0, 270.0, 288.0, 306.0, 324.0, 342.0, 360.0]
-        p = ['hot', 'warm'] * 10
-        all = []
-        for i in range(10):
-            all.append([p[i], on[i], off[i]])
-        self.all = all
-
-
-    def setup_terms(self):
-        self.p = protocol.ExperimentalFactor('pain', self.all, delta=False)
-        self.p.convolved = False
-
-        self.IRF1 = hrf.glover
-        self.IRF2 = hrf.glover_deriv
-
-        self.t = N.arange(0,300,1)
-
-
-
-    def testContrast2(self):
-        self.setup_terms()
-        drift_fn = functions.SplineConfound(window=(0,300), df=4)
-        drift = protocol.ExperimentalQuantitative('drift', drift_fn)
-        formula = self.p + drift
-        c = contrast.Contrast(self.p.main_effect(), formula)
-        c.getmatrix(time=self.t)
-        N.testing.assert_almost_equal(c.matrix,
-                                          [0.,0.,0.,0.,-1.,1.])
-
 
 class ProtocolTest(unittest.TestCase):
 
@@ -185,7 +121,8 @@ class ProtocolTest(unittest.TestCase):
         c = contrast.Contrast(self.p.main_effect(), formula)
         c.getmatrix(time=self.t)
         N.testing.assert_almost_equal(c.matrix,
-                                          [0.,0.,0.,0.,-1.,1.])
+                                          [[0.,0.,0.,0.,1.,0.],
+                                           [0.,0.,0.,0.,0.,1.]])
 
     def testDesign1(self):
         self.setup_terms()
