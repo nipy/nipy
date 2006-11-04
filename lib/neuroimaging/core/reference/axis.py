968087121d967ce8c2d10b0c68512bcd44c60049
tim.leslie <>
2006-11-03 21:44:20 -0700

    add more docs

--- lib/neuroimaging/core/reference/axis.py
+++ lib/neuroimaging/core/reference/axis.py
@@ -1,8 +1,8 @@
 """
-The Axis family of classes are used to represent a named axis within a
+The L{Axis} family of classes are used to represent a named axis within a
 coordinate system. Axes can be regular discrete or continuous, finite or infinite.
 
-The current class hirachy looks like this.
+The current class hirachy looks like this::
 
                  Axis
                    |
@@ -44,23 +44,45 @@ class Axis(object):
                 % (self.name)
 
     def __eq__(self, other):
-        """ Equality is defined by name """
+        """ Equality is defined by name 
+        
+        @param other: The object to be compared with
+        @type other: L{Axis}
+        @rtype: C{bool}
+        """
         return self.name == other.name
 
     def valid(self, x):
-        """ test if x is a point on the axis. """
+        """ Test if x is a point on the axis. 
+        
+        @param x: A voxel.
+        @type x: C{float}
+        @raises NotImplementedError: Abstract method
+        @rtype: C{bool}
+        """
         raise NotImplementedError
 
     def max(self):
-        """ The maximum value of the axis. """
+        """ The maximum value of the axis. 
+        
+        @raises NotImplementedError: Abstract method
+        @rtype: C{numpy.float}        
+        """
         raise NotImplementedError
 
     def min(self):
-        """ The minimum value of the axis. """
+        """ The minimum value of the axis. 
+        
+        @raises NotImplementedError: Abstract method
+        @rtype: C{numpy.float}
+        """
         raise NotImplementedError
 
     def range(self):
-        """ A (min, max) pair representing the range of the axis. """
+        """ A (min, max) pair representing the range of the axis. 
+        
+        @rtype: C{(numpy.float, numpy.float)}
+        """
         return (self.min(), self.max())
 
 
@@ -70,12 +92,31 @@ class ContinuousAxis(Axis):
     This range can extend to infinity in either direction.
     """
     def __init__(self, name, low=-N.inf, high=N.inf):
+        """
+        @param name: The name for the axis
+        @type name: C{string}
+        @param low: The lower bound of the axis
+        @type low: C{numpy.float}
+        @param high: The upper bound of the axis
+        @type high: C{numpy.float}
+        
+        @note: If low > high, the behaviour of the class is undefined, though
+            this can be changed if a particular choice of bevaviour is clearly
+            useful.
+        
+        @precondition: C{name} must be an element of L{axis.valid}    
+        """
         self.low = low
         self.high = high
         Axis.__init__(self, name)
 
     def __eq__(self, other):
-        """ Equality is defined by name and range. """
+        """ Equality is defined by name and range. 
+        
+        @param other: The object to be compared with
+        @type other: L{ContinuousAxis}
+        @rtype: C{bool}        
+        """
         return self.range() == other.range() and \
                Axis.__eq__(self, other)
 
@@ -84,9 +125,17 @@ class ContinuousAxis(Axis):
         return self.low <= x < self.high
 
     def max(self):
+        """ The maximum value of the axis. 
+        
+        @rtype: C{numpy.float}        
+        """    
         return self.high
 
     def min(self):
+        """ The minimum value of the axis. 
+        
+        @rtype: C{numpy.float}
+        """    
         return self.low
 
             
@@ -104,13 +153,33 @@ class RegularAxis (Axis):
     """
 
     def __init__(self, name, length=N.inf, start=0, step=1):
+        """
+        Create a regularly spaced axis with a given name.
+        
+        @param name: The name for the axis
+        @type name: C{string}
+        @param length: The overall length of the axis
+        @type length: C{numpy.float}
+        @param start: The starting value of the axis
+        @type start: C{numpy.float}
+        @param step: The spacing of points on the axis
+        @type step: C{numpy.float}
+
+        @precondition: C{name} must be an element of L{axis.valid}
+        """
+
         self.length = length
         self.start = start
         self.step = step        
         Axis.__init__(self, name)
 
     def __eq__(self, other):        
-        """ Equality is defined by (start, stop, length) and name. """
+        """ Equality is defined by (start, stop, length) and name. 
+        
+        @param other: The object to be compared with
+        @type other: L{RegularAxis}
+        @rtype: C{bool}        
+        """
         return self.length == other.length and \
                self.start == other.start and \
                self.step == other.step and \
@@ -129,6 +198,8 @@ class RegularAxis (Axis):
         """
         Return all the values in the axis.
         Return a generator for the infinite case
+        
+        @rtype: C{numpy.ndarray(numpy.float)} or C{generator} of C{numpy.float}
         """
         if self.length == N.inf:
             def f(x):
@@ -140,9 +211,17 @@ class RegularAxis (Axis):
             return N.linspace(self.min(), self.max() + self.step, self.length, False)
 
     def max(self):
+        """ The maximum value of the axis. 
+        
+        @rtype: C{numpy.float}        
+        """    
         return self.start + self.step*(self.length - 1)
 
     def min(self):
+        """ The minimum value of the axis. 
+        
+        @rtype: C{numpy.float}        
+        """    
         return self.start
 
 class VoxelAxis (RegularAxis):
@@ -150,6 +229,16 @@ class VoxelAxis (RegularAxis):
     A RegularAxis which starts at 0 and has a step of 1.
     """
     def __init__(self, name, length=N.inf):
+        """
+        Create a voxel axis with a given name.
+        
+        @param name: The name for the axis
+        @type name: C{string}
+        @param length: The overall length of the axis
+        @type length: C{numpy.float}
+
+        @precondition: C{name} must be an element of L{axis.valid}
+        """    
         RegularAxis.__init__(self, name, length, start=0, step=1)
 
 
