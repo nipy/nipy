bc5cbc1b17e3c7738d03b725e25973eaca21a355
cindee.madison <>
2006-11-24 11:43:57 -0700

    updates to ecat2ana

--- lib/neuroimaging/sandbox/ecat/ecat2ana.py
+++ lib/neuroimaging/sandbox/ecat/ecat2ana.py
@@ -1,93 +1,82 @@
 import neuroimaging.data_io.formats.analyze as Ana
 import neuroimaging.data_io.formats.ecat7 as Ecat7
 import neuroimaging.data_io.formats.nifti1 as Nifti
+from neuroimaging.data_io.formats import utils
 import numpy as N
-from  neuroimaging.core.image.image import Image
-from neuroimaging.utils.tests.data import repository
 
 from neuroimaging.utils import wxmpl
 import matplotlib.cm as cm
 
-newfile = '/home/surge/cindeem/DEVEL/RAW_PET/B05_206-43D52D9100000211-de.v'
-myecat = Ecat7.Ecat7(newfile)
-#myecat = Ecat7.Ecat7("FDG-de.v.bz2",datasource=repository)
-anaPetFile = '/home/surge/cindeem/DEVEL/RAW_PET/B05_206-43D52D9100000211-de1.hdr'
-myAna = Ana.Analyze(anaPetFile)
-#myNifti =  Nifti.Nifti1
-#myNifti.intent = 
 
-# init check for input image and create generic outname if one is not provided
 
-# Allow for modification of header fields
-
-# allow for reorientation of image
-app = wxmpl.PlotApp('Ecat2Nifti')
-
-fig = app.get_figure()
-
-# Create an Axes on the Figure to plot in.
-# create 3 axis
-axes1 = fig.add_subplot(221)
-axes2 = fig.add_subplot(222)
-axes3 = fig.add_subplot(223)
-axes4 = fig.add_subplot(224)
-#axes = fig.gca()
-
-# get image
-jnk = myecat.frames[0]
-myImg = Image(jnk.data)
-myImgArray = myImg.readall()
-myImgArray2 = myImgArray.astype('float')/(myecat.scale * jnk.subheader['SCALE_FACTOR'])
-
-myAnaImg =Image(myAna.data)
-myAnaArray = myAnaImg.readall()
-# Plot the Image slice
-#axes1.imshow(myImgArray2[:,128,:]/N.float(myImgArray.max()),
-#             cmap=cm.gray)
-axes1.imshow(myImgArray2[:,128,:], cmap=cm.gray, origin='lower')
-
-axes1.set_aspect(5,adjustable='box',anchor='C')
-axes2.imshow(myImgArray2[:,:,128]/N.float(myImgArray.max()),
-             cmap=cm.gray, origin='lower')
-axes2.set_aspect(5,adjustable='box',anchor='C')
-axes3.imshow(myImgArray2[22,:,:],
-             cmap=cm.gray, origin='lower')
-axes3.set_aspect(1,adjustable='box',anchor='C')
-
-axes4.plot(myImgArray2[22,128,:])
-
-app.MainLoop()
-
-anafromecat = 
 
 
 class Ecat2Analyze(Ana.Analyze):
     """
     A class to make a new Analyze instance out of ECAT data
     """
-    def __init__(self, myecat, newfilename=myecat.data_file):
+    def __init__(self, myecat, newfilename,frame = 0):
         """
         initialize new header
         """
         
-    def header_from_given(self):
-        #note data type will ned to change to conform to ANALYZE options
+        tmpframe = myecat.frames[frame]
+        
+        #Ana.Analyze.__init__(self, newfilename, mode='r')
+        self.header_formats = Ana.struct_formats
+        Ana.Analyze.header_defaults()
+        self.header_from_given()
+
+        
+        self.grid = tmpframe.grid
+        self.dtype = tmpframe.data.dtype
+        self.byteorder = utils.NATIVE
+        self.dtype = self.dtype.newbyteorder(self.byteorder)
+
+        self.data = tmpframe.postread(tmpframe.data)
+        
+        
+        #Ana.Analyze.__init__(self, newfilename, mode='w')
+
+        
+
+        
+        
+    def header_from_given(self,myecat,tmpframe):
+        #note data type will need to change to conform to ANALYZE options
         # by default change to Ana.FLOAT
 
+        
+
         self.header['bitbix']=32
-        isotop = myecat.header.get('isotope_name')[0:9]
+        isotop = self.myecat.header.get('isotope_name')[0:9]
         self.header['data_type'] = isotop.replace('\x00','')
         self.header['datatype']=Ana.FLOAT
         self.header['descrip']='ecat2ana.py'
         
         self.header['dim'] = [3,
-                              myecat.data.shape[1],
-                              myecat.data.shape[2],
-                              myecat.data.shape[3]]
+                              self.myecat.data.shape[1],
+                              self.myecat.data.shape[2],
+                              self.myecat.data.shape[3]]
         #orientation
         self.header['orient']= self.get_orientation(myecat)
-        self.header['origin'] =
+        self.header['origin'] =[0.0 + tmpframe.subheader.get('X_OFFSET'),
+                                0.0 + tmpframe.subheader.get('Y_OFFSET'),
+                                0.0 + tmpframe.subheader.get('Z_OFFSET')]
+        self.header['pixdim'] = [1,tmpframe.subheader.get('X_PIXEL_SIZE'),
+                                 tmpframe.subheader.get('Y_PIXEL_SIZE'),
+                                 tmpframe.subheader.get('Z_PIXEL_SIZE'),
+                                 0.0,0.0,0.0,0.0]
+        self.header['scale_factor'] = 1.0
+        # will account for scale factor in acutal data
+        
+        
+                                 
 
+        
+
+        
+        
     def get_origin(self, myecat):
         """
         determin original origin from ecat
@@ -135,3 +124,10 @@ class Ecat2Analyze(Ana.Analyze):
         FFDL   AP RL IS -> Ana.orient = -1 (unknown)
         HFDL   PA RL SI -> Ana.orient = -1 (unknown)
         """
+
+
+newfile = '/home/surge/cindeem/DEVEL/RAW_PET/B05_206-43D52D9100000211-de.v'
+myecat = Ecat7.Ecat7(newfile)
+
+newfileA = '/home/surge/cindeem/DEVEL/TestData/newAna.hdr'
+newAna = Ecat2Analyze(myecat, newfileA,frame=0) 
