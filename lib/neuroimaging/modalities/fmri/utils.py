b5ad128c509a35aaa153290efc921940ff03b7b4
tim.leslie <>
2007-01-14 03:16:28 -0700

    cleanups as per #74

--- lib/neuroimaging/modalities/fmri/utils.py
+++ lib/neuroimaging/modalities/fmri/utils.py
@@ -23,7 +23,7 @@ class WaveFunction:
     def __init__(self, start, duration, height):
         self.start = start
         self.duration = duration
-        self.height = height
+        self.height = float(height)
 
     def __call__(self, time):
         return N.greater_equal(time, self.start) * N.less(time, self.start + self.duration) * self.height
@@ -35,19 +35,20 @@ def ConvolveFunctions(fn1, fn2, interval, dt, padding_f=0.1, normalize=(0, 0)):
     """
     Convolve fn1 with fn2 -- where fn1 may return a multidimensional output.
     """
-    
-    ltime = max(interval) - min(interval)
-    time = N.arange(min(interval), max(interval) + padding_f * ltime, dt)
+
+    max_interval, min_interval = max(interval), min(interval)
+    ltime = max_interval - min_interval
+    time = N.arange(min_interval, max_interval + padding_f * ltime, dt)
 
     _fn1 = N.array(fn1(time))
     _fn2 = N.array(fn2(time))
 
     if normalize[0]:
-        _fn1 = _fn1 / N.sqrt(N.add.reduce(_fn1**2))
+        _fn1 /= N.sqrt(N.add.reduce(_fn1**2))
     _fft1 = FFT.rfft(_fn1)
 
     if normalize[1]:
-        _fn2 = _fn2 / N.sqrt(N.add.reduce(_fn2**2))
+        _fn2 /= N.sqrt(N.add.reduce(_fn2**2))
     _fft2 = FFT.rfft(_fn2)
 
     value = FFT.irfft(_fft1 * _fft2)
@@ -58,11 +59,11 @@ def ConvolveFunctions(fn1, fn2, interval, dt, padding_f=0.1, normalize=(0, 0)):
     if len(value.shape) == 2:
         fns = []
         for i in range(value.shape[0]):
-            fns.append(LinearInterpolant(time + min(interval), value[i]))
+            fns.append(LinearInterpolant(time + min_interval, value[i]))
 
         return fns
     else:
-        newf = LinearInterpolant(time + min(interval), value)
+        newf = LinearInterpolant(time + min_interval, value)
         return newf
 
 class CutPoly:
