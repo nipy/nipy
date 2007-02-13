08be574435b49fd54710ae0da3e8b05766c80c8b
jarrod.millman <>
2007-02-13 05:32:52 -0700

    initial api.py attempt

---
+++ lib/neuroimaging/core/api.py
@@ -0,0 +1,11 @@
+"""
+Pseudo-package for all of the core symbols from the image object and its reference
+system.  Use this module for importing core names into your namespace. For example:
+ from neuorimaging.core.api import Image
+"""
+
+from neuroimaging.core.image.image import Image, ImageSequenceIterator
+from neuroimaging.core.reference.grid import SamplingGrid
+from neuroimaging.core.reference.iterators import SliceIterator, \
+     SliceParcelIterator, ParcelIterator, fMRIParcelterator
+from neuroimaging.core.reference.mapping import Mapping, Affine
