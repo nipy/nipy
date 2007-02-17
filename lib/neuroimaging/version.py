90bdca5bf319a8a91d3dd6dae52b994cd50b98dc
jonathan.taylor <>
2007-02-16 23:39:02 -0700

    instead of raising an exception, do if/else for version

--- lib/neuroimaging/version.py
+++ lib/neuroimaging/version.py
@@ -17,11 +17,11 @@ if not release:
     import neuroimaging
 
     nipy = sys.modules.get('neuroimaging')
-    try:
-        svn_entries = os.path.join(os.path.dirname(nipy.__file__),'.svn', 'entries')
+    svn_entries = os.path.join(os.path.dirname(nipy.__file__),'.svn', 'entries')
+    if os.path.exists(svn_entries):
         svn_doc = parse(open(svn_entries)).documentElement
         svn_version = _get_svn_version(svn_doc)
 
         version += '.dev'+svn_version
-    except IOError:
+    else:
         version += '.dev'+'_unknown_svn'
