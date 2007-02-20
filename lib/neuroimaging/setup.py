0960f64af0dfa4ba6393995f2d0d53e2b351f703
tim.leslie <>
2007-02-20 05:37:56 -0700

    more distutils stuff

---
+++ lib/neuroimaging/setup.py
@@ -0,0 +1,16 @@
+def configuration(parent_package='',top_path=None):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('neuroimaging', parent_package, top_path)
+
+    config.add_subpackage('algorithms')
+    config.add_subpackage('core')
+    config.add_subpackage('data_io')
+    config.add_subpackage('modalities')
+    config.add_subpackage('ui')
+    config.add_subpackage('utils')            
+
+    return config
+
+if __name__ == '__main__':
+    from numpy.distutils.core import setup
+    setup(**configuration(top_path='').todict())
