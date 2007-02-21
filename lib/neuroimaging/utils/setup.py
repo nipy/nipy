c9ad82a28484f284b14e5cfac82f372576a5544c
tim.leslie <>
2007-02-21 08:03:22 -0700

    clean up

--- lib/neuroimaging/utils/setup.py
+++ lib/neuroimaging/utils/setup.py
@@ -1,18 +1,15 @@
 from os.path import join
 from neuroimaging import ENTHOUGHT_TRAITS_DEF
 
-
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('utils', parent_package, top_path)
 
     config.add_data_dir('tests')
 
-
     config.add_subpackage('config')
 
     if not ENTHOUGHT_TRAITS_DEF:
-        print "yo"
         config.add_subpackage('enthought')
         config.add_extension('enthought.traits.ctraits',
           [join(*('enthought/traits/ctraits.c'.split('/')))])
