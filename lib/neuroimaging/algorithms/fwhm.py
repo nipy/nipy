65c89f84060edf7d0fd363fe3eaa8ced8742a88b
jonathan.taylor <>
2007-02-26 12:51:24 -0700

    adding documentation to fwhm module
    removed some unnecessary clutter and arguments

--- lib/neuroimaging/algorithms/fwhm.py
+++ lib/neuroimaging/algorithms/fwhm.py
@@ -5,6 +5,13 @@ to determine resolution elements (resels).
 
 A resolution element (resel) is defined as a block of pixels of the same
 size as the FWHM of the smoothed image.
+
+There are two methods implemented to estimate (3d, or volumewise) FWHM
+based on a 4d Image:
+
+    fastFHWM: used if the entire 4d Image is available
+    iterFWHM: used when 4d Image is being filled in by slices of residuals
+    
 """
 
 __docformat__ = 'restructuredtext'
@@ -23,20 +30,22 @@ class Resels(object):
                  mask=None, clobber=False, D=3):
         """
         :Parameters:
-            grid : TODO
-                TODO
-            normalized : ``bool``
-                TODO
-            fwhm : TODO
-                TODO
-            resels : TODO
-                TODO
-            mask : TODO
-                TODO
+            grid : ``SamplingGrid``
+                 SamplingGrid over which fwhm and resels are to be estimated.                
+                 Used in fwhm/resel conversion.
+            fwhm : ``Image``
+                Optional Image of FWHM. Used to convert
+                FWHM Image to resels if FWHM is not being estimated.
+            resels : ``Image``
+                Optional Image of resels. Used to 
+                compute resels within a mask, for instance, if
+                FWHM has already been estimated.
+            mask : ``Image``
+                Mask over which to integrate resels.
             clobber : ``bool``
-                TODO
+                Clobber output FWHM and resel images?
             D : ``int``
-                TODO
+                Can be 2 or 3, the dimension of the final volume.
         """
         self.fwhm = fwhm
         self.resels = resels
@@ -47,23 +56,26 @@ class Resels(object):
 
         _transform = self.grid.mapping.transform
         self.wedge = N.power(N.fabs(det(_transform)), 1./self.D)
-        self.normalized = normalized
 
     def integrate(self, mask=None):
         """
         :Parameters:
-            mask : TODO
-                TODO
+            mask : ``Image``
+                  Optional mask over which to integrate (add) resels.
         
-        :Returns: TODO
+        :Returns: (total_resels, FWHM, nvoxel)
+
+                  total_resels: the resels contained in the mask
+                  FWHM: an estimate of FWHM based on the average resel per voxel
+                  nvoxel: the number of voxels in the mask 
         """
-        _resels = self.resels.readall()
+        _resels = self.resels[:]
         if mask is not None:
             _mask = mask
         else:
             _mask = self.mask
         if _mask is not None:
-            _mask = _mask.readall().astype(N.int32)
+            _mask = _mask[:].astype(N.int32)
             nvoxel = _mask.sum()
         else:
             _mask = 1.
@@ -72,23 +84,26 @@ class Resels(object):
         _fwhm = self.resel2fwhm(_resels / nvoxel)
         return _resels, _fwhm, nvoxel
 
-    def resel2fwhm(self, x):
+    def resel2fwhm(self, resels):
         """
         :Parameters:
-            x : TODO
-                TODO
+            resels : ``float``
+                Convert a resel value to an equivalent isotropic FWHM based on
+                step sizes in self.grid.
         
-        :Returns: TODO
+        :Returns: FWHM
         """
         return N.sqrt(4*N.log(2.)) * self.wedge * recipr(N.power(x, 1./self.D))
 
-    def fwhm2resel(self, x):
+    def fwhm2resel(self, fwhm):
         """
         :Parameters:
-            x : TODO
-                TODO
+            fwhm : ``float``
+                Convert an FWHM value to an equivalent resels per voxel based on
+                step sizes in self.grid.
 
-        :Returns: TODO
+
+        :Returns: resels
         """
         return recipr(N.power(x / N.sqrt(4*N.log(2)) * self.wedge, self.D))
 
@@ -101,14 +116,14 @@ class Resels(object):
         else:
             im = \
               Image(self.fwhm, clobber=self.clobber, mode='w', grid=self.grid)
-        self.fwhm = iter(im)
+        self.fwhm = im
 
         if not self.resels:
             im = Image(N.zeros(self.grid.shape), grid=self.grid)
         else:
             im = \
               Image(self.resels, clobber=self.clobber, mode='w', grid=self.grid)
-        self.resels = iter(im)
+        self.resels = im
 
         return self
 
@@ -117,66 +132,72 @@ class ReselImage(Resels):
     def __init__(self, resels=None, fwhm=None, **keywords):
         """
         :Parameters:
-            resels : TODO
-                TODO
-            fwhm : TODO
-                TODO
+            resels : `core.api.Image`
+                Image of resel per voxel values. 
+            fwhm : `core.api.Image`
+                Image of FWHM values.
             keywords : ``dict``
                 Passed as keywords arguments to `core.api.Image`
         """
         if not resels and not fwhm:
             raise ValueError, 'need either a resels image or an FWHM image'
 
-        if fwhm:
+        if fwhm is not None:
             fwhm = Image(fwhm, **keywords)
             Resels.__init__(self, fwhm, resels=resels, fwhm=fwhm)
 
-        if resels:
+        if resels is not None:
             resels = Image(resels, **keywords)
             Resels.__init__(self, resels, resels=resels, fwhm=fwhm)
 
         if not self.fwhm:
-            self.fwhm = Image(self.resel2fwhm(self.resels.readall()),
-                              mapping=self.resels.grid.mapping, **keywords)
+            self.fwhm = Image(self.resel2fwhm(self.resels[:]),
+                              grid=self.grid, **keywords)
 
         if not self.resels:
-            self.resels = Image(self.fwhm2resel(self.fwhm.readall()),
-                                mapping=self.fwhm.grid.mapping, **keywords)
+            self.resels = Image(self.fwhm2resel(self.fwhm[:]),
+                                grid=self.grid, **keywords)
 
     def __iter__(self): 
         """
-        :Returns: ``None`` FIXME: should this be ``self``?
+        :Returns: ``self``
         """
-        return
+        return self
 
 class iterFWHM(Resels):
     """
     Estimate FWHM on an image of residuals sequentially. This is handy when,
     say, residuals from a linear model are written out slice-by-slice.
+
+    Resulting FWHM is clipped at self.FWHMmax, which defaults to 50.
     """
 
-    def __init__(self, grid, df_resid=5.0, df_limit=4.0, mask=None, **keywords):
+    FWHMmax=50.
+
+    def __init__(self, grid, normalized=False, df_resid=5.0, mask=None, **keywords):
         """Setup a FWHM estimator.
         
         :Parameters:
-            grid : TODO
-                TODO
+            grid : ``SamplingGrid``
+                 SamplingGrid over which fwhm and resels are to be estimated.                
+                 Used in fwhm/resel conversion.
+            normalized : ``bool``
+                Are residuals normalized to have length 1? If False, residuals
+                are normalized before estimating FWHM.
             df_resid : ``float``
-                TODO
-            df_limit : ``float``
-                TODO
-            mask : TODO
-                TODO
+                How many degrees of freedom are there in the residuals?
+                Must be greater than self.D + 1.
+            mask : ``Image``
+                  Optional mask over which to integrate (add) resels.
             keywords : ``dict``
                 Passed as keyword parameters to `Resels.__init__`
         """
 
         Resels.__init__(self, grid, mask=mask, **keywords)
-        self.df_resid = df_resid
-        self.df_limit = df_limit
+        self.normalized = normalized
         self.Y = grid.shape[1]
         self.X = grid.shape[2]
-        self.setup_nneigh()
+        self._setup_nneigh()
         self._fwhm = N.zeros((self.Y, self.X))
         self._resels = N.zeros((self.Y, self.X))
         self.Yindex = N.arange(self.Y)
@@ -190,16 +211,20 @@ class iterFWHM(Resels):
         self.slices = []
         self.nslices = grid.shape[0]
         iter(self)
-
+        if df_resid <= self.D + 1:
+            raise ValueError, 'insufficient residual degrees of freedom to estimate FWHM'
+        
     def __call__(self, resid):
         """
+        Estimate FWHM and resels per voxel.
+
         :Parameters:
-            resid : TODO
-                TODO
+            resid : ``Image``
+                Image of residuals used for estimating FWHM and resels per voxel.
         
         :Returns: ``None``
         """
-        resid = iter(resid)
+        resid = resid.slice_iterator()
         iter(self)
         while True:
             try:
@@ -210,11 +235,12 @@ class iterFWHM(Resels):
 
     def normalize(self, resid):
         """
+        Normalize residuals subtracting mean, and fixing length to 1.
+
         :Parameters:
-            resid : TODO
-                TODO
+            resid : Array of residuals.
                 
-        :Returns: TODO
+        :Returns: Normalized residuals.
         """
         _mu = 0.
         _sumsq = 0.
@@ -258,8 +284,10 @@ class iterFWHM(Resels):
         self.slices = []
         return self
 
-    def setup_nneigh(self):
+    def _setup_nneigh(self):
         """
+        Setup the number of neighbours within a slice of size self.Y, self.X.
+
         :Returns: ``None``
         """
         self.nneigh = 4. * N.ones((self.Y, self.X))
@@ -272,13 +300,13 @@ class iterFWHM(Resels):
         self.nneigh[-1, -1] = 1
         self.nneigh[-1, 0] = 1
 
-    def set_next(self, data, FWHMmax=50.):
+    def set_next(self, resid):
         """
+        Pass a slice of residuals into slicewise estimate of FWHM.
+
         :Parameters:
-            data : TODO
-                TODO
-            FWHMmax : ``float``
-                TODO
+            resid : ``array``
+                 slice of residuals
         
         :Returns: ``None``
         """
@@ -305,7 +333,7 @@ class iterFWHM(Resels):
                     axy = N.add.reduce(self.uy[:, xslice] *
                                        self.ux[yslice, :], 2)
                     axy *= self.Yweight[index] * self.Xweight[index]
-                    detlam = calc_detlam(axx, ayy, 1, ayx, 0, 0)
+                    detlam = _calc_detlam(axx, ayy, 1, ayx, 0, 0)
                     test = N.greater(detlam, 0).astype(N.float64)
                     _resels = N.sqrt(test * detlam)
                     self._resels[yslice, xslice] += _resels
@@ -324,17 +352,17 @@ class iterFWHM(Resels):
                 axx = self.Axx[yslice, :]
                 ayy = self.Ayy[:, xslice]
                 azz = self.Azz[yslice, xslice]
-                if self.df_resid > self.df_limit:
-                    xx = self.ux[yslice: ]
-                    yy = self.uy[:, xslice]
-                    zz = self.uz[yslice, xslice]
-                    ayx = N.add.reduce(yy * xx, 2)
-                    azx = N.add.reduce(xx * zz, 2)
-                    azy = N.add.reduce(yy * zz, 2)
-                    ayx *= self.Yweight[index] * self.Xweight[index]
-                    azx *= self.Yweight[index]
-                    azy *= self.Xweight[index]
-                    detlam = calc_detlam(axx, ayy, azz, ayx, azx, azy)
+
+                xx = self.ux[yslice: ]
+                yy = self.uy[:, xslice]
+                zz = self.uz[yslice, xslice]
+                ayx = N.add.reduce(yy * xx, 2)
+                azx = N.add.reduce(xx * zz, 2)
+                azy = N.add.reduce(yy * zz, 2)
+                ayx *= self.Yweight[index] * self.Xweight[index]
+                azx *= self.Yweight[index]
+                azy *= self.Xweight[index]
+                detlam = _calc_detlam(axx, ayy, azz, ayx, azx, azy)
 
                 test = N.greater(detlam, 0).astype(N.float64)
                 _resels = N.sqrt(test * detlam)
@@ -344,8 +372,6 @@ class iterFWHM(Resels):
             # Get slice ready for output
             self._fwhm /= ((self.slice > 1) + 1.) * self.nneigh
             self._resels /= ((self.slice > 1) + 1.) * self.nneigh
-
-            self.output(FWHMmax)
 
             # Clear buffers
 
@@ -367,18 +393,19 @@ class iterFWHM(Resels):
                 axx = self.Axx[xslice, :]
                 ayy = self.Ayy[:, yslice]
                 azz = self.Azz[xslice, yslice]
-                if self.df_resid > self.df_limit:
-                    xx = self.ux[xslice, :]
-                    yy = self.uy[:, yslice]
-                    zz = self.uz[xslice, yslice]
-                    ayx = N.add.reduce(yy * xx, 2)
-                    azx = N.add.reduce(xx * zz, 2)
-                    azy = N.add.reduce(yy * zz, 2)
 
-                    ayx *= self.Yweight[index] * self.Xweight[index]
-                    azx *= self.Yweight[index]
-                    azy *= self.Xweight[index]
-                    detlam = calc_detlam(axx, ayy, azz, ayx, azx, azy)
+                xx = self.ux[xslice, :]
+                yy = self.uy[:, yslice]
+                zz = self.uz[xslice, yslice]
+                ayx = N.add.reduce(yy * xx, 2)
+                azx = N.add.reduce(xx * zz, 2)
+                azy = N.add.reduce(yy * zz, 2)
+                
+                ayx *= self.Yweight[index] * self.Xweight[index]
+                azx *= self.Yweight[index]
+                azy *= self.Xweight[index]
+                detlam = _calc_detlam(axx, ayy, azz, ayx, azx, azy)
+
                 test = N.greater(N.fabs(detlam), 0).astype(N.float64)
                 _resels = N.sqrt(test * detlam)
                 self._resels[xslice, yslice] += _resels
@@ -392,31 +419,39 @@ class iterFWHM(Resels):
         
         self.slice += 1
 
-    def output(self, FWHMmax=50.):
+    def output(self):
         """
-        :Parameters:
-            FWHMmax : ``float``
-                TODO
-                
         :Returns: ``None``
         """
         value = self.grid.next()
 
-        self.fwhm[value.slice] =  N.clip(self._fwhm, 0, FWHMmax)
-        self.resels[value.slice] = N.clip(self._resels, 0, FWHMmax)
+        self.fwhm[value.slice] =  N.clip(self._fwhm, 0, self.FWHMmax)
+        self.resels[value.slice] = N.clip(self._resels, 0, self.FWHMmax)
 
 class fastFWHM(Resels):
-    """
-    Given a 4d image of residuals, i.e. not one filled in step by step
-    by an iterator, estimate fwhm and resels.
-    """
 
-    def __init__(self, rimage, **keywords):
-        Resels.__init__(self, rimage.grid.subgrid(0), **keywords)
-        self.n = rimage.grid.shape[0]
-        self.rimage = Image(rimage)
+    def __init__(self, resid, **keywords):
+        """
+        Given a 4d image of residuals, i.e. not one filled in step by step
+        by an iterator, estimate FWHM and resels.
 
-    def __call__(self, verbose=False):
+        :Parameters:
+            resid : ``array``
+                 Image of residuals used to estimate FWHM and resels per voxel.
+        
+        :Returns: ``None``
+        """
+
+        Resels.__init__(self, resid.grid.subgrid(0), **keywords)
+        self.n = resid.grid.shape[0]
+        self.resid = Image(resid)
+
+    def __call__(self):
+        """
+        Estimate FWHM and resels per voxel.
+        
+        :Returns: ``None``
+        """
 
         Resels.__iter__(self)
 
@@ -425,8 +460,6 @@ class fastFWHM(Resels):
 
         if not self.normalized:
             for i in range(self.n):
-                if verbose:
-                    print '(Normalizing) Frame: [%d]' % i
                 _frame = self.rimage[slice(i, i+1)]
                 _mu += _frame
                 _sumsq += _frame**2
@@ -461,20 +494,26 @@ class fastFWHM(Resels):
 
             del(dz); del(dy); del(dx); del(g) ; gc.collect()
             
-
-
-        detlam = calc_detlam(Lxx, Lyy, Lzz, Lyx, Lzx, Lzy)
+        detlam = _calc_detlam(Lxx, Lyy, Lzz, Lyx, Lzx, Lzy)
 
         test = N.greater(detlam, 0)
         resels = N.sqrt(detlam * test)
         fwhm = self.resel2fwhm(resels)
 
         fullslice = [slice(0, x) for x in self.grid.shape]
-        self.resels[fullslice] = resels
-        self.fwhm[fullslice] = fwhm
+        self.resels[:] = resels
+        self.fwhm[:] = fwhm
 
 
-def calc_detlam(xx, yy, zz, yx, zx, zy):
+def _calc_detlam(xx, yy, zz, yx, zx, zy):
+    """
+    Calculate determinant of symmetric 3x3 matrix
+
+    [[xx,yx,xz],
+     [yx,yy,zy],
+     [zx,zy,zz]]
+     """
+    
     return zz * (yy*xx - yx**2) - \
            zy * (zy*xx - zx*yx) + \
            zx * (zy*yx - zx*yy)
