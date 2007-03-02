7372a5f94b03e7dd0647f04f342fcaf46815a30e
tim.leslie <>
2007-03-02 03:04:14 -0700

    clean up defines.py

--- lib/neuroimaging/defines.py
+++ lib/neuroimaging/defines.py
@@ -9,40 +9,21 @@ def pylab_def():
     """
     Check to see if pylab/matplotlib is importable.
     """
-#    global PYLAB_DEF
-#    global pylab
     try:
         import pylab
     except (ImportError, RuntimeError):
         PYLAB_DEF = False
         pylab = None
-        pass
     else:
         PYLAB_DEF = True
 
     return PYLAB_DEF, pylab
 
-#def qt_def():
-#    """
-#    Check to see if qt is importable.
-#    """
-#    global QT_DEF
-#    global qt
-#    try:
-#        import qt
-#        QT_DEF = True
-#    except:
-#        QT_DEF = False
-#        qt = None
-#        pass
-#    return QT_DEF, qt
 
 def enthought_traits_def():
     """
     Check to see if enthought.traits is importable.
     """
-#    global ENTHOUGHT_TRAITS_DEF
-#    global traits
     try:
         import enthought.traits.api as traits
     except ImportError:
@@ -51,7 +32,6 @@ def enthought_traits_def():
         except ImportError:
             ENTHOUGHT_TRAITS_DEF = False
             traits = None
-            pass
         else:
             ENTHOUGHT_TRAITS_DEF = True
     else:
