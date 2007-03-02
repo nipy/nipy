0482fbff291fbcec6a1c9580b338ae6302bedb14
tim.leslie <>
2007-03-02 14:52:04 -0700

    fill in algorithms.__init__.py

--- lib/neuroimaging/algorithms/__init__.py
+++ lib/neuroimaging/algorithms/__init__.py
@@ -3,3 +3,10 @@ Package containing generic algorithms such as registration, statistics,
 simulation, etc.
 """
 __docformat__ = 'restructuredtext'
+
+import statistics
+import fwhm, interpolation, kernel_smooth, onesample, regression
+
+def test(level=1, verbosity=1):
+    from numpy.testing import NumpyTest
+    return NumpyTest().test(level, verbosity)
