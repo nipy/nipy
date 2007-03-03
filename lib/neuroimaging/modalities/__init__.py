f80b27bb25124725c968e12b0ea7d2e88eed1e1d
tim.leslie <>
2007-03-03 00:32:44 -0700

    add test() functions

--- lib/neuroimaging/modalities/__init__.py
+++ lib/neuroimaging/modalities/__init__.py
@@ -2,3 +2,7 @@
 Package containing modality-specific classes.
 """
 __docformat__ = 'restructuredtext'
+
+def test(level=1, verbosity=1):
+    from numpy.testing import NumpyTest
+    return NumpyTest().test(level, verbosity)
