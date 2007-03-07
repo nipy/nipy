d68812a7aa79733cff4054d2a522c1ed3b386d26
tim.leslie <>
2007-03-06 21:29:56 -0700

    use io.data_path

--- examples/FIAC/scripts/fixed_run.py
+++ examples/FIAC/scripts/fixed_run.py
@@ -10,15 +10,14 @@ if __name__ == "__main__":
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
     import fixed, io
-    io.data_path = '/home/analysis/FIAC'
 
     if len(sys.argv) == 2:
         subject = int(sys.argv[1])
     else:
-        subject = 3
+        subject = 1
 
-    os.system("rm /home/analysis/FIAC/fixed/*/*/*/fiac%d/*bz2" % subject)
+    os.system("rm %s/fixed/*/*/*/fiac%d/*bz2" % (io.data_path, subject))
 
     fixed.run(subj=subject)
 
-    os.system("bzip2 /home/analysis/FIAC/fixed/*/*/*/fiac%d/*nii" % subject)
+    os.system("bzip2 %s/fixed/*/*/*/fiac%d/*nii" % (io.data_path, subject))
