5654920dc9563ddb0281d65074eba653c1cedb44
tim.leslie <>
2007-03-08 14:17:40 -0700

    use io.data_path

--- examples/FIAC/scripts/model_run.py
+++ examples/FIAC/scripts/model_run.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
     import sys, os
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-    import model
+    import model, io
 
     if len(sys.argv) == 3:
         subj, run = map(int, sys.argv[1:])
     else:
-        subj, run = (3, 3)
+        subj, run = (1, 2)
 
     model.run(subj=subj, run=run)
-    os.system("bzip2 /home/analysis/FIAC/fiac%d/fonc%d/fsl/fmristat_run/*/*/*nii" % (subj, run))
+    os.system("bzip2 %s/fiac%d/fonc%d/fsl/fmristat_run/*/*/*nii" % (io.data_path, subj, run))
