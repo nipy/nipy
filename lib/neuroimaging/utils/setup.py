4b04133e70b2c6230480a77d34c50fc67a717fb3
tim.leslie <>
2007-03-11 04:36:33 -0700

    unzip data files before installing them. This addresses #79

--- lib/neuroimaging/utils/setup.py
+++ lib/neuroimaging/utils/setup.py
@@ -5,7 +5,20 @@ def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('utils', parent_package, top_path)
 
+    # We want to unzip our data here where we have permissions
+    # before moving it into the install location.
+    from data_io.datasource import unzip
+    import os
+    path = "lib/neuroimaging/utils/tests/data"
+    for filename in os.listdir(path):
+        if filename.endswith('bz2'):
+            filename = os.path.join(path, filename)
+            unzip(filename)
+            os.remove(filename)
+
+
     config.add_data_dir('tests')
+
 
     config.add_subpackage('config')
 
