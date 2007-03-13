4aed050e24ff3347c7342f723925976bbe046120
tim.leslie <>
2007-03-12 19:58:47 -0700

    clean up data_io.api

--- lib/neuroimaging/data_io/api.py
+++ lib/neuroimaging/data_io/api.py
@@ -2,4 +2,7 @@ from datasource import DataSource, iswritemode, iszip, unzip, splitzipext, \
      Repository, Cache, ensuredirs
 from formats.format import getformats, Format
 from formats.analyze import Analyze
-import formats.nifti1 as nifti1
+from formats.nifti1 import Nifti1
+from formats.ecat7 import Ecat7
+from formats.afni import AFNI
+
