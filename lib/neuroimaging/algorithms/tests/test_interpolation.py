30d76aebcc6827c3b577d7c413adf537d97ee46f
tim.leslie <>
2007-03-14 21:16:05 -0700

    add test files for more modules

---
+++ lib/neuroimaging/algorithms/tests/test_interpolation.py
@@ -0,0 +1,5 @@
+from neuroimaging.utils.testutils import make_doctest_suite
+test_suite = make_doctest_suite('neuroimaging.algorithms.interpolation')
+        
+if __name__ == '__main__':
+    NumpyTest.run()
