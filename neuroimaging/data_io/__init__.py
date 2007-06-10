64a8c7fbee32f557576ec18daa18a2760b90d282
brian.hawthorne <>
2007-06-09 21:04:12 -0700

    remove unnecessary imports and extra test declarations

--- neuroimaging/data_io/__init__.py
+++ neuroimaging/data_io/__init__.py
@@ -2,22 +2,4 @@
 Package contains generic functions for data input/output. This includes
 methods for accessing file systems and network resources.
 """
-
 __docformat__ = 'restructuredtext'
-
-
-__all__ = ["formats", "datasource"]
-
-import formats
-#, datasource
-
-def test(level=1, verbosity=1, flags=[]):
-    from neuroimaging.utils.testutils import set_flags
-    set_flags(flags)
-    from numpy.testing import NumpyTest
-    return NumpyTest().test(level, verbosity)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
--- neuroimaging/data_io/formats/__init__.py
+++
@@ -1,8 +0,0 @@
-import binary, format, utils
-import afni, nifti1, analyze, ecat7
-
-def test(level=1, verbosity=1, flags=[]):
-    from neuroimaging.utils.testutils import set_flags
-    set_flags(flags)
-    from numpy.testing import NumpyTest
-    return NumpyTest().test(level, verbosity)
