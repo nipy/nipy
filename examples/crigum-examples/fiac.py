2f1fd4ce5a1020d913934547ec96c71575e7266e
jonathan.taylor <>
2008-02-04 01:46:44 -0700

    fixed paths to be URLs so it (might) execute on other computers

--- examples/crigum-examples/fiac.py
+++ examples/crigum-examples/fiac.py
@@ -5,5 +5,11 @@ prefix = "http://kff.stanford.edu"
 anat = Image("%s/FIAC/avganat.img" % prefix)
 avg152 = Image("%s/FIAC/avg152T1_brain.img" % prefix)
 fmri = Image("%s/FIAC/fiac3/fonc3/fsl/filtered_func_data.img" % prefix)
-#sentence = Image("sentence.nii")
-#average = Image("average.nii")
+
+
+# subject 3's sentence and overall 
+
+prefix = "http://www-stat.stanford.edu/~jtaylo/matthew"
+sentence = Image("%s/fixed-spm/block/contrasts/sentence/fiac3/t.nii" % prefix)
+average = Image("%s/fixed-spm/block/contrasts/average/fiac3/t.nii" % prefix)
+
