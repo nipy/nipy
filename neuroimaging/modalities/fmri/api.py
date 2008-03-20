1c85f99178c8786cb6e991f0afa1e160133627bf
Jonathan Taylor <jetaylor74@gmail.com>
2008-03-20 05:05:41 -0700

    changes to fmri reflecting ImageList class

--- neuroimaging/modalities/fmri/api.py
+++ neuroimaging/modalities/fmri/api.py
@@ -1,5 +1,5 @@
-from fmri import FmriImage, slice_iterator, parcel_iterator, slice_parcel_iterator, fromarray
+from fmri import FmriImage, parcel_iterator, slice_parcel_iterator, fromimage
 
-from fmri import load as load_image
-from neuroimaging.core.api import save_image
+#from fmri import load as load_image
+#from neuroimaging.core.api import save_image
 
