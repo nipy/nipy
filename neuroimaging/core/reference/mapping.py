9498e77f1756597f8cd8ded2c8987a5bdf6ef166
Jonathan Taylor <jtaylo@kff>
2008-03-28 15:54:43 -0700

    numpy: N -> np switch

--- neuroimaging/core/reference/mapping.py
+++ neuroimaging/core/reference/mapping.py
@@ -11,7 +11,7 @@ import csv, copy
 import urllib
 from struct import unpack
 
-import numpy as N
+import numpy as np
 from numpy.linalg import inv
 
 def _2matvec(transform):
@@ -25,7 +25,7 @@ def _2matvec(transform):
 def _2transform(matrix, vector):
     """ Combine a matrix and vector into a transform. """
     nin, nout = matrix.shape
-    t = N.zeros((nin+1,nout+1))
+    t = np.zeros((nin+1,nout+1))
     t[0:nin, 0:nout] = matrix
     t[nin,   nout] = 1.
     t[0:nin, nout] = vector
@@ -38,7 +38,7 @@ def permutation_matrix(order=range(3)[2::-1]):
     0, ..., N-1.
     """
     n = len(order)
-    matrix = N.zeros((n, n))
+    matrix = np.zeros((n, n))
     if set(order) != set(range(n)):
         raise ValueError('order should be a sequence of integers with' \
                          'values, 0 ... len(order)-1.')
@@ -53,7 +53,7 @@ def permutation_transform(order=range(3)[2::-1]):
     containing the values 0,...,N-1.
     """
     matrix = permutation_matrix(order=order)
-    vector = N.zeros(len(order))
+    vector = np.zeros(len(order))
     return _2transform(matrix, vector)
 
 
@@ -61,7 +61,7 @@ def translation_transform(x, ndim):
     """
     Create an affine transformation matrix representing translation by x.
     """
-    return _2transform(N.identity(ndim), x)
+    return _2transform(np.identity(ndim), x)
     
 
 
@@ -78,9 +78,9 @@ def isdiagonal(matrix):
     """
 
     ndim = matrix.shape[0]
-    mask = ~N.identity(ndim, dtype=bool)
-    masked = N.ma.array(matrix, mask=mask, fill_value=0.).filled()
-    return N.all(masked == matrix)
+    mask = ~np.identity(ndim, dtype=bool)
+    masked = np.ma.array(matrix, mask=mask, fill_value=0.).filled()
+    return np.all(masked == matrix)
 
 
 class Mapping(object):
@@ -105,8 +105,7 @@ class Mapping(object):
         self._map = map
         self._inverse = inverse
         self.name = name
-        self._ndim = ndim
-        
+        self.ndim = ndim
         
     def __call__(self, x):
         """ Apply this mapping to the given coordinates. """
@@ -159,13 +158,6 @@ class Mapping(object):
             inverse = None
         return Mapping(map, inverse=inverse)
 
-    def ndim(self):
-        """ The number of input dimensions
-
-        :Returns: ``int``
-        """
-        return self._ndim
-
     def isinvertible(self):
         """
         Does this mapping have an inverse?
@@ -191,14 +183,14 @@ class Mapping(object):
         voxels, return the closest voxel for reaf. Will choke if mapping is
         not invertible.
 
-        :Raises N.linalg.LinAlgError: is mapping is not invertible.
+        :Raises np.linalg.LinAlgError: is mapping is not invertible.
         """
         shape = real.shape
         if len(shape) > 1:
-            real.shape = (shape[0], N.product(shape[1:]))
-        voxel = N.around(self.inverse()(real))
+            real.shape = (shape[0], np.product(shape[1:]))
+        voxel = np.around(self.inverse()(real))
         voxel.shape = shape
-        return N.array(voxel)
+        return np.array(voxel)
 
     def _preslice_mapping(self, index, gshape):
         """
@@ -240,7 +232,7 @@ class Mapping(object):
                     x = stop - start
                 except:
                     raise ValueError, `stop` + ' ' + `start` + ' ' + `i` + ' ' + `type(i)` + ' ' + `i.stop`
-                shape.append(int((stop - start) / step))
+                shape.append(int(np.ceil((stop - start) / (step * 1.))))
                 sstarts.append(start)
                 maps.append(lambda x: x * step + start)
                 ssteps.append(step)
@@ -284,10 +276,10 @@ class Mapping(object):
 
     def _f(self, x):
         """ helper function for `matlab2python` and `python2matlab` """
-        ndim = self.ndim()
+        ndim = self.ndim
         mat = permutation_matrix(range(ndim)[::-1])
         t1 = _2transform(mat, x)
-        t2 = _2transform(mat, N.zeros(ndim))
+        t2 = _2transform(mat, np.zeros(ndim))
         w1 = Affine(t1)
         w2 = Affine(t2)
         return (w2 * self) * w1
@@ -300,12 +292,12 @@ class Mapping(object):
         mapc = copy.deepcopy(self._map)
         def mapping(x):
             y = x.copy()
-            o = N.ones(x.shape)
+            o = np.ones(x.shape)
             for i in range(nvar):
                 y[i] = maps[i](y[i])
-            for i in range(self.ndim()):
+            for i in range(self.ndim):
                 if i not in varcoords:
-                    o = np.vstack([o, np.ones(x.shape) * N.ones[i]])
+                    o = np.vstack([o, np.ones(x.shape) * np.ones[i]])
                 else:
                     o = np.vstack([o, y[i]])
 
@@ -327,7 +319,7 @@ class Affine(Mapping):
 
         :Returns: `Affine`
         """
-        return Affine(N.identity(ndim+1))
+        return Affine(np.identity(ndim+1))
 
 
     def _slice_mapping(self, index, gshape):
@@ -335,14 +327,14 @@ class Affine(Mapping):
         Return an Affine instance
         """
         varcoords, _, start, step, shape = self._preslice_mapping(index, gshape)
-        stepmatrix = N.diag(list(step) + [1])
-        startvector = N.dot(self.transform, N.array(list(start)+[1]))[:-1]
+        stepmatrix = np.diag(list(step) + [1])
+        startvector = np.dot(self.transform, np.array(list(start)+[1]))[:-1]
 
-        tmatrix = N.zeros((len(gshape) + 1, len(varcoords) + 1))
+        tmatrix = np.zeros((len(gshape) + 1, len(varcoords) + 1))
         for i, j in enumerate(varcoords):
             tmatrix[:-1,i] = self.transform[:-1,j]
         tmatrix[-1,-1] = 1.
-        tmatrix = N.dot(tmatrix, stepmatrix)
+        tmatrix = np.dot(tmatrix, stepmatrix)
         tmatrix[:-1,-1] = startvector
         return varcoords, Affine(tmatrix), shape
 
@@ -368,8 +360,8 @@ class Affine(Mapping):
         
         :Returns: ``numpy.ndarray``
         """
-        value = N.dot(self._fmatrix, coords) 
-        value += N.multiply.outer(self._fvector, N.ones(value.shape[1:]))
+        value = np.dot(self._fmatrix, coords) 
+        value += np.multiply.outer(self._fvector, np.ones(value.shape[1:]))
         return value
 
     def __eq__(self, other):
@@ -384,7 +376,7 @@ class Affine(Mapping):
         """
         if not hasattr(other, "transform"): 
             return False
-        return N.all(N.asarray(self.transform) == N.asarray(other.transform)) \
+        return np.all(np.asarray(self.transform) == np.asarray(other.transform)) \
                and self.name == other.name
 
 
@@ -397,7 +389,7 @@ class Affine(Mapping):
         :Returns: `Mapping` or `Affine`
         """
         if isinstance(other, Affine):
-            return Affine(N.dot(other.transform, self.transform))            
+            return Affine(np.dot(other.transform, self.transform))            
         else: 
             return Mapping.__rmul__(self, other)
 
