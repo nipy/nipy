85809e34f1d8ad3d8736e141f3cb2e6045260938
Christopher Burns <cburns@berkeley.edu>
2008-07-06 22:04:48 -0700

    DOC: Add doc for pynifti package.

--- neuroimaging/externals/pynifti/nifti/__init__.py
+++ neuroimaging/externals/pynifti/nifti/__init__.py
@@ -1,2 +1,11 @@
+"""
+Nifti
+=====
+
+    Python bindings for the nifticlibs.  Access through the NiftiImage class.
+
+    See help for pyniftiio.nifti.NiftiImage
+
+"""
 
 from niftiimage import NiftiImage
