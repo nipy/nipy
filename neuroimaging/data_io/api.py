901457f5d442f2cc1c8e8c7d4d90142758d9f9a7
Christopher Burns <cburns@berkeley.edu>
2008-07-29 14:58:44 -0700

    Update package modules for io.

--- neuroimaging/data_io/api.py
+++
@@ -1,8 +0,0 @@
-from datasource import DataSource, iswritemode, iszip, unzip, splitzipext, \
-     Repository, Cache, ensuredirs
-from formats.format import getformats, Format
-from formats.analyze import Analyze
-from formats.nifti1 import Nifti1
-from formats.ecat7 import Ecat7
-from formats.afni import AFNI
-
---
+++ neuroimaging/io/api.py
@@ -0,0 +1,4 @@
+from datasource import DataSource, iswritemode, iszip, unzip, splitzipext, \
+     Repository, Cache, ensuredirs
+
+from pyniftiio import PyNiftiIO
