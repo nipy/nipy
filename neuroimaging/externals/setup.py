9331c43296b534e0cd049ffcff067232eb5c2304
Christopher Burns <cburns@berkeley.edu>
2008-07-31 16:49:36 -0700

    Remove config from setup file.

--- neuroimaging/externals/setup.py
+++ neuroimaging/externals/setup.py
@@ -3,7 +3,6 @@ from os.path import join
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('externals', parent_package, top_path)
-    config.add_subpackage('config')
     config.add_subpackage('pynifti')
 
     return config
