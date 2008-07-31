c3d9edc40c5a4f8bec8e19dd317b83c1acf0d74a
Christopher Burns <cburns@berkeley.edu>
2008-07-30 18:50:24 -0700

    Fix test funcs in algorithms packaging.

--- neuroimaging/algorithms/__init__.py
+++ neuroimaging/algorithms/__init__.py
@@ -7,9 +7,6 @@ __docformat__ = 'restructuredtext'
 import statistics
 import fwhm, interpolation, kernel_smooth, onesample, regression
 
-
-def test(level=1, verbosity=1, flags=[]):
-    from neuroimaging.utils.testutils import set_flags
-    set_flags(flags)
-    from neuroimaging.testing import *
-    return NumpyTest().test(level, verbosity)
+from neuroimaging.testing import Tester
+test = Tester().test
+bench = Tester().bench
