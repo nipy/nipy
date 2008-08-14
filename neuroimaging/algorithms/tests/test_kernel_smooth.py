c85832ea07a1846f46edee51b579187eeab91979
Christopher Burns <cburns@berkeley.edu>
2008-08-14 12:17:03 -0700

    Fix Image returned as Image bug, call np.asarray.  Define grid space.  Misc cleanup.

--- neuroimaging/algorithms/tests/test_kernel_smooth.py
+++ neuroimaging/algorithms/tests/test_kernel_smooth.py
@@ -1,16 +1,16 @@
-import numpy as N
-import numpy.random as R
+import numpy as np
+import numpy.random as nprand
 from neuroimaging.testing import *
 
 from neuroimaging.algorithms.kernel_smooth import LinearFilter
 from neuroimaging.core.api import Image
 from neuroimaging.utils.tests.data import repository
-from neuroimaging.core.reference.grid import SamplingGrid, space
+from neuroimaging.core.reference.grid import SamplingGrid
 
 from neuroimaging.algorithms.kernel_smooth import sigma2fwhm, fwhm2sigma
 
 """
-# Need to make an automated test for this!
+# FIXME: Need to make an automated test for this!
 class test_Kernel(TestCase):
     @dec.gui
     import pylab
@@ -35,8 +35,8 @@ class test_SigmaFWHM(TestCase):
         """
         ensure that fwhm2sigma and sigma2fwhm are inverses of each other        
         """
-        fwhm = N.arange(1.0, 5.0, 0.1)
-        sigma = N.arange(1.0, 5.0, 0.1)
+        fwhm = np.arange(1.0, 5.0, 0.1)
+        sigma = np.arange(1.0, 5.0, 0.1)
         assert_almost_equal(sigma2fwhm(fwhm2sigma(fwhm)), fwhm)
         assert_almost_equal(fwhm2sigma(sigma2fwhm(sigma)), sigma)
 
@@ -51,52 +51,56 @@ class test_SigmaFWHM(TestCase):
         tol = 0.9999
         sdtol = 1.0e-8
         for i in range(6):
-            shape = R.random_integers(30,60,(3,))
-            ii, jj, kk = R.random_integers(11,17, (3,))
+            shape = nprand.random_integers(30,60,(3,))
+            ii, jj, kk = nprand.random_integers(11,17, (3,))
 
-            grid = SamplingGrid.from_start_step(names=space, shape=shape, step=R.random_integers(5,10,(3,))*0.5, start=R.random_integers(5,20,(3,))*0.25)
+            space = ('zspace', 'yspace', 'xspace')
+            grid = SamplingGrid.from_start_step(names=space, shape=shape,
+                                                step=nprand.random_integers(5,10,(3,))*0.5,
+                                                start=nprand.random_integers(5,20,(3,))*0.25)
 
-            signal = N.zeros(shape)
+            signal = np.zeros(shape)
             signal[ii,jj,kk] = 1.
             signal = Image(signal, grid=grid)
     
-            kernel = LinearFilter(grid, fwhm=R.random_integers(50,100)/10.)
+            kernel = LinearFilter(grid, fwhm=nprand.random_integers(50,100)/10.)
             ssignal = kernel.smooth(signal)
+            ssignal = np.asarray(ssignal)
             ssignal[:] *= kernel.norms[kernel.normalization]
 
-            I = N.indices(ssignal.shape)
-            I.shape = (3, N.product(shape))
-            i, j, k = I[:,N.argmax(ssignal[:].flat)]
+            I = np.indices(ssignal.shape)
+            I.shape = (3, np.product(shape))
+            i, j, k = I[:,np.argmax(ssignal[:].flat)]
 
             self.assertTrue((i,j,k) == (ii,jj,kk))
 
-            II = N.copy(I)
+            II = np.copy(I)
             Z = kernel.grid.mapping(I) - kernel.grid.mapping([[i],[j],[k]])
 
             _k = kernel(Z)
             _k.shape = ssignal.shape
-            self.assertTrue(N.corrcoef(_k[:].flat, ssignal[:].flat)[0,1] > tol)
+            self.assertTrue(np.corrcoef(_k[:].flat, ssignal[:].flat)[0,1] > tol)
             self.assertTrue((_k[:] - ssignal[:]).std() < sdtol)
             
             def _indices(i,j,k,axis):
-                I = N.zeros((3,20))
+                I = np.zeros((3,20))
                 I[0] += i
                 I[1] += j
                 I[2] += k
-                I[axis] += N.arange(-10,10)
+                I[axis] += np.arange(-10,10)
                 return I
 
             vx = ssignal[i,j,(k-10):(k+10)]
             vvx = grid.mapping(_indices(i,j,k,2)) - grid.mapping([[i],[j],[k]])
-            self.assertTrue(N.corrcoef(vx, kernel(vvx))[0,1] > tol)
+            self.assertTrue(np.corrcoef(vx, kernel(vvx))[0,1] > tol)
 
             vy = ssignal[i,(j-10):(j+10),k]
             vvy = grid.mapping(_indices(i,j,k,1)) - grid.mapping([[i],[j],[k]])
-            self.assertTrue(N.corrcoef(vy, kernel(vvy))[0,1] > tol)
+            self.assertTrue(np.corrcoef(vy, kernel(vvy))[0,1] > tol)
 
             vz = ssignal[(i-10):(i+10),j,k]
             vvz = grid.mapping(_indices(i,j,k,0)) - grid.mapping([[i],[j],[k]])
-            self.assertTrue(N.corrcoef(vz, kernel(vvz))[0,1] > tol)
+            self.assertTrue(np.corrcoef(vz, kernel(vvz))[0,1] > tol)
 
 
 
