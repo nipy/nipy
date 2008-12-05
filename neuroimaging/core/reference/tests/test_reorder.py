79075628469c124f9100475955c05a07c12b61c0
Jonathan Taylor <jonathan.taylor@stanford.edu>
2008-12-05 03:40:40 -0500

    two more nose test scripts

---
+++ neuroimaging/core/reference/tests/test_reorder.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from neuroimaging.core.api import VoxelAxis, RegularAxis, VoxelCoordinateSystem, CoordinateSystem, DiagonalCoordinateSystem
+from neuroimaging.core.reference.coordinate_map import CoordinateMap, ConcatenatedComaps, \
+     ConcatenatedIdenticalComaps
+from neuroimaging.core.reference.mapping import Affine
+
+def setup_cmap():
+    shape = (3,4,5)
+    output_axes = [RegularAxis(s, step=i+1) for i, s in enumerate('xyz')]
+    input_axes = [VoxelAxis(s, length=shape[i]) for i, s in enumerate('ijk')]
+    input_coords = VoxelCoordinateSystem('input', input_axes)
+    output_coords = DiagonalCoordinateSystem('output', output_axes)
+    cmap = CoordinateMap(Affine(output_coords.affine), input_coords,
+                         output_coords)
+    return cmap
+
+def test_reorder1():
+    cmap = setup_cmap()
+    cmap2 = cmap.reorder_input()
+    assert hasattr(cmap2, 'shape')
+    assert cmap2.input_coords.name == 'input-reordered'
+    assert cmap2.output_coords.name == 'output'
+    assert cmap2.shape == cmap.shape[::-1]
+
+def test_reorder2():
+
+    cmap = setup_cmap()
+    cmap2 = cmap.reorder_output()
+    assert hasattr(cmap2, 'shape')
+    assert cmap2.input_coords.name == 'input'
+    assert cmap2.output_coords.name == 'output-reordered'
+    assert cmap2.shape == cmap.shape
