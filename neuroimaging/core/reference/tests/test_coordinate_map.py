06bd0d9fed031c055a8f1c916f8ef42639660b31
Jonathan Taylor <jtaylo@dell-desktop>
2008-12-15 00:21:11 -0800

    more work on tests

--- neuroimaging/core/reference/tests/test_coordinate_map.py
+++ neuroimaging/core/reference/tests/test_coordinate_map.py
@@ -1,43 +1,36 @@
 import numpy as np
 from neuroimaging.testing import *
 
+import nose.tools
+
 from neuroimaging.core.reference.coordinate_map import CoordinateMap
-
 from neuroimaging.core.reference.mapping import Affine
-
 from neuroimaging.testing import anatfile, funcfile
-
 from neuroimaging.core.api import load_image
 
 
-class test_coordmap(TestCase):
 
-    def setUp(self):
-        self.img = load_image(anatfile)
+def test_identity():
+    shape = (30,40,50)
+    i = CoordinateMap.identity(['zspace', 'yspace', 'xshape'], shape=shape)
+    nose.tools.assert_equals(tuple(i.shape), shape)
+    y = i.mapping([3,4,5])
+    nose.tools.assert_true(np.allclose(y, np.array([3,4,5])))
 
-
-
-    def test_identity(self):
-        shape = (30,40,50)
-        i = CoordinateMap.identity(['zspace', 'yspace', 'xshape'], shape=shape)
-        self.assertEquals(tuple(i.shape), shape)
-        y = i.mapping([3,4,5])
-        assert_almost_equal(y, np.array([3,4,5]))
-
-    def test_identity2(self):
-        shape = (30, 40)
-        self.assertRaises(IndexError, CoordinateMap.identity, ['zspace', 'yspace', 'xspace'], shape)
+def test_identity2():
+    shape = (30, 40)
+    nose.tools.assert_raises(IndexError, CoordinateMap.identity, ['zspace', 'yspace', 'xspace'], shape)
 
 
-    def test_from_affine(self):
-        a = Affine.identity(2)
-        g = CoordinateMap.from_affine('ij', 'xy', a, (20,30))
+def test_from_affine():
+    a = Affine.identity(2)
+    g = CoordinateMap.from_affine('ij', 'xy', a, (20,30))
+    nose.tools.assert_equals(g.shape, (20,30))
 
-
-    def test_start_step(self):
-        ''' Test from_start_step '''
-        dcs = CoordinateMap.from_start_step('ijk', 'xyz', [5,5,5],[2,2,2], (10,20,30))
-        self.assertTrue(np.allclose(dcs.affine, [[2,0,0,5],
-                                                 [0,2,0,5],
-                                                 [0,0,2,5],
-                                                 [0,0,0,1]]))
+def test_start_step():
+    ''' Test from_start_step '''
+    dcs = CoordinateMap.from_start_step('ijk', 'xyz', [5,5,5],[2,2,2], (10,20,30))
+    nose.tools.assert_true(np.allclose(dcs.affine, [[2,0,0,5],
+                                                    [0,2,0,5],
+                                                    [0,0,2,5],
+                                                    [0,0,0,1]]))
