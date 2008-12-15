c70e8deb7cd4bac0893593363e05565a4ff8401d
Jonathan Taylor <jonathan.taylor@stanford.edu>
2008-12-15 17:10:58 -0500

    2. -> 2

--- neuroimaging/core/reference/mni.py
+++ neuroimaging/core/reference/mni.py
@@ -6,14 +6,13 @@ __docformat__ = 'restructuredtext'
 
 from neuroimaging.core.reference.axis import Axis
 from neuroimaging.core.reference.coordinate_system import CoordinateSystem
-from neuroimaging.core.reference.mapping import Affine
-from neuroimaging.core.reference.coordinate_map import CoordinateMap
+from neuroimaging.core.reference.coordinate_map import Affine
 
 
-MNI_mapping = CoordinateMap.from_start_step(['xspace', 'yspace', 'zspace'],
-                                            ['xspace', 'yspace', 'zspace'],
-                                            [-72,-126,-90], [2,2,2.], 
-                                            (91, 109,91))
+MNI_mapping = Affine.from_start_step(['xspace', 'yspace', 'zspace'],
+                                     ['xspace', 'yspace', 'zspace'],
+                                     [-72,-126,-90], [2,2,2], 
+                                     (91, 109,91))
 MNI_voxel = MNI_mapping.input_coords
 MNI_world = MNI_mapping.output_coords
 
