db69404359976257c44bb38b5fc71f9338c4ad96
Jonathan Taylor <jtaylo@dell-desktop>
2008-12-15 00:25:29 -0800

    major cleanup of mapping.py
    remove "slice" references, this can be done easier with CoordinateMaps
    
    added some 'reshape'/'raveling' 
    
    added a 'linearize' function
    
    replaced * overloading with 'compose' function
    
    NEXT: I think mapping.py can be deleted, and the functionality swept into CoordinateMap

--- neuroimaging/core/reference/mapping.py
+++ neuroimaging/core/reference/mapping.py
@@ -14,7 +14,7 @@ from struct import unpack
 import numpy as np
 from numpy.linalg import inv, pinv
 
-def _2matvec(transform):
+def matvec_from_transform(transform):
     """ Split a tranform into it's matrix and vector components. """
     ndimin = transform.shape[0] - 1
     ndimout = transform.shape[1] - 1
@@ -22,73 +22,14 @@ def _2matvec(transform):
     vector = transform[0:ndimin, ndimout]
     return matrix, vector
 
-def _2transform(matrix, vector):
+def transform_from_matvec(matrix, vector):
     """ Combine a matrix and vector into a transform. """
     nin, nout = matrix.shape
-    t = np.zeros((nin+1,nout+1))
+    t = np.zeros((nin+1,nout+1), matrix.dtype)
     t[0:nin, 0:nout] = matrix
     t[nin,   nout] = 1.
     t[0:nin, nout] = vector
     return t
-
-
-def permutation_matrix(order=range(3)[2::-1]):
-    """
-    Create an NxN permutation matrix from a sequence, containing the values
-    0, ..., N-1.
-    """
-    n = len(order)
-    matrix = np.zeros((n, n))
-    if set(order) != set(range(n)):
-        raise ValueError('order should be a sequence of integers with' \
-                         'values, 0 ... len(order)-1.')
-    for i in range(n): 
-        matrix[i, order[i]] = 1
-    return matrix
-
-
-def permutation_transform(order=range(3)[2::-1]):
-    """
-    Create an (N+1)x(N+1) permutation transformation matrix from a sequence,
-    containing the values 0,...,N-1.
-    """
-    matrix = permutation_matrix(order=order)
-    vector = np.zeros(len(order))
-    return _2transform(matrix, vector)
-
-
-def translation_transform(x, ndim):
-    """
-    Create an affine transformation matrix representing translation by x.
-    
-    translation_transform( x=(1,2,3), ndim=3)
-    array([[ 1.,  0.,  0.,  1.],
-           [ 0.,  1.,  0.,  2.],
-           [ 0.,  0.,  1.,  3.],
-           [ 0.,  0.,  0.,  1.]])
-
-
-    """
-    return _2transform(np.identity(ndim), x)
-    
-
-
-def isdiagonal(matrix):
-    """
-    Test if the given matrix is diagonal.
-
-    :Parameters:
-        matrix : ``numpy.ndarray``
-            The matrix to check.
-    :Returns: ``bool``
-
-    :Precondition: matrix must be square
-    """
-
-    ndim = matrix.shape[0]
-    mask = ~np.identity(ndim, dtype=bool)
-    masked = np.ma.array(matrix, mask=mask, fill_value=0.).filled()
-    return np.all(masked == matrix)
 
 class Mapping(object):
     """
@@ -97,186 +38,68 @@ class Mapping(object):
     and an optional inverse.
     """
 
-    def __init__(self, map, inverse=None, name="mapping", ndim=(None,None)):
+    def __init__(self, map, inverse=None):
         """
         :Parameters:
             map : callable
                 A function which takes a coordinate vector and returns a new coordinate vector
             inverse : callable
                 An inverse function to ``map``
-            name : ``string``
-                The name of the mapping
             ndim : (``int``, ``int``)
                 The number of (input, output) dimensions of the mapping.
         """
         self._map = map
-        self._inverse = inverse
-        self.name = name
-        self.ndim = ndim
+        if inverse is not None:
+            self._inverse = inverse
         
     def __call__(self, x):
-        """ Apply this mapping to the given coordinates. """
+        """ Apply this mapping to the given coordinates. 
+        It expects a coordinate array of shape (*, self.transform.shape[1]-1)
+        as it multiplies x on the right by the transformation matrix.
+        
+        :Parameters:
+            x : ``numpy.ndarray``
+                Points at which to evaluate the mapping.
+        
+        :Returns: ``numpy.ndarray``
+        """
+        self._checkshape(x)
         return self._map(x)
 
-
-    def __str__(self):
-        return '%s:map=%s\n' % (self.name, self._map) + \
-        '%s:inverse=%s\n' % (self.name, self._inverse)
-
-
-    @staticmethod
-    def from_callable(c, ndim=(None, None)):
+    def _checkshape(self, x):
         """
-        Construct a Mapping from a callable. If the
-        callable is an instance of Mapping, return it.
+        Verify that x has the proper shape for evaluating the mapping
         """
-        if isinstance(c, Mapping):
-            return c
-        else:
-            return Mapping(c, ndim=ndim)
+        if x.dtype.isbuiltin:
+            if x.ndim > 2:
+                raise ValueError('if dtype is builtin, expecting a 2-d with shape[-1] == ndim_input, or a 1-d array of shape[0] == ndim_input')
+        elif x.ndim > 1:
+            raise ValueError, 'if dtype is not builtin, expecting 1-d array, or a 0-d array' 
 
-    def __ne__(self, other): 
-        """
-        :SeeAlso:
-         - `Mapping.__eq__`
-        """
-        return not self.__eq__(other)
-        
-    def __eq__(self, other):
-        """
-        We can't say whether two map functions are the same so we just
-        raise an exception if people try to compare mappings.
-
-        :Raises NotImplementedError:
-        """
-        raise NotImplementedError
-
-    def __mul__(self, other):
-        """If this method is not over-written we get complaints about sequences.
-
-        :Returns: `Mapping`
-        """
-        return other.__rmul__(self)
-    
-    def __rmul__(self, other):
-        """ mapping composition
-
-        :Parameters:
-            other : `Mapping`
-                The mapping to compose with.
-        :Returns: `Mapping`
-        """
-        other = Mapping.from_callable(other)
-        def _map(coords): 
-            return other(self(coords))
-        if self.isinvertible() and other.isinvertible():
-            def inverse(coords): 
-                return self.inverse()(other.inverse()(coords))
-        else: 
-            inverse = None
-        return Mapping(_map, inverse=inverse, ndim=(self.ndim[0], other.ndim[1]))
-
-    def isinvertible(self):
+    def _getisinvertible(self):
         """
         Does this mapping have an inverse?
 
         :Returns: ``bool``
         """
-        return self._inverse is not None
+        if self.inverse is not None:
+            return callable(self.inverse)
+        return False
 
-    def inverse(self):
+    isinvertible = property(_getisinvertible)
+
+    def _getinverse(self):
         """
         Create a new Mapping instance which is the inverse of self.
 
         :Returns: `Mapping`
         """
-        if self.isinvertible():
-            return Mapping(self._inverse, self, ndim=self.ndim[::-1])
-        else: 
-            raise AttributeError("non-invertible mapping")
+        try:
+            return Mapping(self._inverse, self)
+        except AttributeError:
+            return None
+    inverse = property(_getinverse)
 
-    def tovoxel(self, real):
-        """
-        Given a real coordinate, where self.input_coords are assumed to be
-        voxels, return the closest voxel for reaf. Will choke if mapping is
-        not invertible.
-
-        :Raises np.linalg.LinAlgError: is mapping is not invertible.
-        """
-        shape = real.shape
-        if len(shape) > 1:
-            real.shape = (shape[0], np.product(shape[1:]))
-        voxel = np.around(self.inverse()(real))
-        voxel.shape = shape
-        return np.array(voxel)
-
-    def _preslice_mapping(self, index, gshape):
-        """
-        Compute fixed and varying indices in the slice object index.
-
-        Output is used in self.slice_mapping method.
-
-        shape is used to compute where to stop ?
-
-        """
-        
-        varcoords = []
-        shape = []
-        maps = []
-        ssteps = []
-        sstarts = []
-
-        if type(index) not in  [type(()), type([])]:
-            index = (index,)
-        for i in range(len(index), len(gshape)):
-            index += (slice(0,gshape[i],1),)
-
-        for j, i in enumerate(index):
-            if type(i) is type(1):
-                maps.append(lambda x: x)
-                sstarts.append(i)
-            elif type(i) is type(slice(0,1)):
-                varcoords.append(j)
-                start, stop, step = i.start, i.stop, i.step
-                if step is None:
-                    step = 1 
-                if stop is None:
-                    stop = gshape[j]
-                stop = min(stop, gshape[j])
-                if start is None:
-                    start = 0
-                start = min(start, gshape[j])
-                try:
-                    x = stop - start
-                except:
-                    raise ValueError, `stop` + ' ' + `start` + ' ' + `i` + ' ' + `type(i)` + ' ' + `i.stop`
-                shape.append(int(np.ceil((stop - start) / (step * 1.))))
-                sstarts.append(start)
-                maps.append(lambda x: x * step + start)
-                ssteps.append(step)
-            else:
-                raise ValueError, 'expecting a tuple or sequence of integers or slices'
-        return varcoords, tuple(maps), sstarts, ssteps, shape
-
-    def _slice_mapping(self, index, gshape):
-        """
-        TODO: this has to be tested for nonaffine mappings... check the ndim below
-        """
-        varcoords, maps, _, _, shape = self._preslice_mapping(index, gshape)
-        mapc = copy.deepcopy(self._map)
-        def mapping(x):
-            y = x.copy()
-            o = np.ones(x.shape)
-            for i in range(nvar):
-                y[i] = maps[i](y[i])
-            for i in range(self.ndim[0]):
-                if i not in varcoords:
-                    o = np.vstack([o, np.ones(x.shape) * np.ones[i]])
-                else:
-                    o = np.vstack([o, y[i]])
-
-            return mapc(o[1:])
-        return varcoords, Mapping(mapping), shape
 
 class Affine(Mapping):
     """
@@ -295,151 +118,178 @@ class Affine(Mapping):
         """
         return Affine(np.identity(ndim+1))
 
+    def __eq__(self, other):
+        if hasattr(other, 'transform'):
+            return np.allclose(self.transform, other.transform)
+        return False
 
-    def _slice_mapping(self, index, gshape):
-        """
-        Return an Affine instance
-        """
-        varcoords, _, start, step, shape = self._preslice_mapping(index, gshape)
-        stepmatrix = np.diag(list(step) + [1])
-        startvector = np.dot(self.transform, np.array(list(start)+[1]))[:-1]
-
-        tmatrix = np.zeros((len(gshape) + 1, len(varcoords) + 1))
-        for i, j in enumerate(varcoords):
-            tmatrix[:-1,i] = self.transform[:-1,j]
-        tmatrix[-1,-1] = 1.
-        tmatrix = np.dot(tmatrix, stepmatrix)
-        tmatrix[:-1,-1] = startvector
-        return varcoords, Affine(tmatrix), shape
-
-    def __init__(self, transform, name="affine", dtype=np.float):
+    def __init__(self, affine_transform):
         """
         :Parameters:
-            transform : ``numpy.ndarray``
+            affine_transform : ``numpy.ndarray``
                 A transformation matrix
-            name : ``string``
-                The name of the mapping
-            dtype : ``numpy.ndtype``
-                If transform.dtype is not in np.sctypes['float'] + np.sctypes['complex'],
-                it will be typecast to this dtype. 
 
         """
-        if transform.dtype not in np.sctypes['float'] + np.sctypes['complex']:
-            transform = np.asarray(transform, dtype=dtype)
-        self.transform = transform
-        ndimin = transform.shape[0] - 1
-        ndimout = transform.shape[1] - 1
-        self._fmatrix, self._fvector = _2matvec(transform)
-        Mapping.__init__(self, None, name=name, ndim=(ndimin, ndimout))
+        self.transform = affine_transform
+        ndimin = affine_transform.shape[0] - 1
+        ndimout = affine_transform.shape[1] - 1
+        self._fmatrix, self._fvector = matvec_from_transform(affine_transform)
+        Mapping.__init__(self, None)
+
+    def _checkshape(self, x):
+        """
+        Verify that x has the proper shape for evaluating the mapping
+        """
+        A, b = self.params
+        ndim = A.shape[::-1]
+        if x.dtype.isbuiltin:
+            if x.ndim > 2 or x.shape[-1] != ndim[0]:
+                raise ValueError('if dtype is builtin, expecting a 2-d array of shape (*,%d) or a 1-d array of shape (%d,)' % (ndim[0], ndim[0]))
+        elif x.ndim > 1:
+            raise ValueError, 'if dtype is not builtin, expecting 1-d array, or a 0-d array' 
 
     def get_params(self):
-        return self._fmatrix, self._fvector
+        return matvec_from_transform(self.transform)
     params = property(get_params,doc='Matrix, vector representation of affine')
 
-    def ravel(self, x):
+    def reshape(self, x):
         """
         Typecast x to have the same dtype as self.transform
-        and return a ravel'ed version.
+        and return a ravel'ed version with appropriate
+        shape.
         
-        If x.dtype is not a built in, it returns
+        If x.dtype is not a built in, it reshapes
         x.ravel().view(self.transform.dtype).
 
-        If x.dtype is builtin it returns
+        If x.dtype is builtin it reshapes
         x.ravel(). 
         """
 
+        A, b = self.params
+        ndim = A.shape[::-1]
+
         if x.dtype.isbuiltin:
-            if x.ndim > 2:
-                raise ValueError, 'if dtype is builtin, expecting a 2-d array of shape (*,%d) or a 1-d array of shape (%d,)' % (self.ndim[0], self.ndim[0])
-            return x.ravel() # do nothing except ravel(), probably not necessary with this check above?
+            y = x.ravel() # do nothing except ravel()
+        else:
+            y = x.ravel().view(self.transform.dtype)
+        y.shape = (y.shape[0] / ndim[0], ndim[0])
+        return y
 
-        if x.ndim > 1:
-            raise ValueError, 'if dtype is not builtin, expecting 1-d array, or a 0-d array' 
-        return x.ravel().view(self.transform.dtype)
-
-    def __call__(self, coords):
+    def __call__(self, x):
         """ Apply this mapping to the given coordinates. 
-        It expects a coordinate array of shape (*, self.ndim[0])
-        as it multiplies the coords on the right by the transformation matrix.
+        It expects a coordinate array of shape (*, self.transform.shape[1]-1)
+        as it multiplies x on the right by the transformation matrix.
         
         :Parameters:
-            coords : ``numpy.ndarray``
-                A coordinate vector
+            x : ``numpy.ndarray``
+                Points at which to evaluate the mapping.
         
         :Returns: ``numpy.ndarray``
         """
 
-        # Typecasting explicitly here:
-        # it expects an ndarray with shape (*, self._fmatrix.shape[0])
-        # of the same dtype as self._matrix
+        x = np.asarray(x)
+        self._checkshape(x)
 
-        coords = np.asarray(coords)
-
-        tcoords = self.ravel(coords)
-        tcoords.shape = (tcoords.shape[0] / self.ndim[0], self.ndim[0])
-        value = np.dot(tcoords, self._fmatrix.T) 
-
-        value += np.multiply.outer(np.ones(value.shape[0]), self._fvector)
-        if coords.shape == ():
+        A, b = self.params
+        tx = self.reshape(x)
+        value = np.dot(tx, A.T) 
+        value += np.multiply.outer(np.ones(value.shape[0]), b)
+        if x.shape == ():
             value.shape == ()
         return value
 
-    def __eq__(self, other):
-        """
-        Equality is defined as equality of both name and transform matrix.
-
-        :Parameters:
-            other : `Affine`
-                The mapping to be compared to.
-                
-        :Returns: ``bool``
-        """
-        if not hasattr(other, "transform"): 
-            return False
-        return np.all(np.asarray(self.transform) == np.asarray(other.transform)) \
-               and self.name == other.name
-
-
-    def __rmul__(self, other):
-        """
-        :Parameters:
-            other : `Mapping` or `Affine`
-                The mapping to be multiplied by.
-                
-        :Returns: `Mapping` or `Affine`
-        """
-        if isinstance(other, Affine):
-            return Affine(np.dot(other.transform, self.transform))            
-        else: 
-            return Mapping.__rmul__(self, other)
-
-
-    def __str__(self):
-        """
-        :Returns: ``string``
-        """
-        return "%s:fmatrix=%s\n%s:fvector=%s" % \
-          (self.name, `self._fmatrix`, self.name,`self._fvector`)
- 
-
-    def isinvertible(self):
-        """
-        Does this mapping have an inverse?
-
-        :Returns: ``bool``
-        """
-        try:
-            inv(self.transform)
-            return True
-        except:
-            return False
-
-    def inverse(self):
+    def _getinverse(self):
         """
         Create a new `Affine` instance which is the inverse of self.
 
         :Returns: `Affine`
         """
-        return Affine(pinv(self.transform))
+        # Does it make sense to return pinv?
+        try:
+            return Affine(inv(self.transform))
+        except np.linalg.linalg.LinAlgError:
+            return None
 
+    inverse = property(_getinverse)
+
+def compose(*mappings):
+    """ Mapping composition
+
+    :Parameters:
+         mappings: sequence of Mappings
+                The mapping to compose with.
+    :Returns: `Mapping`
+
+    :Note: If all mappings are Affine, return an Affine instance.
+
+    """
+    def _compose2(map1, map2):
+        forward = lambda input: map1(map2(input))
+        if map1.isinvertible and map2.isinvertible:
+            backward = lambda output: map2.inverse(map1.inverse(output))
+        else:
+            backward = None
+        return Mapping(forward, inverse=backward)
+    mapping = mappings[-1]
+    for i in range(len(mappings)-2,-1,-1):
+        mapping = _compose2(mappings[i], mapping)
+
+    notaffine = filter(lambda mapping: not isinstance(mapping, Affine), mappings)
+    if not notaffine:
+        mapping = linearize(mapping, mappings[-1].transform.shape[1]-1)
+    return mapping
     
+def linearize(mapping, ndimin, step=np.array(1.), origin=None):
+    """
+    Given a Mapping of ndimin variables, 
+    with an input builtin dtype, return the linearization
+    of mapping at origin based on a given step size
+    in each coordinate axis.
+
+    If not specified, origin defaults to np.zeros(ndimin, dtype=dtype).
+    
+    :Inputs: 
+        mapping: ``Mapping``
+              A Mapping to linearize
+        ndimin: ``int``
+              Number of input dimensions to mapping
+        origin: ``ndarray``
+              Origin at which to linearize mapping
+        step: ``ndarray``
+              Step size, an ndarray with step.shape == ().
+
+    :Returns:
+        A: ``ndarray``
+            Linearization of mapping in homogeneous coordinates, i.e. 
+            an array of size (ndimout+1, ndimin+1) where
+            ndimout = mapping(origin).shape[0].
+
+    :Notes: The dtype of the resulting Affine mapping
+            will be the dtype of mapping(origin)/step, regardless
+            of the input dtype.
+
+    """
+    step = np.asarray(step)
+    if step.shape != ():
+        raise ValueError('step should be a scalar value')
+    if origin is None:
+        origin = np.zeros(ndimin, step.dtype)
+    else:
+        if origin.dtype != step.dtype:
+            warnings.warn('origin.dtype != step.dtype in function linearize, using step.dtype')
+        origin = np.asarray(origin, dtype=step.dtype)
+        if origin.shape != (ndimin,):
+            raise ValueError('origin.shape != (%d,)' % ndimin)
+    b = mapping(origin)
+
+    origin = np.multiply.outer(np.ones(ndimin), origin)
+    y1 = mapping(step*np.identity(ndimin) + origin)
+    y0 = mapping(origin)
+
+    ndimout = y1.shape[1]
+    C = np.zeros((ndimout+1, ndimin+1), (y0/step).dtype)
+    C[-1,-1] = 1
+    C[:ndimout,-1] = b
+    C[:ndimout,:ndimin] = (y1 - y0).T / step
+    return Affine(C)
+
