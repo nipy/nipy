d10dcef79bd224f23414929ae03700b465201ff0
Jonathan Taylor <jonathan.taylor@stanford.edu>
2008-12-27 00:16:43 -0500

    fmri tests fixed

--- neuroimaging/modalities/fmri/tests/test_fmri.py
+++ neuroimaging/modalities/fmri/tests/test_fmri.py
@@ -1,61 +1,62 @@
 import gc, os
 from tempfile import mkstemp
 import numpy as np
-from neuroimaging.testing import *
 
-import neuroimaging.core.reference.axis as axis
+import nose.tools
+
 import neuroimaging.core.reference.coordinate_map as coordinate_map
-
 from neuroimaging.modalities.fmri.api import FmriImageList, fmri_generator, fromimage
 from neuroimaging.core.api import Image, load_image, data_generator, parcels, save_image
 from neuroimaging.testing import anatfile, funcfile
 
+edict = {}
 
-# not a test until test data is found
-class test_fMRI(TestCase):
+def setup():
+    edict['parcelmap'] = load_image(funcfile)[0]
+    edict['img'] = load_image(funcfile)
+    edict['fmri'] = fromimage(edict['img'])
 
-    def setUp(self):
-        self.parcels = load_image(funcfile)[0]
-        self.img = load_image(funcfile)
-        self.fmri = fromimage(self.img)
+def test_write():
+    fp, fname = mkstemp('.nii')
+    img = edict['img']
+    save_image(img, fname)
+    test = fromimage(load_image(fname))
+    yield nose.tools.assert_equal, test[0].affine.shape, (4,4)
+    yield nose.tools.assert_equal, img[0].affine.shape, (5,4)
+    yield nose.tools.assert_true, np.allclose(test[0].affine, img[0].affine[1:])
+    os.remove(fname)
 
-    def data_setUp(self):
-        self.img = load_image(funcfile)
+def test_iter():
+    img = edict['img']
+    j = 0
+    for i, d in fmri_generator(img):
+        j += 1
+        nose.tools.assert_equal(d.shape, (20,2,20))
+        del(i); gc.collect()
+    nose.tools.assert_equal(j, 20)
 
-    def test_write(self):
-        #self.fail('this is a problem with reading/writing so-called "mat" files -- all the functions for these have been moved from core.reference.mapping to data_io.formats.analyze -- and they need to be fixed because they do not work. the names of the functions are: matfromstr, matfromfile, matfrombin, matfromxfm, mattofile')
-        fp, fname = mkstemp('.nii')
-        save_image(self.img, fname)
-        test = fromimage(load_image(fname))
-        self.assertEquals(test[0].coordmap.shape, self.img[0].coordmap.shape)
-        os.remove(fname)
-
-    def test_iter(self):
-        j = 0
-        for i, d in fmri_generator(self.img):
-            j += 1
-            self.assertEquals(d.shape, (20,2,20))
-            del(i); gc.collect()
-        self.assertEquals(j, 20)
-
-    def test_subcoordmap(self):
-        subcoordmap = self.img.coordmap[3]
+def test_subcoordmap():
+    img = edict['img']
+    subcoordmap = img[3].coordmap
         
-        xform = np.array([[ 0., 0., 0., 10.35363007],
-                          [-7.,  0., 0., 14.],
-                          [ 0.,  -2.34375, 0., 56.25],
-                          [ 0.,  0., -2.34375, 56.25],
-                          [ 0.,  0., 0., 1.]])
+    xform = np.array([[ 0., 0., 0., 10.35363007],
+                      [-7.,  0., 0., 0.],
+                      [ 0.,  -2.34375, 0., 0.],
+                      [ 0.,  0., -2.34375, 0.],
+                      [ 0.,  0., 0., 1.]])
         
-        assert_almost_equal(subcoordmap.mapping.transform, xform)
+    print img.affine
+    nose.tools.assert_true(np.allclose(subcoordmap.affine, xform))
         
-    def test_labels1(self):
-        parcelmap = (np.asarray(self.parcels) * 100).astype(np.int32)
+def test_labels1():
+    parcelmap = edict['parcelmap']
+    img = edict['img']
+    parcelmap = (np.asarray(parcelmap) * 100).astype(np.int32)
         
-        v = 0
-        for i, d in fmri_generator(self.img, parcels(parcelmap)):
-            v += d.shape[1]
-        self.assertEquals(v, parcelmap.size)
+    v = 0
+    for i, d in fmri_generator(img, parcels(parcelmap)):
+        v += d.shape[1]
+    nose.tools.assert_equal(v, parcelmap.size)
 
 
 
