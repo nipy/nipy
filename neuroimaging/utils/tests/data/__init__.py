9a7c80744bc1e57fe0ec5fc7cf149dada2d05121
Christopher Burns <cburns@berkeley.edu>
2009-03-10 17:05:24 -0700

    Extend error message regarding missing test data.

--- neuroimaging/utils/tests/data/__init__.py
+++ neuroimaging/utils/tests/data/__init__.py
@@ -1,5 +1,4 @@
-"""Information used for locating nipy test data.
-
+"""
 Nipy uses a set of test data that is installed separately.  The test
 data should be located in the directory ``~/.nipy/tests/data``.
 
@@ -8,6 +7,12 @@ Install the data in your home directory from the data repository::
   $ svn co http://neuroimaging.scipy.org/svn/ni/data/trunk/fmri .nipy/tests/data
 
 """
+
+# Fernando pointed out that we should wrap the test data into a
+# tarball and write a pure python function to grab the data for people
+# instead of using svn.  Users may not have svn and the mkdir may not
+# work on Windows.
+
 from os.path import expanduser, exists, join
 
 from neuroimaging.io.datasource import Repository
@@ -16,7 +21,7 @@ from neuroimaging.io.datasource import Repository
 datapath = expanduser(join('~', '.nipy', 'tests', 'data'))
 
 if not exists(datapath):
-    raise IOError, 'Nipy data directory is not found!'
+    msg = 'Nipy data directory is not found!\n%s' % __doc__
+    raise IOError(msg)
 
 repository = Repository(datapath)
-
