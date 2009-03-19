a435ec59188aff80fdadbb9b974128308d0ce1b5
Christopher Burns <cburns@berkeley.edu>
2009-03-19 11:29:07 -0700

    Add bzr revno to version on non-release.

--- neuroimaging/version.py
+++ neuroimaging/version.py
@@ -2,4 +2,13 @@ version='0.1.2'
 release=False
 
 if not release:
-    version += '.dev'
+    try:
+        import subprocess
+        proc = subprocess.Popen(['bzr', 'revno'], stdout=subprocess.PIPE)
+        revno, errcode = proc.communicate()
+        revno = int(revno)
+        version += 'r%d' % revno
+    except OSError:
+        # bzr was probably not found
+        # just tack on a dev label
+        version += '.dev'
