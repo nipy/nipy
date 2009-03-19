5f68f1de9ba5e8717b2b7f963db4488037e92911
Christopher Burns <cburns@berkeley.edu>
2009-03-19 14:14:53 -0700

    BUG: Make sure 'bzr revno' is executed in same directory as version module.

--- neuroimaging/version.py
+++ neuroimaging/version.py
@@ -2,13 +2,25 @@ version='0.1.2'
 release=False
 
 if not release:
+    have_revno = False
     try:
+        import os
         import subprocess
-        proc = subprocess.Popen(['bzr', 'revno'], stdout=subprocess.PIPE)
+        # Use bzr to get the revision number.  Execute bzr in the same
+        # directory as this file.
+        dir = os.path.dirname(__file__)
+        proc = subprocess.Popen(['bzr', 'revno'], stdout=subprocess.PIPE,
+                                cwd=dir)
         revno, errcode = proc.communicate()
-        revno = int(revno)
-        version += 'r%d' % revno
+        if revno:
+            revno = int(revno)
+            version += 'r%d' % revno
+            have_revno = True
     except OSError:
-        # bzr was probably not found
-        # just tack on a dev label
-        version += '.dev'
+        # Either bzr was not found or nipy was imported from a
+        # directory that isn't a bzr branch.  Just ignore.
+        pass
+    finally:
+        if not have_revno:
+            # just tack on a dev label
+            version += '.dev'
