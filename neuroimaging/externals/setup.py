a0f9ac8976d9e2a399950cc42bdaed4cdb7041d7
Christopher Burns <cburns@berkeley.edu>
2009-03-24 22:55:42 -0700

    API: Remove pynifti.  It's now an external dependency.

--- neuroimaging/externals/setup.py
+++ neuroimaging/externals/setup.py
@@ -1,9 +1,7 @@
-from os.path import join
 
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
     config = Configuration('externals', parent_package, top_path)
-    config.add_subpackage('pynifti')
 
     return config
 
