5e2cc33bf6f84539e09e6475087b7f96190453b8
Gael Varoquaux <gael.varoquaux@normalesup.org>
2009-03-29 04:20:45 +0200

    BUG: Fix build on windows (attempt to)

--- neuroimaging/neurospin/glm/setup.py
+++ neuroimaging/neurospin/glm/setup.py
@@ -8,7 +8,11 @@ def configuration(parent_package='',top_path=None):
     # fortran library, and the linker needs this information.
     from numpy.distutils.system_info import get_info
     lapack_info = get_info('lapack_opt',0)
-    
+    if 'libraries' not in lapack_info:
+        # But on OSX that may not give us what we need, so try with 'lapack'
+        # instead.  NOTE: scipy.linalg uses lapack_opt, not 'lapack'...
+        lapack_info = get_info('lapack',0)
+
     config = Configuration('glm', parent_package, top_path)
     config.add_data_dir('tests')
     config.add_extension(
