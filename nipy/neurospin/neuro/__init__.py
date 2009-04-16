c9e07be3aad8541299cdf46df0d12a9196b0349c
Alexis Roche <alexis.roche@gmail.com>
2009-04-16 22:58:57 +0200

    Fixed obsolete imports.

--- nipy/neurospin/neuro/__init__.py
+++ nipy/neurospin/neuro/__init__.py
@@ -1,8 +1,3 @@
-from image_classes import *
-from linear_model import linear_model
-from statistical_test import cluster_stats, onesample_test
-import fmri
-
 from numpy.testing import Tester
 
 test = Tester().test
