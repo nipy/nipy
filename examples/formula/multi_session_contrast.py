fixing examples/formula; added a helper define function:

examples/formula/multi_session_contrast.py
@@ -1,6 +1,6 @@
 import numpy as np
 import sympy
-from neuroimaging.modalities.fmri import formula, utils, hrf
+from nipy.modalities.fmri import formula, utils, hrf
 
 # hrf 
 
@@ -18,7 +18,7 @@ d1 = utils.fourier_basis([0.3,0.5,0.7]); d1 = d1.subs(t, t1)
 tval1 = np.linspace(0,20,101)
 
 f1 = formula.Formula([c11,c21,c31]) + d1
-f1.aliases['hrf1'] = hrf.glover
+f1 = f1.subs('hrf1', hrf.glover)
 
 # Session 2
 
@@ -30,7 +30,7 @@ d2 = utils.fourier_basis([0.3,0.5,0.7]); d2 = d2.subs(t, t2)
 tval2 = np.linspace(0,10,51)
 
 f2 = formula.Formula([c12,c22,c32]) + d2
-f2.aliases['hrf2'] = hrf.dglover
+f2 = f2.subs('hrf2', hrf.dglover)
 
 sess_factor = formula.Factor('sess', [1,2])
 
@@ -45,9 +45,8 @@ ttval1 = np.hstack([tval1, np.zeros(tval2.shape)])
 ttval2 = np.hstack([np.zeros(tval1.shape), tval2])
 session = np.array([1]*tval1.shape[0] + [2]*tval2.shape[0])
 
-f.aliases['hrf1'] = hrf.glover
-f.aliases['hrf2'] = hrf.dglover
-d = formula.Design(f)
+f.subs('hrf1', hrf.glover)
+f.subs('hrf2', hrf.dglover)
 
 # Create the recarray that will be used to create the design matrix
 
@@ -62,16 +61,16 @@ rec = np.array([(t1,t2, s) for t1, t2, s in zip(ttval1, ttval2, session)],
 # the contrast from the Formula, "f" above,
 # applying all of f's aliases to it....
 
-contrast = formula.Formula([c11-c12])
-contrast.aliases['hrf1'] = hrf.glover
-contrast.aliases['hrf2'] = hrf.dglover
+# contrast = formula.Formula([c11-c12])
+# contrast.aliases['hrf1'] = hrf.glover
+# contrast.aliases['hrf2'] = hrf.dglover
 
-# Create the design matrix
+# # Create the design matrix
 
-X = d(rec, return_float=True)
+# X = d(rec, return_float=True)
 
-d2 = formula.Design(contrast, return_float=True)
-preC = d2(rec)
+# d2 = formula.Design(contrast, return_float=True)
+# preC = d2(rec)
 
-C = np.dot(np.linalg.pinv(X), preC)
-print C
+# C = np.dot(np.linalg.pinv(X), preC)
+# print C

examples/formula/parametric_design.py
@@ -15,32 +15,11 @@ from nipy.modalities.fmri import utils, formula, hrf
 dt = np.random.uniform(low=0, high=2.5, size=(50,))
 t = np.cumsum(dt)
 
-def alias(expr, name):
-    """
-    Take an expression of 't' (possibly complicated)
-    and make it a '%s(t)' % name, such that
-    when it evaluates it has the right values.
-
-    Parameters:
-    -----------
-
-    expr : sympy expression, with only 't' as a Symbol
-
-    name : str
-
-    Outputs:
-    --------
-
-    nexpr: sympy expression
-    
-    """
-    v = formula.vectorize(expr)
-    return formula.aliased_function(name, v)(formula.Term('t'))
 
 a = sympy.Symbol('a')
-linear = alias(utils.events(t, dt, f=hrf.glover), 'linear')
-quadratic = alias(utils.events(t, dt, f=hrf.glover, g=a**2), 'quad')
-cubic = alias(utils.events(t, dt, f=hrf.glover, g=a**3), 'cubic')
+linear = formula.define('linear', utils.events(t, dt, f=hrf.glover))
+quadratic = formula.define('quad', utils.events(t, dt, f=hrf.glover, g=a**2))
+cubic = formula.define('cubic', utils.events(t, dt, f=hrf.glover, g=a**3))
 
 f1 = formula.Formula([linear, quadratic, cubic])
 

nipy/modalities/fmri/fmristat/tests/test_FIAC.py
@@ -81,11 +81,8 @@ def protocol(fh, design_type, *hrfs):
 
         _begin = np.array(times)[~keep]
 
-        s = formula.vectorize(utils.events(_begin, f=hrf.glover))
-        begin = formula.aliased_function('begin', s)
-
         termdict = {}        
-        termdict['begin'] = begin(hrf.t)
+        termdict['begin'] = formula.define('begin', utils.events(_begin, f=hrf.glover))
         drift = formula.natural_spline(hrf.t, knots=[191/2.+1.25], intercept=True)
         for i, t in enumerate(drift.terms):
             termdict['drift%d' % i] = t
@@ -101,8 +98,8 @@ def protocol(fh, design_type, *hrfs):
             for l, h in enumerate(hrfs):
                 k = np.array([events[i] == v for i in 
                               range(times.shape[0])])
-                s = formula.vectorize(utils.events(times[k], f=h))
-                termdict['%s%d' % (v,l)] = formula.aliased_function("%s%d" % (v, l), s)(hrf.t)
+                termdict['%s%d' % (v,l)] = formula.define("%s%d" % (v, l), 
+							  utils.events(times[k], f=h))
 
         f = formula.Formula(termdict.values())
 
@@ -171,11 +168,8 @@ def altprotocol(fh, design_type, *hrfs):
         _begin = d.event[~keep]
 	d = d[keep]
 
-        s = formula.vectorize(utils.events(_begin, f=hrf.glover))
-        begin = formula.aliased_function('begin', s)
-
         termdict = {}        
-        termdict['begin'] = begin(hrf.t)
+        termdict['begin'] = formula.define('begin', utils.events(_begin, f=hrf.glover))
         drift = formula.natural_spline(hrf.t, knots=[191/2.+1.25], intercept=True)
         for i, t in enumerate(drift.terms):
             termdict['drift%d' % i] = t
@@ -198,8 +192,7 @@ def altprotocol(fh, design_type, *hrfs):
             for l, h in enumerate(hrfs):
                 signs = indic[key].design(d, return_float=True)
 		symb = utils.events(d.event, amplitudes=signs, f=h)
-		vec = formula.vectorize(symb)
-		termdict['%s%d' % (key, l)] = formula.aliased_function('%s%d' % (key, l), vec)(hrf.t)
+		termdict['%s%d' % (key, l)] = formula.define('%s%d' % (key, l), symb)
 
         f = formula.Formula(termdict.values())
 

nipy/modalities/fmri/formula.py
@@ -234,8 +234,7 @@ class Formula(object):
         _b0*g(t) + _b1*g(s)
 
         """
-        f = Formula([term.subs(old, new) for term in self.terms])
-        return f
+        return Formula([term.subs(old, new) for term in self.terms])
 
     def _getcoefs(self):
         if not hasattr(self, '_coefs'):
@@ -749,3 +748,26 @@ class RandomEffects(Formula):
         """
         D = self.design(term, param=param, return_float=True)
         return np.dot(D, np.dot(self.sigma, D.T))
+
+def define(name, expr):
+    """
+    Take an expression of 't' (possibly complicated)
+    and make it a '%s(t)' % name, such that
+    when it evaluates it has the right values.
+
+    Parameters:
+    -----------
+
+    expr : sympy expression, with only 't' as a Symbol
+
+    name : str
+
+    Outputs:
+    --------
+
+    nexpr: sympy expression
+    
+    """
+    v = vectorize(expr)
+    return aliased_function(name, v)(Term('t'))
+