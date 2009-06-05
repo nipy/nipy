20e9493576e1c81e24dbb9f7c57fee005dd81021
Matthew Brett <matthew.brett@gmail.com>
2009-06-05 13:23:16 -0400

    Added setup files for brifti import; cleanup up copy_brifti script

--- nipy/io/imageformats/setup.py
+++ nipy/io/imageformats/setup.py
@@ -1,10 +1,11 @@
+import os.path
+import numpy.core
+
 def configuration(parent_package='',top_path=None):
     from numpy.distutils.misc_util import Configuration
-
-    config = Configuration('io', parent_package, top_path)
+    config = Configuration('imageformats', parent_package, top_path)
 
     config.add_data_dir('tests')
-    config.add_subpackage('imageformats')
 
     return config
 
--- tools/copy_brifti.py
+++ tools/copy_brifti.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-''' Script to copy a version of pynifti / brifti into NIPY tree
+''' Script to make a copy of pynifti / brifti for insertion into NIPY tree
 
 It downloads a copy from the repository, patches the paths, tests, makes
 an archive, deletes temporary copies, and exits.
@@ -26,43 +26,67 @@ subs = (
     )
 
 caller = functools.partial(call, shell=True)
+git_path = 'git://git.debian.org/git/pkg-exppsy/pynifti.git'
+# because I am working locally for the moment
+git_path = '/home/mb312/dev_trees/pynifti/.git'
 
-gitpath = '/home/mb312/dev_trees/pynifti/.git'
 
-outpath = tempfile.mkdtemp()
-os.chdir(outpath)
-caller('git clone ' + gitpath)
-os.chdir('pynifti')
-caller('git archive origin/mb/brifti nifti > nifti.tar')
-os.chdir('..')
-caller('tar xvf pynifti/nifti.tar')
-shutil.rmtree('pynifti')
-os.makedirs(pjoin('nipy', 'io'))
-file(pjoin('nipy', '__init__.py'), 'wt').write('\n')
-file(pjoin('nipy', 'io', '__init__.py'), 'wt').write('\n')
-os.rename('nifti', pjoin('nipy','io','imageformats'))
+def create_archive(out_path, git_path, git_id):
+    pwd = os.path.abspath(os.curdir)
+    # pull out git archive
+    tmp_path = tempfile.mkdtemp()
+    os.chdir(tmp_path)
+    caller('git clone ' + git_path)
+    # extract nifti library tree from git archive
+    os.chdir('pynifti')
+    caller('git archive %s nifti > nifti.tar' % git_id)
+    os.chdir(tmp_path)
+    caller('tar xvf pynifti/nifti.tar')
+    shutil.rmtree('pynifti')
+    # create path structure for moved imports
+    os.makedirs(pjoin('nipy', 'io'))
+    file(pjoin('nipy', '__init__.py'), 'wt').write('\n')
+    file(pjoin('nipy', 'io', '__init__.py'), 'wt').write('\n')
+    os.rename('nifti', pjoin('nipy','io','imageformats'))
+    # do search and replace for imports to change to NIPY ones
+    for root, dirs, files in os.walk('nipy'):
+        for fname in files:
+            if not fname.endswith('.py'):
+                continue
+            fpath = os.path.join(root, fname)
+            lines = file(fpath).readlines()
+            outfile = file(fpath, 'wt')
+            for line in lines:
+                for regexp, repstr in subs:
+                    if regexp.search(line):
+                        line = regexp.sub(repstr, line)
+                        continue
+                outfile.write(line)
+            outfile.close()
+    # make archive for later use
+    os.chdir(pjoin('nipy','io'))
+    caller('tar zcvf %s imageformats' % out_path)
+    os.chdir(tmp_path)
+    # run tests with new import names, bomb out if any fail
+    sys.path.insert(0, os.curdir)
+    if not nose.run(argv=[os.curdir, '--with-doctest']):
+        os.unlink(out_path)
+        raise OSError('Tests failed, please check ' + tmp_path)
+    os.chdir(pwd)
+    shutil.rmtree(tmp_path)
+    
 
-# do search and replace for imports to change to NIPY ones
-for root, dirs, files in os.walk('nipy'):
-    for fname in files:
-        if not fname.endswith('.py'):
-            continue
-        fpath = os.path.join(root, fname)
-        lines = file(fpath).readlines()
-        outfile = file(fpath, 'wt')
-        for line in lines:
-            for regexp, repstr in subs:
-                if regexp.search(line):
-                    line = regexp.sub(repstr, line)
-                    continue
-            outfile.write(line)
-        outfile.close()
-
-# run tests with new import names
-sys.path.insert(0, os.curdir)
-if not nose.run(argv=[os.curdir, '--with-doctest']):
-    raise OSError('Tests failed, please check ' + outpath)
-
-os.chdir('nipy/io')
-caller('tar zcvf imageformats.tar.gz imageformats')
-print outpath
+if __name__ == '__main__':
+    try:
+        out_path = sys.argv[1]
+    except IndexError:
+        raise OSError('Need archive outpath path as input')
+    try:
+        git_id = sys.argv[2]
+    except IndexError:
+        git_id = 'brifti-0.1'
+    try:
+        git_path = sys.argv[3]
+    except IndexError:
+        pass
+    create_archive(out_path, git_path, git_id)
