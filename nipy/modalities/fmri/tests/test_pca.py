cb24c6f24e441973376c2f01e514e9f8710c6381
Jonathan Taylor <jonathan.taylor@stanford.edu>
2009-06-12 03:05:39 -0400

    refactoring fmri.pca module, no class, just one function

--- nipy/modalities/fmri/tests/test_pca.py
+++ nipy/modalities/fmri/tests/test_pca.py
@@ -1,70 +1,138 @@
 import numpy as np
 
 from nipy.modalities.fmri.api import FmriImageList
-from nipy.modalities.fmri.pca import PCA
+from nipy.modalities.fmri.pca import pca
 from nipy.core.api import Image
 from nipy.io.api import  load_image
-from nipy.testing import funcfile, TestCase, dec
+import nipy.testing as niptest
+
+data = {}
+
+def setup():
+    img = load_image(niptest.funcfile)
+    data['fmridata'] = FmriImageList.from_image(img)
+    frame = data['fmridata'][0]
+    data['mask'] = Image(np.greater(np.asarray(frame), 500).astype(np.float64),
+                         frame.coordmap)
+    print data['mask'].shape, np.sum(np.array(data['mask']))
+
+def test_PCAMask():
+    nimages = len(data['fmridata'].list) 
+    ntotal = nimages - 1
+    ncomp = 5
+    p = pca(data['fmridata'], data['mask'], ncomp=ncomp)
+
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
+
+def test_PCAMask_nostandardize():
+    nimages = len(data['fmridata'].list) 
+    ntotal = nimages - 1
+    ncomp = 5
+    p = pca(data['fmridata'], data['mask'], ncomp=ncomp, standardize=False)
+
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 
-class test_PCA(TestCase):
-    def setUp(self):
-        self.img = load_image(funcfile)
-        self.fmridata = FmriImageList.from_image(self.img)
+def test_PCANoMask():
+    nimages = len(data['fmridata'].list) 
+    ntotal = nimages - 1
+    ncomp = 5
+    p = pca(data['fmridata'], ncomp=ncomp)
 
-        frame = self.fmridata[0]
-        self.mask = Image(np.greater(np.asarray(frame), 500).astype(np.float64),
-                          frame.coordmap)
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
 
-class test_PCAMask(test_PCA):
-    # FIXME: Fix slice_iterator errors in pca module.
-    @dec.knownfailure
-    def test_PCAmask(self):
-        p = PCA(self.fmridata, self.mask)
-        p.fit()
-        output = p.images(which=range(4))
+def test_PCANoMask_nostandardize():
+    nimages = len(data['fmridata'].list) 
+    ntotal = nimages - 1
+    ncomp = 5
+    p = pca(data['fmridata'], ncomp=ncomp, standardize=False)
+
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
+
+def test_keep():
+    """
+    Data is projected onto k=10 dimensional subspace
+    then has its mean removed.
+    Should still have rank 10.
+
+    """
+    k = 10
+    ncomp = 5
+    nimages = len(data['fmridata'].list) 
+    ntotal = k
+    X = np.random.standard_normal((nimages, k))
+    p = pca(data['fmridata'], ncomp=ncomp, design_keep=X)
+
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
+
+def test_resid():
+    """
+    Data is projected onto k=10 dimensional subspace
+    then has its mean removed.
+    Should still have rank 10.
+
+    """
+    k = 10
+    ncomp = 5
+    nimages = len(data['fmridata'].list) 
+    ntotal = k
+    X = np.random.standard_normal((nimages, k))
+    p = pca(data['fmridata'], ncomp=ncomp, design_resid=X)
+
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 
-class test_PCANoMask(test_PCA):
-    # FIXME: Fix slice_iterator errors in pca modules.
-    @dec.knownfailure
-    @dec.slow
-    @dec.data
-    def test_PCA(self):
-        p = PCA(self.fmridata)
-        p.fit()
-        output = p.images(which=range(4))
 
-# FIXME: Figure out good automated test to replace these graphical tests.
-"""
-class test_PCAMontageNoMask(test_PCA):
-    @dec.slow
-    @dec.data
-    def test_PCAmontage(self):
-        from nipy.modalities.fmri.pca import PCAmontage
-        from pylab import savefig
-        p = PCAmontage(self.fmridata)
-        p.fit()
-        output = p.images(which=range(4))
-        p.time_series()
-        p.montage()
-        savefig('image.png')
-        os.remove('image.png')
 
-class test_PCAMontageMask(test_PCA):
-    @dec.slow
-    @dec.data
-    def test_PCAmontage_nomask(self):
-        from nipy.modalities.fmri.pca import PCAmontage
-        from pylab import savefig
-        p = PCAmontage(self.fmridata, mask=self.mask)
-        p.fit()
-        output = p.images(which=range(4))
-        p.time_series()
-        p.montage()
-        savefig('image.png')
-        os.remove('image.png')
-"""
+
+def test_both():
+    k1 = 10
+    k2 = 8
+    ncomp = 5
+    nimages = len(data['fmridata'].list) 
+    ntotal = k1
+    X1 = np.random.standard_normal((nimages, k1))
+    X2 = np.random.standard_normal((nimages, k2))
+    p = pca(data['fmridata'], ncomp=ncomp, design_resid=X2, design_keep=X1)
+
+    yield niptest.assert_equal, p['rank'], ntotal
+    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
+                                                         nimages)
+    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
+    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
+
+
 
 
 
