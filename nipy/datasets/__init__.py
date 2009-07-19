105a42419a9faa39f9071ea546f50c6df74ff0cf
Gael Varoquaux <gael.varoquaux@normalesup.org>
2009-07-20 00:29:27 +0200

    Change __init__ to match renaming

--- nipy/datasets/__init__.py
+++ nipy/datasets/__init__.py
@@ -3,7 +3,7 @@ Package containing core nipy classes.
 """
 __docformat__ = 'restructuredtext'
 
-from .volumes.base_image import BaseImage
+from .volumes.volume_field import VolumeField
 from .volumes.volume_image import VolumeImage
 from .transforms.transform import Transform
 from .transforms.affine_transform import AffineTransform
