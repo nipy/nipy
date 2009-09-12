96cd26d43a47310e77d835c79848fa292f0583dd
Bertrand Thirion <thirion@is143471>
2009-09-12 23:23:13 +0200

    added some tests and corrected utils functions

--- nipy/neurospin/utils/threshold.py
+++ nipy/neurospin/utils/threshold.py
@@ -16,8 +16,52 @@ from nipy.io.imageformats import load, save, Nifti1Image
 #        2) the test at the end of the code should be converted in a unit 
 #           test (requires 1).
 
+def threshold_array(data_array, mask_array=None, th=0., smin=0, nn=18):
+    """
+    Array thresholding facility    
 
-def threshold_scalar_image(iimage, oimage=None, th=0., smin=0, mask_image=None):
+    Parameters
+    ----------
+    data_array, array 
+                the data to threshold in its positions
+    mask_array=None, array
+                     the positsion to consider within data_array
+                     if None, all teh original image is considered
+    th=0., float , teh scalar threshold to apply
+    smin=0, int, cluster size threshold
+    nn=18, int spatial neighboring system: 6,18 or 26
+
+    Returns
+    -------
+    thx array of shape (sum(mask)) that yields the values that remain
+    """
+    if mask_array==None:
+        mask_array = np.ones(np.shape(data_array))
+    if np.shape(data_array)!=np.shape(mask_array):
+        raise ValueError, "mask_array and data_array do not have the same shape"
+    data_array = data_array[mask_array>0]
+    xyz = np.array(np.where(mask_array>0)).T.astype(np.int)
+
+    #2. threshold the map
+    nvox = np.sum(mask_array>0)
+    thx = np.zeros(nvox)
+    supvox = np.sum(data_array>th)
+    if supvox>0:
+        # 2.a make a field based onthe thresholded image
+        thim = ff.Field(supvox)
+        thim.from_3d_grid(xyz[data_array>th])
+
+        # 2.b get the cc and their size
+        u = thim.cc()
+        nsu = np.array([np.sum([u==i]) for i in range(u.max()+1)])
+        invalid = nsu<smin
+        u[invalid[u]]=-1
+        
+        # 2.c reorder u and write the final label map
+        thx[data_array>th] = data_array[data_array>th]*(u>-1) 
+    return thx
+
+def threshold_scalar_image(iimage, oimage=None, th=0., smin=0, nn=18, mask_image=None):
     """
     this function takes a 'grey level' threshold and a size threshold
     and gives as output an image where only the suprathreshold component
@@ -30,6 +74,7 @@ def threshold_scalar_image(iimage, oimage=None, th=0., smin=0, mask_image=None):
                  if None teh output image is not written
     th=0., float,  the chosen trheshold
     smin=0, int, cluster size threshold
+    nn=18, int spatial neighboring system: 6,18 or 26
     mask_image=None: a mask image to determine where in image this applies
                      if mask_image==None, the function is implied on 
                      where(image)
@@ -42,50 +87,88 @@ def threshold_scalar_image(iimage, oimage=None, th=0., smin=0, mask_image=None):
     """
     # fixme : add a header check here
     # 1. read the input image
-    inim = load(iimage)
-    ref_dim = inim.get_shape()
-    x = inim.get_data()
+    
     if mask_image==None:
-        nvox = np.size(np.nonzero(x))
-        xyz = np.array(np.where(x)).T.astype(np.int)
-        m = (x!=0)
+        m = None
     else:
         mask = load(mask_image)
         m = mask.get_data()
 
-    nvox = np.sum(m>0)
-    x = x[np.where(m)]
-    xyz = np.array(np.where(m)).T.astype(np.int)
-
-    #2. threshold the map
-    thx = np.zeros(nvox)
-    supvox = np.sum(x>th)
-    if supvox>0:
-        # 2.a make a field based onthe thresholded image
-        thim = ff.Field(supvox)
-        thim.from_3d_grid(xyz[x>th])
-
-        # 2.b get the cc and their size
-        u = thim.cc()
-        nsu = np.array([np.sum([u==i]) for i in range(u.max()+1)])
-        invalid = nsu<smin
-        u[invalid[u]]=-1
-        
-        # 2.c reorder u and write the final label map
-        thx[x>th] = x[x>th]*(u>-1) 
-
+    inim = load(iimage)    
+    x = inim.get_data()
+    
+    thx = threshold_array(x, m, th, smin, nn=nn)
+    
+    ref_dim = inim.get_shape()
     result = np.zeros(ref_dim)
     result[m>0] = thx
     onim = Nifti1Image(result.T,inim.get_affine())	
-    onim.get_header()['descrip']= "thresholded image, threshold= %f,\ 
-                                  cluster size=%d"%(th,smin)
+    onim.get_header()['descrip']= "thresholded image, threshold= %f,\
+                                   cluster size=%d"%(th,smin)
     if oimage !=None:
        save(onim, oimage)	
     return onim
 
+def threshold_z_array(data_array, mask_array=None, correction=None, 
+                                  pval=None, smin=0, nn=18, method=None):
+    """
+    threshold an array at a secified p-value, with/out correction 
+    for multiple compaison
+    
+    Parameters
+    ----------
+    data_array, array 
+                the data to threshold in its positions
+    mask_array=None, array
+                     the position to consider within data_array
+                     if None, all teh original image is considered
+    correction=None, string  the correction for multiple comparison method
+               correction can be either None or 'bon' (Bonferroni) or 'fdr'
+    pval=none, float, the desired classical p-value.
+               the default behaviour of pval depends on correction
+               if correction==None, pval = 0.001, else pval = 0.05
+    smin=0, int, the  cluster size threshold
+    method=None: model of the null distribution:
+                 if method==None: standard null
+                 if method=='emp': empirical null
+    
+    """
+    if mask_array==None:
+        mask_array = np.ones(np.shape(data_array))
+    if np.shape(data_array)!=np.shape(mask_array):
+        raise ValueError, "mask_array and data_array do not have\
+                          the same shape"
+    nvox = np.sum(mask_array)    
 
-def threshold_z_image(iimage, oimage=None, corr=None, pval=None, smin=0, 
-            mask_image=None, method=None):
+    # 1. determine the threshold in z-variate
+    #1.a determine the used-pval
+    if pval==None:
+        pval=0.001
+        if correction=='bon':pval=0.05
+        if correction=='fdr':pval=0.05
+
+    # requires nvox !
+    if correction=='bon':
+        pval/=nvox
+    th = st.norm.isf(pval)
+    
+    # requires the  data
+    if correction=='fdr':
+        if method=='emp':
+            from emp_null import ENN
+            efdr = ENN(data_array[mask_array>0])
+            th = efdr.threshold(pval,verbose=1)
+        else:
+            from emp_null import FDR
+            lf = FDR(data_array[mask_array>0])
+            th = lf.threshold(pval)
+    print 'the threhsold is:', th
+
+    # 3. threshold the image 
+    return threshold_array(data_array, mask_array, th, smin, nn)
+
+def threshold_z_image(iimage, oimage=None, correction=None, pval=None, smin=0, 
+            nn=18, mask_image=None, method=None):
     """
     this function takes a presumably gaussian image threshold and a
     size threshold and gives as output an image where only the
@@ -98,11 +181,11 @@ def threshold_z_image(iimage, oimage=None, corr=None, pval=None, smin=0,
     ----------
     iimage, string, the path of a presumably z-variate input image
     oimage=None, string, the path of the output image
-    corr=None, string  the correction for multiple comparison method
-               corr can be either None or 'bon' (Bonferroni) or 'fdr'
+    correction=None, string  the correction for multiple comparison method
+               correction can be either None or 'bon' (Bonferroni) or 'fdr'
     pval=none, float, the desired classical p-value.
-               the default behaviour of pval depends on corr
-               if corr==None, pval = 0.001, else pval = 0.05
+               the default behaviour of pval depends on correction
+               if correction==None, pval = 0.001, else pval = 0.05
     smin=0, int, the  cluster size threshold
     mask_image=None, string path of a mask image to determine 
                      where thresholding is  applies
@@ -117,82 +200,27 @@ def threshold_z_image(iimage, oimage=None, corr=None, pval=None, smin=0,
     oimage: the output image
     """
     #?# 1.  read the image(s)
-    nim = load(iimage)
-    x = nim.get_data()
     if mask_image==None:
-        nvox = np.size(np.nonzero(x)) 
-        x = x[np.where(x)]
+        m = None
     else:
         mask = load(mask_image)
         m = mask.get_data()
-        nvox = np.sum(m>0)
-        x = x[np.where(m)]
+    nim = load(iimage)
+    x = nim.get_data()
         
-    # 2. determine the threshold in z-variate
-    #2.a determine the used-pval
-    if pval==None:
-        pval=0.001
-        if corr=='bon':pval=0.05
-        if corr=='fdr':pval=0.05
-
-    # requires nvox !
-    if corr=='bon':
-        pval/=nvox
-    th = st.norm.isf(pval)
+    thx = threshold_z_array(x, m, correction, pval, smin, nn, method)
     
-    # requires the  data
-    if corr=='fdr':
-        if method=='emp':
-            from emp_null import ENN
-            efdr = ENN(x)
-            th = efdr.threshold(pval,verbose=1)
-        else:
-            from emp_null import FDR
-            lf = FDR(x)
-            th = lf.threshold(pval)
-            print th
+    ref_dim = inim.get_shape()
+    result = np.zeros(ref_dim)
+    result[m>0] = thx
+    onim = Nifti1Image(result.T,inim.get_affine())	
+    onim.get_header()['descrip']= "thresholded image, threshold= %f,\
+                                   cluster size=%d"%(th,smin)
+    if oimage !=None:
+       save(onim, oimage)	
+    return onim
 
-    # 3. threshold the image 
     output_image = threshold_scalar_image(iimage, oimage, th=th, smin=smin,
                         mask_image=mask_image)
     return output_image
-
-
-
-
-def _test_():
-
-    from  os.path import join
-
-    # Get the data
-    nbru = range(1,13)
-    nbeta = [29]
-    theta = float(st.t.isf(0.01,100))
-    smin = 25
-    swd = "/tmp/"
-    
-    # a mask of the brain in each subject
-    Mask_Images =["/volatile/thirion/Localizer/sujet%02d/functional/fMRI/spm_analysis_RNorm_S/mask.img" % bru for bru in nbru]
-    #Mask_Images =["/data/thirion/Localizer/sujet%02d/functional/fMRI/spm_analysis_RNorm_S/mask.img" % bru for bru in nbru]
-    
-    # activation image in each subject
-    betas = [["/volatile/thirion/Localizer/sujet%02d/functional/fMRI/spm_analysis_RNorm_S/spmT_%04d.img" % (bru, n) for n in nbeta] for bru in nbru]
-    #betas = [["/data/thirion/Localizer/sujet%02d/functional/fMRI/spm_analysis_RNorm_S/spmT_%04d.img" % (bru, n) for n in nbeta] for bru in nbru]
-
-
-    oimage = join(swd,'toto1.nii')
-    threshold_scalar_image(betas[0][0],oimage,th=theta,smin=smin)
-    oimage = join(swd,'toto2.nii')
-    threshold_scalar_image(betas[0][0],oimage,th=theta,smin=smin,mask_image=Mask_Images[0])
-    oimage = join(swd,'toto3.nii')
-    threshold_z_image(betas[0][0],oimage,corr=None,smin=5)
-    oimage = join(swd,'toto4.nii')
-    threshold_z_image(betas[0][0],oimage,corr='fdr',smin=5)
-    oimage = join(swd,'toto4.nii')
-    threshold_z_image(betas[0][0],oimage,corr='fdr',smin=5,pval=0.2)
-    oimage = join(swd,'toto5.nii')
-    threshold_z_image(betas[0][0],oimage,corr='fdr',pval=0.1,smin=5,method='emp')
-    oimage = join(swd,'toto6.nii')
-    threshold_z_image(betas[0][0],oimage,corr='bon',smin=5)
-
 
