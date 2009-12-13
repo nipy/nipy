fb3187129c676338a56910a6cf42653b65288725
Matthew Brett <mb312@blair.local>
2009-12-12 23:09:31 -0500

    Refactored PCA to generalize axis

--- nipy/modalities/fmri/pca.py
+++ nipy/modalities/fmri/pca.py
@@ -12,66 +12,51 @@ More specifically, the data is projected onto the eigenvectors of the
 covariance matrix.
 """
 
-__docformat__ = 'restructuredtext'
-
 import numpy as np
-import numpy.linalg as L
+import numpy.linalg as npl
 from nipy.fixes.scipy.stats.models.utils import pos_recipr
 
-def pca(data, mask=None, ncomp=1, standardize=True,
+
+def pca(data, axis=-1, mask=None, ncomp=None, standardize=True,
         design_keep=None, design_resid=None):
     """
-    Compute the PCA of an array-like thing over ``axis=0``.
-    We use np.array to cast to an ndarray.
+    Compute the PCA of an array-like thing over `axis`.
 
     Parameters
     ----------
-
     data : ndarray-like (np.float)
-        The image on which to perform PCA over its first axis.
-
+       The array on which to perform PCA over axis `axis` (below)
+    axis : int
+       The axis over which to perform PCA (axis identifying
+       observations).  Default is -1 (last)
     mask : ndarray-like (np.bool)
-        An optional mask, should have shape == image.shape[1:]
-
-    ncomp : int
-        How many components to return. All the time series
-        are returned but only ncomp of the images are computed.
-
+       An optional mask, should have shape given by data axes, with
+       `axis` removed, i.e.: ``s = data.shape; s.pop(axis); msk_shape=s``
+    ncomp : {None, int}
+       How many components to return. If ncomp is None (the default)
+       then the number of components is given by the calculated rank of
+       the data, after applying `design_keep` and `design_resid` below
     standardize : bool
-        Standardize so each time series has same error-sum-of-squares?
-
+       Standardize so each time series has same error-sum-of-squares?
     design_keep : ndarray
-        Data is projected onto the column span of design_keep.
-        Defaults to np.identity(data.shape[0])
-
+       Data is projected onto the column span of design_keep.
+       Defaults to ``np.identity(data.shape[axis])``
     design_resid : ndarray
-        After projecting onto the column span of design_keep, data is
-        projected perpendicular to the column span of this matrix.
-        Defaults to a matrix of 1s, removing the mean.
-
+       After projecting onto the column span of design_keep, data is
+       projected perpendicular to the column span of this matrix.
+       Defaults to a matrix of 1s, removing the mean.
     """
     data = np.asarray(data)
+    # We roll the PCA axis to be first, for convenience
+    if axis is None:
+        raise ValueError('axis cannot be None')
+    data = np.rollaxis(data, axis)
     if mask is not None:
         mask = np.asarray(mask)
-
-    if mask is not None:
-        nvoxel = mask.sum()
-    else:
-        nvoxel = np.product(data.shape[1:])
-
     nimages = data.shape[0]
-
-    if design_keep is not None:
-        pinv_design_keep = L.pinv(design_keep)
-        def project_keep(Y):
-            return np.dot(np.dot(design_keep, pinv_design_keep), Y)
-    else:
-        def project_keep(Y):
-            return Y
-
     if design_resid is None:
         design_resid = np.ones((data.shape[0], 1))
-    pinv_design_resid = L.pinv(design_resid)
+    pinv_design_resid = npl.pinv(design_resid)
 
     def project_resid(Y):
         return Y - np.dot(np.dot(design_resid, pinv_design_resid), Y)
@@ -90,69 +75,57 @@ def pca(data, mask=None, ncomp=1, standardize=True,
     design_resid.
 
     """
-
     if design_keep is None:
         design_keep = np.identity(nimages)
-
-    X = np.dot(design_keep, L.pinv(design_keep))
-    XZ = X - np.dot(design_resid, np.dot(L.pinv(design_resid), X))
-    UX, SX, VX = L.svd(XZ, full_matrices=0)
-
+    X = np.dot(design_keep, npl.pinv(design_keep))
+    XZ = X - np.dot(design_resid, np.dot(npl.pinv(design_resid), X))
+    UX, SX, VX = npl.svd(XZ, full_matrices=0)
     # The matrix UX has orthonormal columns and represents the
     # final "column space" that the data will be projected onto.
-
     rank = np.greater(SX/SX.max(), 0.01).astype(np.int32).sum()
     UX = UX[:,range(rank)].T
-
     C = np.zeros((rank, rank))
     for i in range(data.shape[1]):
         Y = data[:,i].reshape((data.shape[0], np.product(data.shape[2:])))
         YX = np.dot(UX, Y)
-
         if standardize:
             S2 = (project_resid(Y)**2).sum(0)
             Smhalf = pos_recipr(np.sqrt(S2)); del(S2)
             YX *= Smhalf
-
         if mask is not None:
             YX = YX * np.nan_to_num(mask[i].reshape(Y.shape[1]))
-
         C += np.dot(YX, YX.T)
-
-    D, Vs = L.eigh(C)
+    D, Vs = npl.eigh(C)
     order = np.argsort(-D)
     D = D[order]
     pcntvar = D * 100 / D.sum()
-
     time_series = np.dot(UX.T, Vs).T[order]
-
     """
-    Output the component images -- by default, we only output the first
-    principal component.
-
+    Output the component images
     """
-
+    if ncomp is None:
+        ncomp = rank
     subVX = time_series[:ncomp]
-
     out = np.empty((ncomp,) + data.shape[1:], np.float)
     for i in range(data.shape[1]):
         Y = data[:,i].reshape((data.shape[0], np.product(data.shape[2:])))
         U = np.dot(subVX, Y)
-
         if standardize:
             S2 = (project_resid(Y)**2).sum(0)
             Smhalf = pos_recipr(np.sqrt(S2)); del(S2)
             YX *= Smhalf
-
         if mask is not None:
             YX *= np.nan_to_num(mask[i].reshape(Y.shape[1]))
-
         U.shape = (U.shape[0],) + data.shape[2:]
         out[:,i] = U
-    return {'time_series':time_series[:ncomp,],
+    # Roll PCA image axis back to original position in data array
+    if axis < 0:
+        axis += data.ndim
+    out = np.rollaxis(out, 0, axis+1)
+    return {'time_series': time_series[:ncomp,],
             'pcnt_var': pcntvar,
-            'images':out, 
-            'rank':rank}
+            'images': out, 
+            'rank': rank}
 
 
 
--- nipy/modalities/fmri/tests/test_pca.py
+++ nipy/modalities/fmri/tests/test_pca.py
@@ -1,113 +1,120 @@
 import numpy as np
 
-from nipy.modalities.fmri.api import FmriImageList
 from nipy.modalities.fmri.pca import pca
-from nipy.core.api import Image
 from nipy.io.api import  load_image
-import nipy.testing as niptest
+from nipy.testing import assert_equal, assert_almost_equal, \
+    assert_array_almost_equal, funcfile, parametric, \
+    assert_array_equal, assert_raises
+
 
 data = {}
 
+
 def setup():
-    img = load_image(niptest.funcfile)
+    img = load_image(funcfile)
     arr = np.array(img)
-    arr = np.rollaxis(arr, 3)
-    data['nimages'] = arr.shape[0]
+    #arr = np.rollaxis(arr, 3)
+    data['nimages'] = arr.shape[3]
     data['fmridata'] = arr
-    frame = data['fmridata'][0]
+    frame = data['fmridata'][...,0]
     data['mask'] = (frame > 500).astype(np.float64)
+
+
+@parametric
+def test_input_effects():
+    ntotal = data['nimages'] - 1
+    # return full rank PCA over last axis by default
+    p = pca(data['fmridata'])
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ntotal, data['nimages'])
+    yield assert_equal, p['images'].shape, data['mask'].shape + (ntotal,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    # same effect if over axis 0
+    arr = data['fmridata']
+    arr = np.rollaxis(arr, -1)
+    pr = pca(data['fmridata'], 0)
+    out_arr = np.rollaxis(pr['images'], 0, 3)
+    yield assert_array_equal, out_arr, p['images']
+    yield assert_array_equal, p['time_series'], pr['time_series']
+    yield assert_array_equal, p['pcnt_var'], pr['pcnt_var']
+    # Check axis None raises error
+    yield assert_raises, ValueError, pca, data['fmridata'], None
 
 
 def test_PCAMask():
     ntotal = data['nimages'] - 1
     ncomp = 5
-    p = pca(data['fmridata'], data['mask'], ncomp=ncomp)
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
+    p = pca(data['fmridata'], -1, data['mask'], ncomp=ncomp)
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape, data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
+    
 
 def test_PCAMask_nostandardize():
     ntotal = data['nimages'] - 1
     ncomp = 5
-    p = pca(data['fmridata'], data['mask'], ncomp=ncomp, standardize=False)
-
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
+    p = pca(data['fmridata'], -1, data['mask'], ncomp=ncomp,
+            standardize=False)
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape, data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 
 def test_PCANoMask():
     ntotal = data['nimages'] - 1
     ncomp = 5
     p = pca(data['fmridata'], ncomp=ncomp)
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape,  data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
 
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 def test_PCANoMask_nostandardize():
     ntotal = data['nimages'] - 1
     ncomp = 5
     p = pca(data['fmridata'], ncomp=ncomp, standardize=False)
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape,  data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
 
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 def test_keep():
-    """
-    Data is projected onto k=10 dimensional subspace
-    then has its mean removed.
-    Should still have rank 10.
-
-    """
+    # Data is projected onto k=10 dimensional subspace
+    # then has its mean removed.
+    # Should still have rank 10.
     k = 10
     ncomp = 5
     ntotal = k
     X = np.random.standard_normal((data['nimages'], k))
     p = pca(data['fmridata'], ncomp=ncomp, design_keep=X)
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape,  data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
 
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 def test_resid():
-    """
-    Data is projected onto k=10 dimensional subspace
-    then has its mean removed.
-    Should still have rank 10.
-
-    """
+    # Data is projected onto k=10 dimensional subspace then has its mean
+    # removed.  Should still have rank 10.
     k = 10
     ncomp = 5
     ntotal = k
     X = np.random.standard_normal((data['nimages'], k))
     p = pca(data['fmridata'], ncomp=ncomp, design_resid=X)
-
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
-
-
-
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape, data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
 
 
 def test_both():
@@ -118,18 +125,8 @@ def test_both():
     X1 = np.random.standard_normal((data['nimages'], k1))
     X2 = np.random.standard_normal((data['nimages'], k2))
     p = pca(data['fmridata'], ncomp=ncomp, design_resid=X2, design_keep=X1)
-
-    yield niptest.assert_equal, p['rank'], ntotal
-    yield niptest.assert_equal, p['time_series'].shape, (ncomp,
-                                                         data['nimages'])
-    yield niptest.assert_equal, p['images'].shape, (ncomp,) + data['mask'].shape
-    yield niptest.assert_equal, p['pcnt_var'].shape, (ntotal,)
-    yield niptest.assert_almost_equal, p['pcnt_var'].sum(), 100.
-
-
-
-
-
-        
-
-
+    yield assert_equal, p['rank'], ntotal
+    yield assert_equal, p['time_series'].shape, (ncomp, data['nimages'])
+    yield assert_equal, p['images'].shape,  data['mask'].shape + (ncomp,)
+    yield assert_equal, p['pcnt_var'].shape, (ntotal,)
+    yield assert_almost_equal, p['pcnt_var'].sum(), 100.
