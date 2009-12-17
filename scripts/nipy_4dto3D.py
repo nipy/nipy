51701b35d9ef9401abf0d86fd5726e669326390d
Matthew Brett <mb312@blair.local>
2009-12-17 16:02:53 -0500

    Use argparse for 4D to 3D

--- scripts/nipy_4dto3D.py
+++ scripts/nipy_4dto3D.py
@@ -3,22 +3,30 @@
 analyze, MINC, at the moment, as nifti 3D files '''
 
 import os
-import sys
 
+import nipy.externals.argparse as argparse
 import nipy.io.imageformats as nii
 
 
-if __name__ == '__main__':
-    try:
-        fname = sys.argv[1]
-    except IndexError:
-        raise OSError('Expecting 4d image filename')
-    img = nii.load(fname)
+def main():
+    # create the parser
+    parser = argparse.ArgumentParser()
+    # add the arguments
+    parser.add_argument('filename', type=str,
+                        help='4D image filename')
+    # parse the command line
+    args = parser.parse_args()
+    img = nii.load(args.filename)
     imgs = nii.four_to_three(img)
-    froot, ext = os.path.splitext(fname)
+    froot, ext = os.path.splitext(args.filename)
     if ext in ('.gz', '.bz2'):
         froot, ext = os.path.splitext(froot)
     for i, img3d in enumerate(imgs):
         fname3d = '%s_%04d.nii' % (froot, i)
         nii.save(img3d, fname3d)
+
+
+if __name__ == '__main__':
+    main()
+
         
