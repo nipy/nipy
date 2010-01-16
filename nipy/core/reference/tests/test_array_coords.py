811b6765485cb93a147b3bf380701ef161ec64ff
Matthew Brett <mb312@blair.lan>
2010-01-16 18:00:33 +0000

    Commenting on array_coords test

--- nipy/core/reference/tests/test_array_coords.py
+++ nipy/core/reference/tests/test_array_coords.py
@@ -18,6 +18,12 @@ import nipy.core.reference.array_coords as acs
 
 @parametric
 def test_array_coord_map():
+    # array coord map recreates the affine when you slice an image.  In
+    # general, if you take an integer slice in some dimension, the
+    # corresponding column of the affine will go, leaving a row for the
+    # lost dimension, with all zeros, execpt for the translation in the
+    # now-removed dimension, encoding the position of that particular
+    # slice
     xz = 1.1; yz = 2.3; zz = 3.5
     xt = 10.0; yt = 11; zt = 12
     aff = np.diag([xz, yz, zz, 1])
@@ -25,7 +31,10 @@ def test_array_coord_map():
     shape = (2,3,4)
     cmap = Affine.from_params('ijk', 'xyz', aff)
     acm = acs.ArrayCoordMap(cmap, shape)
+    # slice the coordinate map for the first axis
     sacm = acm[1]
+    # The affine has lost the first column, but has a remaining row (the
+    # first) encoding the translation to get to this slice
     yield assert_array_almost_equal(sacm.coordmap.affine,
                              np.array([
                 [0, 0, xz+xt],
@@ -33,6 +42,7 @@ def test_array_coord_map():
                 [0, zz, zt],
                 [0, 0, 1]]))
     sacm = acm[:,1]
+    # lost second column, remaining second row with translation
     yield assert_array_almost_equal(sacm.coordmap.affine,
                              np.array([
                 [xz, 0, xt],
@@ -40,12 +50,14 @@ def test_array_coord_map():
                 [0, zz, zt],
                 [0, 0, 1]]))
     sacm = acm[:,:,2]
+    # ditto third column and row
     yield assert_array_almost_equal(sacm.coordmap.affine,
                              np.array([
                 [xz, 0, xt],
                 [0, yz, yt],
                 [0, 0, 2*zz+zt],
                 [0, 0, 1]]))
+    # check ellipsis slicing is the same as [:,: ...
     sacm = acm[...,2]
     yield assert_array_almost_equal(sacm.coordmap.affine,
                              np.array([
@@ -53,6 +65,7 @@ def test_array_coord_map():
                 [0, yz, yt],
                 [0, 0, 2*zz+zt],
                 [0, 0, 1]]))
+    # that ellipsis can follow other slice types
     sacm = acm[:,...,2]
     yield assert_array_almost_equal(sacm.coordmap.affine,
                              np.array([
@@ -60,14 +73,11 @@ def test_array_coord_map():
                 [0, yz, yt],
                 [0, 0, 2*zz+zt],
                 [0, 0, 1]]))
+    # that there can be only one ellipsis
     yield assert_raises(ValueError, acm.__getitem__, (
             (Ellipsis,Ellipsis,2)))
-    yield assert_array_almost_equal(sacm.coordmap.affine,
-                             np.array([
-                [xz, 0, xt],
-                [0, yz, yt],
-                [0, 0, 2*zz+zt],
-                [0, 0, 1]]))
+    # that you can integer slice in all three dimensions, leaving only
+    # the translation column
     sacm = acm[1,0,2]
     yield assert_array_almost_equal(sacm.coordmap.affine,
                              np.array([
