6961942dc2415c1d4f1986ccc1159230e462963f
Gael Varoquaux <gael.varoquaux@normalesup.org>
2010-01-17 14:31:08 +0100

    ENH: Cleanup: separate tests in different file.

--- nipy/neurospin/viz/coord_tools.py
+++ nipy/neurospin/viz/coord_tools.py
@@ -80,24 +80,6 @@ def coord_transform(x, y, z, affine):
     return x.squeeze(), y.squeeze(), z.squeeze()
 
 
-def test_coord_transform_trivial():
-    sform = np.eye(4)
-    x = np.random.random((10,))
-    y = np.random.random((10,))
-    z = np.random.random((10,))
-
-    x_, y_, z_ = coord_transform(x, y, z, sform)
-    np.testing.assert_array_equal(x, x_)
-    np.testing.assert_array_equal(y, y_)
-    np.testing.assert_array_equal(z, z_)
-
-    sform[:, -1] = 1
-    x_, y_, z_ = coord_transform(x, y, z, sform)
-    np.testing.assert_array_equal(x+1, x_)
-    np.testing.assert_array_equal(y+1, y_)
-    np.testing.assert_array_equal(z+1, z_)
-
-
 def find_activation(map, mask=None, pvalue=0.05, upper_only=False):
     """ Use the empirical normal null estimator to find threshold for 
         negative and positive activation.
@@ -169,16 +151,6 @@ def find_cut_coords(map, mask=None, activation_threshold=None):
             my_map[np.logical_not(largest_cc(my_map>second_threshold))] = 0
     cut_coords = ndimage.center_of_mass(my_map)
     return cut_coords
-
-
-def test_find_cut_coords():
-    map = np.zeros((100, 100, 100))
-    x_map, y_map, z_map = 50, 10, 40
-    map[x_map-30:x_map+30, y_map-3:y_map+3, z_map-10:z_map+10] = 1
-    x, y, z = find_cut_coords(map, mask=np.ones(map.shape, np.bool))
-    np.testing.assert_array_equal(
-                        (int(round(x)), int(round(y)), int(round(z))),
-                                (x_map, y_map, z_map))
 
 
 ################################################################################
