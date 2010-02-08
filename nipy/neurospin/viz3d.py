40f46858b7eb76517191466d9471e0e5ca564499
Gael Varoquaux <gael.varoquaux@normalesup.org>
2010-02-08 10:57:19 +0100

    BUG: Fix failing import

--- nipy/neurospin/viz3d.py
+++ nipy/neurospin/viz3d.py
@@ -1,5 +1,5 @@
 """
 3D plotting of neuroimaging volumes.
 """
-from .viz_tools.maps_3d import as_affine_img, plot_map_3d, plot_anat_3d
+from .viz_tools.maps_3d import affine_img_src, plot_map_3d, plot_anat_3d
 
