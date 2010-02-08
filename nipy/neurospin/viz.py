a523ea8bd932747f59986c5dc57a2fe74025018f
Gael Varoquaux <gael.varoquaux@normalesup.org>
2010-02-08 13:46:21 +0100

    BUG: Fix missing import

--- nipy/neurospin/viz.py
+++ nipy/neurospin/viz.py
@@ -4,6 +4,8 @@ Entry point for doing 2D visualization with NiPy.
 
 from .viz_tools import cm
 from .viz_tools.activation_maps import plot_map, demo_plot_map, auto_plot_map
-from .viz_tools.coord_tools import coord_transform
+
+# XXX: These should die
+from .viz_tools.coord_tools import coord_transform, find_cut_coords
 from .viz_tools.anat_cache import mni_sform
 
