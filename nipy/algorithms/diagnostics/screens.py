8d5ed1720290e9387fdd89fe2f89457aff627163
Matthew Brett <mb312@angela>
2010-02-23 19:16:52 -0800

    Debugged diagnose script

--- nipy/algorithms/diagnostics/screens.py
+++ nipy/algorithms/diagnostics/screens.py
@@ -1,10 +1,13 @@
 ''' Diagnostic 4d image screen '''
+from os.path import join as pjoin
 
 import numpy as np
 
 from ...core.api import Image, drop_io_dim, append_io_dim
+from ...io.api import load_image, save_image
 from ...modalities.fmri import pca
 from .timediff import time_slice_diffs
+from .tsdiffplot import plot_tsdiffs
 
 
 def screen(img4d, ncomp=10):
@@ -58,9 +61,57 @@ def screen(img4d, ncomp=10):
                                     axis=-1,
                                     standardize=False,
                                     ncomp=ncomp)
-    cmap_4d = append_io_dim(cmap_3d, 'l' , 'component')
+    cmap_4d = append_io_dim(cmap_3d, 'l' , 't')
     screen_res['pca'] = Image(screen_res['pca_res']['basis_projections'],
                               cmap_4d)
     # tsdiffana
     screen_res['ts_res'] = time_slice_diffs(data)
     return screen_res
+
+
+def write_screen_res(res, out_path, out_root, out_img_ext='.nii'):
+    ''' Write results from ``screen`` to disk as images
+
+    Parameters
+    ----------
+    res : dict
+       output from ``screen`` function
+    out_path : str
+       directory to which to write output images
+    out_root : str
+       part of filename between image-specific prefix and image-specific
+       extension to use for writing images
+    out_img_ext : str
+       extension (identifying image type) to which to write volume
+       images.
+
+    Returns
+    -------
+    None
+    '''
+    # import matplotlib
+    import matplotlib.pyplot as plt
+    # save images
+    for key in ('mean', 'min', 'max', 'std', 'pca'):
+        fname = pjoin(out_path, '%s_%s%s' % (key,
+                                             out_root,
+                                             out_img_ext))
+        save_image(res[key], fname)
+    # plot, save component time courses
+    ncomp = res['pca'].shape[-1]
+    vectors = res['pca_res']['basis_vectors']
+    plt.figure()
+    for c in range(ncomp):
+        plt.subplot(ncomp, 1, c+1)
+        plt.plot(vectors[:,c])
+        plt.axis('tight')
+    plt.savefig(pjoin(out_path, 'components_%s.png' % out_root))
+    plt.figure()
+    plt.plot(res['pca_res']['pcnt_var'])
+    plt.axis('tight')
+    plt.savefig(pjoin(out_path, 'pcnt_var_%s.png' % out_root))    
+    # plot tsdiffana
+    plt.figure()
+    axes = [plt.subplot(4, 1, i+1) for i in range(4)]
+    plot_tsdiffs(res['ts_res'], axes)
+    plt.savefig(pjoin(out_path, 'tsdiff_%s.png' % out_root))
--- scripts/nipy_diagnose.py
+++ scripts/nipy_diagnose.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python
-
 import os
-from os.path import join as pjoin
-import sys
 
 import numpy as np
 
+import nipy
 import nipy.externals.argparse as argparse
-import nipy.algorithms.diagnostics as nad
-from nipy.io.imageformats.filename_parser, splitext_addext
+import nipy.algorithms.diagnostics.screens as nads
+from nipy.io.imageformats.filename_parser import splitext_addext
 
 import matplotlib
 matplotlib.use('Agg')
@@ -19,34 +17,29 @@ if __name__ == '__main__':
     # create the parser
     parser = argparse.ArgumentParser()
     # add the arguments
-    parser.add_argument('image', type=str,
+    parser.add_argument('filename', type=str,
                         help='4D image filename')
-    parser.add_argument('--out-root', type=str,
-                        help='root for output image names')
+    parser.add_argument('--out-path', type=str,
+                        help='path for output image names')
+    parser.add_argument('--out-fname-prefix', type=str,
+                        help='prefix for output image names')
     parser.add_argument('--ncomponents', type=int, default=10,
                         help='number of PCA components to write')
     # parse the command line
     args = parser.parse_args()
     # process inputs
     filename = args.filename
-    out_root = args.out_root
+    out_path = args.out_path
+    out_root = args.out_fname_prefix
     ncomps = args.ncomponents
+    # collect extension for output images
+    froot, ext, gz = splitext_addext(filename)
+    pth, fname = os.path.split(froot)
+    if out_path is None:
+        out_path = pth
     if out_root is None:
-        out_root, ext, gz = splitext_addext(filename)
-    res = nad.screen(img, ncomps)
-    # save images
-    for key in 'mean', 'min', 'max', 'std', 'pca':
-    nii.save(mean, pjoin(pth, 'mean_' + fname))
-    nii.save(std, pjoin(pth, 'std_' + fname))
-    nii.save(pca, pjoin(pth, 'pca_' + fname))
-    # plot, save component time courses
-    plt.figure()
-    for c in range(ncomp):
-        plt.subplot(ncomp, 1, c+1)
-        plt.plot(vectors[:,c])
-    plt.savefig(pjoin(pth, 'components_%s.png' % froot))
-    # plot tsdiffana
-    plt.figure()
-    axes = [plt.subplot(4, 1, i+1) for i in range(4)]
-    nad.plot_tsdiffs(tsd, axes)
-    plt.savefig(pjoin(pth, 'tsdiff_%s.png' % froot))
+        out_root = fname
+    img = nipy.load_image(filename)
+    res = nads.screen(img, ncomps)
+    nads.write_screen_res(res, out_path, out_root, ext)
+    
