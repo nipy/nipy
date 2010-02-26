517c77d1eb258952c725da66e4a04a13c41f3d45
Christopher Burns <cburns@berkeley.edu>
2010-02-26 13:40:21 -0800

    Add more documentation to nipy_diagnose.

--- scripts/nipy_diagnose.py
+++ scripts/nipy_diagnose.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 ''' Script to calculate and write results for diagnostic screen
+
+nipy_diagnose will generate a series of diagnostic images for a 4D
+fMRI image volume.  The following images will be generated:
+
+    * components_<prefix>.png : plots of PCA basis vectors
+    * max_<prefix>.nii : max image
+    * mean_<prefix>.nii : mean image
+    * min_<prefix>.nii : min image
+    * pca_<prefix>.nii : 4D image of PCA component images
+    * pcnt_var_<prefix>.png : XXX
+    * std_<prefix>.nii : standard deviation image XXX
+    * tsdiff_<prefix>.png : XXX
+
+The generated files will be saved in the directory specified by the
+--out-path parameter.  If the out-path directory is not specified,
+generated images will be saved in the same directory as the input
+image.
+
 '''
 import os
 
@@ -14,19 +32,30 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-
-if __name__ == '__main__':
+def main():
     # create the parser
-    parser = argparse.ArgumentParser()
+    desc = __doc__.splitlines()[0]
+    parser = argparse.ArgumentParser(description=desc)
     # add the arguments
     parser.add_argument('filename', type=str,
                         help='4D image filename')
     parser.add_argument('--out-path', type=str,
-                        help='path for output image names')
+                        help='path for output image files')
     parser.add_argument('--out-fname-prefix', type=str,
                         help='prefix for output image names')
     parser.add_argument('--ncomponents', type=int, default=10,
                         help='number of PCA components to write')
+    parser.add_argument('--extended-help', action='store_true',
+                        help='print extended help')
+
+    # HACK: Allow user to get additional help without having to
+    # provide a filename (or any positional arg).  Could not find a
+    # better way to do this with argparse.
+    import sys
+    if '--extended-help' in sys.argv:
+        print __doc__
+        return
+
     # parse the command line
     args = parser.parse_args()
     # process inputs
@@ -45,3 +74,5 @@ if __name__ == '__main__':
     res = nads.screen(img, ncomps)
     nads.write_screen_res(res, out_path, out_root, ext + gz)
     
+if __name__ == '__main__':
+    main()
