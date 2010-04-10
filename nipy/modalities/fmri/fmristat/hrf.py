NF - fix signs of PCA basis functions according to sign of first

nipy/modalities/fmri/fmristat/hrf.py
@@ -1,82 +1,69 @@
-"""
-This module computes the canonical HRF used in 
-fMRIstat, both the 2-term spectral approximation
-and the Taylor series approximation, to a shifted
+""" Computation of the canonical HRF used in fMRIstat, both the 2-term
+spectral approximation and the Taylor series approximation, to a shifted
 version of the canonical Glover HRF.
 
 References
 ----------
-
 Liao, C.H., Worsley, K.J., Poline, J-B., Aston, J.A.D., Duncan, G.H.,
     Evans, A.C. (2002). \'Estimating the delay of the response in fMRI
     data.\' NeuroImage, 16:593-606.
 """
 
 import numpy as np
-import numpy.linalg as L
+import numpy.linalg as npl
 from scipy.interpolate import interp1d
 
 from sympy import Function
 from nipy.modalities.fmri import hrf, formula
 from nipy.modalities.fmri.fmristat.invert import invertR
 
-def spectral_decomposition(hrf2decompose, ncomp=2, time=None,
-                           delta=None):
-    """
 
-    Perform a PCA expansion of a symbolic HRF, evshifted over the values in delta,
-    returning the first ncomp components.
+def spectral_decomposition(hrf2decompose,
+                           time=None,
+                           delta=None,
+                           ncomp=2):
+    """ PCA decomposition of symbolic HRF shifted over time
+
+    Perform a PCA expansion of a symbolic HRF, time shifted over the
+    values in delta, returning the first ncomp components.
 
     This smooths out the HRF as compared to using a Taylor series
     approximation.
 
     Parameters
     ----------
-
     hrf2decompose : sympy expression 
         An expression that can be vectorized
         as a function of 't'. This is the HRF to be expanded in PCA
-
-    ncomp : int
+    time : None or np.ndarray, optional
+        None gives default value of np.linspace(-15,50,3251) chosen to
+        match fMRIstat implementation.  This corresponds to a time
+        interval of 0.02.  Presumed to be equally spaced.
+    delta : None or np.ndarray, optional
+        None results in default value of np.arange(-4.5, 4.6, 0.1)
+        chosen to match fMRIstat implementation.
+    ncomp : int, optional
         Number of principal components to retain.
-
-    time : np.ndarray
-        Default value of np.linspace(-15,50,3751)
-        chosen to match fMRIstat implementation.
-        Presumed to be equally spaced.
-
-    delta : np.ndarray
-        Default value of np.arange(-4.5, 4.6, 0.1)
-        chosen to match fMRIstat implementation.
 
     Returns
     -------
-
     hrf : [sympy expressions]
-        A sequence of symbolic HRFs that are the principal
-        components.
-
+        A sequence length `ncomp` of symbolic HRFs that are the
+        principal components.
     approx : 
         TODO
-
     """
-
     if time is None:
-        time = np.linspace(-15,50,3751)
+        time = np.linspace(-15,50,3251)
     dt = time[1] - time[0]
-
     if delta is None:
         delta = np.arange(-4.5, 4.6, 0.1)
-
     hrft = hrf.vectorize(hrf2decompose(hrf.t))
-
     H = []
     for i in range(delta.shape[0]):
         H.append(hrft(time - delta[i]))
     H = np.nan_to_num(np.asarray(H))
-    U, S, V = L.svd(H.T, full_matrices=0)
-
-    
+    U, S, V = npl.svd(H.T, full_matrices=0)
     basis = []
     for i in range(ncomp):
         b = interp1d(time, U[:, i], bounds_error=False, fill_value=0.)
@@ -84,14 +71,16 @@ def spectral_decomposition(hrf2decompose, ncomp=2, time=None,
             d = np.fabs((b(time) * dt).sum())
         b.y /= d
         basis.append(b)
-
     W = np.array([b(time) for b in basis[:ncomp]])
-    WH = np.dot(L.pinv(W.T), H.T)
+    WH = np.dot(npl.pinv(W.T), H.T)
     coef = [interp1d(delta, w, bounds_error=False, fill_value=0.) for w in WH]
-            
+    # swap sign of first component to match that of input HRF.  Swap
+    # other components if we swap the first to standardize signs of
+    # components across SVD implementations.
     if coef[0](0) < 0:
-        coef[0].y *= -1.
-        basis[0].y *= -1.
+        for i in range(ncomp):
+            coef[i].y *= -1.
+            basis[i].y *= -1.
 
     def approx(time, delta):
         value = 0
@@ -101,60 +90,64 @@ def spectral_decomposition(hrf2decompose, ncomp=2, time=None,
 
     approx.coef = coef
     approx.components = basis
-
     (approx.theta,
      approx.inverse,
      approx.dinverse,
      approx.forward,
      approx.dforward) = invertR(delta, approx.coef)
-
     symbasis = []
     for i, b in enumerate(basis):
-        symbasis.append(formula.aliased_function('%s%d' % (str(hrf2decompose), i), b))
+        symbasis.append(
+            formula.aliased_function('%s%d' % (str(hrf2decompose), i), b))
     return symbasis, approx
 
 
-def taylor_approx(hrf2decompose, tmax=50, tmin=-15, dt=0.02,
-                  delta=np.arange(-4.5, 4.6, 0.1)):
-    """
+def taylor_approx(hrf2decompose,
+                  time=None,
+                  delta=None):
+    """ A Taylor series approximation of an HRF shifted by times `delta`
 
-    Perform a PCA expansion of fn, shifted over the values in delta,
-    returning the first ncomp components.
-    This smooths out the HRF as compared to using a Taylor series
-    approximation.
+    Returns the HRF and the first derivative.
 
     Parameters
     ----------
+    hrf2decompose : sympy expression
+        An expression that can be vectorized as a function of 't'. 
+    time : None or np.ndarray, optional
+        None gives default value of np.linspace(-15,50,3251) chosen to
+        match fMRIstat implementation.  This corresponds to a time
+        interval of 0.02.  Presumed to be equally spaced.
+    delta : None or np.ndarray, optional
+        None results in default value of np.arange(-4.5, 4.6, 0.1)
+        chosen to match fMRIstat implementation.
 
-    hrf2decompose : sympy expression 
-        An expression that can be vectorized
-        as a function of 't'. This is the HRF to be expanded in PCA
-
-    ncomp : int
-        Number of principal components to retain.
+    Returns
+    -------
+    hrf : [sympy expressions]
+        Sequence length 2 comprising (`hrf2decompose`, ``dhrf``) where
+        ``dhrf`` is the first derivative of `hrf2decompose`.
+    approx : 
+        TODO
 
     References
     ----------
-
     Liao, C.H., Worsley, K.J., Poline, J-B., Aston, J.A.D., Duncan, G.H.,
     Evans, A.C. (2002). \'Estimating the delay of the response in fMRI
     data.\' NeuroImage, 16:593-606.
-
     """
-
+    if time is None:
+        time = np.linspace(-15,50,3251)
+    dt = time[1] - time[0]
+    if delta is None:
+        delta = np.arange(-4.5, 4.6, 0.1)
     hrft = hrf.vectorize(hrf2decompose(hrf.t))
-    time = np.arange(tmin, tmax, dt)
-
     dhrft = interp1d(time, -np.gradient(hrft(time), dt), bounds_error=False,
                     fill_value=0.)
-
     dhrft.y *= 2
     H = np.array([hrft(time - d) for d in delta])
     W = np.array([hrft(time), dhrft(time)])
     W = W.T
-
-    WH = np.dot(L.pinv(W), H.T)
-
+    WH = np.dot(npl.pinv(W), H.T)
     coef = [interp1d(delta, w, bounds_error=False,
                      fill_value=0.) for w in WH]
             
@@ -165,16 +158,14 @@ def taylor_approx(hrf2decompose, tmax=50, tmin=-15, dt=0.02,
 
     approx.coef = coef
     approx.components = [hrft, dhrft]
-
     (approx.theta,
      approx.inverse,
      approx.dinverse,
      approx.forward,
      approx.dforward) = invertR(delta, approx.coef)
-     
     dhrf = formula.aliased_function('d%s' % str(hrf2decompose), dhrft)
-
     return [hrf2decompose, dhrf], approx
+
 
 canonical, canonical_approx = taylor_approx(hrf.glover)
 spectral, spectral_approx = spectral_decomposition(hrf.glover)

nipy/modalities/fmri/fmristat/tests/test_FIAC.py
@@ -284,16 +284,15 @@ def test_altprotocol():
 
 
 def matchcol(col, X):
-    """
-    Find the row in X with the highest correlation with 1D col.
+    """ Find the row in X with the highest correlation with 1D col.
 
-    Used to find matching columns in fMRIstat's design with
-    the design created by Protocol. Not meant as a generic
-    helper function.
+    Used to find matching columns in fMRIstat's design with the design
+    created by Protocol. Not meant as a generic helper function.
     """
     c = np.array([np.corrcoef(col, X[i])[0,1] for i in range(X.shape[0])])
     c = np.nan_to_num(c)
-    return np.argmax(c), c.max()
+    ind = np.argmax(np.abs(c))
+    return ind, c[ind]
 
 
 def interpolate(name, col, t):
@@ -309,9 +308,7 @@ def test_agreement():
         for i in range(X[design_type].shape[1]):
             _, cmax = matchcol(X[design_type][:,i], fmristat[design_type])
             if not dd.dtype.names[i].startswith('ns'):
-                if i == 3:
-                    stop
-                yield assert_true(np.greater(cmax, 0.999))
+                yield assert_true(np.greater(np.abs(cmax), 0.999))
 
 
 @dec.slow