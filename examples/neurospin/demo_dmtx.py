ea00406224d191f6cf16c8fe22d6994687b447ea
Bertrand THIRION <bt206016@is143015>
2010-04-19 19:09:47 +0200

    added an example of glm analysis + cleaned some related code

--- examples/neurospin/demo_dmtx.py
+++ examples/neurospin/demo_dmtx.py
@@ -1,62 +1,60 @@
-""" test code to make a design matrix
 """
+Examples of design matrices specification and and computation
+(event-related design, FIR design, etc)
+
+Author : Bertrand Thirion: 2009-2010
+"""
+print __doc__
+
 import numpy as np
-from nipy.neurospin.utils.design_matrix import dmtx_light
+import pylab as mp
+import nipy.neurospin.utils.design_matrix as dm
 
 
 tr = 1.0
-frametimes = np.linspace(0,127*tr,128)
-conditions = [0,0,0,1,1,1,3,3,3]
-onsets=[30,70,100,10,30,90,30,40,60]
+nscans = 128
+frametimes = np.linspace(0, (nscans-1)*tr, nscans)
+
+conditions = [0, 0, 0, 1, 1, 1, 3, 3, 3]
+onsets = [30, 70, 100, 10, 30, 90, 30, 40, 60]
 hrf_model = 'Canonical'
-motion = np.cumsum(np.random.randn(128,6),0)
-add_reg_names = ['tx','ty','tz','rx','ry','rz']
+motion = np.cumsum(np.random.randn(128, 6), 0)
+add_reg_names = ['tx', 'ty',' tz', 'rx', 'ry', 'rz']
 
 #event-related design matrix
-paradigm = np.vstack(([conditions, onsets])).T
-x1,name1 = dmtx_light(frametimes, paradigm, drift_model='Polynomial',
-                      drift_order=3, add_regs=motion, add_reg_names=add_reg_names)
+paradigm =  dm.EventRelatedParadigm(conditions, onsets)
+
+X1 = dm.DesignMatrix(
+    frametimes, paradigm, drift_model='Polynomial', drift_order=3,
+    add_regs=motion, add_reg_names=add_reg_names)
 
 # block design matrix
 duration = 7*np.ones(9)
-paradigm = np.vstack(([conditions, onsets, duration])).T
-x2,name2 = dmtx_light(frametimes, paradigm, drift_model='Polynomial', drift_order=3)
+paradigm =  dm.BlockParadigm(index=conditions, onset=onsets, duration=duration)
+
+X2 = dm.DesignMatrix(frametimes, paradigm, drift_model='Polynomial',
+                         drift_order=3)
 
 # FIR model
-paradigm = np.vstack(([conditions, onsets])).T
+paradigm =  dm.EventRelatedParadigm(conditions, onsets)
 hrf_model = 'FIR'
-x3,name3 = dmtx_light(frametimes, paradigm, hrf_model = 'FIR',
+X3 = dm.DesignMatrix(frametimes, paradigm, hrf_model = 'FIR',
                       drift_model='Polynomial', drift_order=3,
                       fir_delays = range(1,6))
 
-import matplotlib.pylab as mp
-mp.figure()
-mp.imshow(x1/np.sqrt(np.sum(x1**2,0)),interpolation='Nearest', aspect='auto')
-mp.xlabel('conditions')
-mp.ylabel('scan number')
-if name1!=None:
-    mp.xticks(np.arange(len(name1)),name1,rotation=60,ha='right')
-    mp.subplots_adjust(top=0.95,bottom=0.25)
-mp.title('Example of event-related design matrix')
-
-mp.figure()
-mp.imshow(x2/np.sqrt(np.sum(x2**2,0)),interpolation='Nearest', aspect='auto')
-mp.xlabel('conditions')
-mp.ylabel('scan number')
-if name2!=None:
-    mp.xticks(np.arange(len(name2)),name2,rotation=60,ha='right')
-    mp.subplots_adjust(top=0.95,bottom=0.25)
-mp.title('Example of block design matrix')
-
-mp.figure()
-mp.imshow(x3/np.sqrt(np.sum(x3**2,0)),interpolation='Nearest', aspect='auto')
-mp.xlabel('conditions')
-mp.ylabel('scan number')
-if name3!=None:
-    mp.xticks(np.arange(len(name3)),name3,rotation=60,ha='right')
-    mp.subplots_adjust(top=0.95,bottom=0.25)
-mp.title('Example of FIR design matrix')
 
 
+fig = mp.figure()
+ax = mp.subplot(1,3,1)
+X1.show(ax=ax)
+ax.set_title('example of event-related design matrix')
+ax = mp.subplot(1,3,2)
+X2.show(ax=ax)
+ax.set_title('example of block design matrix')
+ax = mp.subplot(1,3,3)
+X3.show(ax=ax)
+ax.set_title('example of FIR design matrix')
+mp.subplots_adjust(top=0.9, bottom=0.25)
+fig.set_size_inches(12, 6, forward=True)
 mp.show()
 
--- examples/neurospin/need_data/get_data_light.py
+++ examples/neurospin/need_data/get_data_light.py
@@ -1,7 +1,7 @@
 """
 Get two images from the web (one mask image and one spmT image) and
 put them in the dir: ~/.nipy/tests/data
-should be quick and light (<3MB)
+should be quick and light (<6MB)
 
 Author : Bertrand Thirion, 2009
 """
@@ -12,6 +12,7 @@ import tarfile
 
 def getIt():
     """
+    light dataset for multi-subject analysis
     """
     # define several paths
     url = 'ftp://ftp.cea.fr/pub/dsv/madic/download/nipy'
@@ -20,14 +21,16 @@ def getIt():
     InputImage = os.path.join(data_dir,'spmT_0029.nii.gz')
     GroupData = os.path.join(data_dir,'group_t_images.tar.gz')
 
+    # possibly create data_dir
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+        assert os.path.exists(data_dir)
+
     # download MaskImage if necessary
     if os.path.exists(MaskImage)==False:
         filename = 'mask.nii.gz'
         datafile = os.path.join(url,filename)
         fp = urllib2.urlopen(datafile)
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-            assert os.path.exists(data_dir)
         local_file = open(MaskImage, 'w')
         local_file.write(fp.read())
         local_file.flush()
@@ -38,9 +41,6 @@ def getIt():
         filename = 'spmT_0029.nii.gz'
         datafile = os.path.join(url,filename)
         fp = urllib2.urlopen(datafile)
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-            assert os.path.exists(data_dir)
         local_file = open(InputImage, 'w')
         local_file.write(fp.read())
         local_file.flush()
@@ -51,9 +51,6 @@ def getIt():
         filename = 'group_t_images.tar.gz'
         datafile = os.path.join(url,filename)
         fp = urllib2.urlopen(datafile)
-        if not os.path.exists(data_dir):
-            os.makedirs(data_dir)
-            assert os.path.exists(data_dir)
         local_file = open(GroupData, 'w')
         local_file.write(fp.read())
         local_file.flush()
@@ -64,7 +61,40 @@ def getIt():
     tar.extractall(data_dir)
     tar.close()
     os.remove(GroupData)
-    
+
+def get_localizer_dataset():
+    """
+    Heavier dataset (30 MO) for first-level analysis
+    """
+    # define several paths
+    url = 'ftp://ftp.cea.fr/pub/dsv/madic/download/nipy'
+    data_dir = os.path.expanduser(os.path.join('~', '.nipy', 'tests', 'data'))
+    raw_fmri = os.path.join(data_dir,'s12069_swaloc1_corr.nii.gz')
+    paradigm = os.path.join(data_dir,'localizer_paradigm.csv')
+
+    # create data_dir
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
+        assert os.path.exists(data_dir)
+
+    # download MaskImage if necessary
+    if os.path.exists(paradigm)==False:
+        datafile = os.path.join(url, 'localizer_paradigm.csv')
+        fp = urllib2.urlopen(datfile)
+        local_file = open(paradigm, 'w')
+        local_file.write(fp.read())
+        local_file.flush()
+        local_file.close()
+
+    # download raw_fmri if necessary
+    if os.path.exists(raw_fmri)==False:
+        filename = 's12069_swaloc1_corr.nii.gz'
+        datafile = os.path.join(url,filename)
+        fp = urllib2.urlopen(datafile)
+        local_file = open(raw_fmri, 'w')
+        local_file.write(fp.read())
+        local_file.flush()
+        local_file.close()
 
 if __name__ == '__main__':
     getIt()
