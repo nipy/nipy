17f277f0497859d415020bd2e7174fc654a9e293
Matthew Brett <matthew.brett@gmail.com>
2010-05-24 11:28:57 -0700

    DOC - small cleanup on gitwash_dumper

--- tools/gitwash_dumper.py
+++ tools/gitwash_dumper.py
@@ -2,6 +2,7 @@
 ''' Checkout gitwash repo into directory and do search replace on name '''
 
 import os
+from os.path import join as pjoin
 import shutil
 import sys
 import re
@@ -11,10 +12,12 @@ import tempfile
 from subprocess import call
 
 
+verbose = False
+
+
 def clone_repo(url, branch):
     tmpdir = tempfile.mkdtemp()
     cmd = 'git clone --branch %s %s %s' % (branch, url, tmpdir)
-    print cmd
     call(cmd, shell=True)
     return tmpdir
 
@@ -26,43 +29,50 @@ def cp_files(in_path, globs, out_path):
         pass
     out_fnames = []
     for in_glob in globs:
-        in_glob_path = os.path.join(in_path, in_glob)
+        in_glob_path = pjoin(in_path, in_glob)
         for in_fname in glob.glob(in_glob_path):
             out_fname = in_fname.replace(in_path, out_path)
             pth, _ = os.path.split(out_fname)
             if not os.path.isdir(pth):
                 os.makedirs(pth)
-            print out_fname
             shutil.copyfile(in_fname, out_fname)
             out_fnames.append(out_fname)
     return out_fnames
 
 
-def perl_dash_pie(in_exp, out_str, filename):
-    in_reg = re.compile(in_exp)
-    in_txt = open(filename, 'rt').read(-1)
-    out_txt = in_reg.sub(out_str, in_txt)
-    if in_txt != out_txt:
-        open(filename, 'wt').write(out_txt)
-        return True
-    return False
+def filename_search_replace(sr_pairs, filename, backup=False):
+    ''' Search and replace for expressions in files
 
-    
+    '''
+    in_txt = open(filename, 'rt').read(-1)
+    out_txt = in_txt[:]
+    for in_exp, out_exp in sr_pairs:
+        in_exp = re.compile(in_exp)
+        out_txt = in_exp.sub(out_exp, out_txt)
+    if in_txt == out_txt:
+        return False
+    open(filename, 'wt').write(out_txt)
+    if backup:
+        open(filename + '.bak', 'wt').write(in_txt)
+    return True
+
+        
 def copy_replace(replace_pairs,
                  out_path,
                  repo_url,
                  repo_branch = 'master',
-                 cp_globs=('gitwash/*',),
-                 rep_globs=('*.rst',)):
+                 cp_globs=('*',),
+                 rep_globs=('*',)):
     repo_path = clone_repo(gitwash_url, repo_branch)
     out_fnames = cp_files(repo_path, cp_globs, out_path)
     shutil.rmtree(repo_path)
     fnames = []
     for rep_glob in rep_globs:
         fnames += fnmatch.filter(out_fnames, rep_glob)
+    if verbose:
+        print '\n'.join(fnames)
     for fname in fnames:
-        for old, new in replace_pairs:
-            perl_dash_pie(old, new, fname)
+        filename_search_replace(replace_pairs, fname, False)
 
 
 usage = ('Usage: '
@@ -95,5 +105,8 @@ if __name__ == '__main__':
     copy_replace((('PROJECTNAME', project_name),
                   ('REPONAME', repo_name),
                   ('MAIN_GH_USER', main_gh_user)),
-                 out_path, gitwash_url, gitwash_branch)
-    
+                 out_path,
+                 gitwash_url,
+                 gitwash_branch,
+                 cp_globs=(pjoin('gitwash', '*'),),
+                 rep_globs=('*.rst',))
