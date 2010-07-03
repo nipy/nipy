3497669627be381d004ac92e4cab46eb9ef6ffca
Matthew Brett <matthew.brett@gmail.com>
2010-07-02 22:29:25 -0400

    BF - fixed events doctest

--- nipy/modalities/fmri/tests/test_utils.py
+++ nipy/modalities/fmri/tests/test_utils.py
@@ -10,8 +10,11 @@ from nipy.modalities.fmri.formula import Term
 from nipy.modalities.fmri.aliased import lambdify
 from nipy.modalities.fmri.utils import (events,
                                         blocks,
+                                        interp,
                                         linear_interp,
-                                        step_function)
+                                        step_function,
+                                        convolve_functions,
+                                        )
 from sympy import Symbol, Function, DiracDelta
 import nipy.modalities.fmri.hrf as mfhrf
 
@@ -23,7 +26,7 @@ from numpy.testing import assert_array_equal, assert_array_almost_equal
 from nipy.testing import parametric
 
 t = Term('t')
-TIME_DTYPE = np.dtype([('t', np.float)])
+
 
 @parametric
 def test_events():
@@ -34,24 +37,39 @@ def test_events():
                        DiracDelta(-3 + t), evs)
     evs = events([3,6,9], f=h)
     yield assert_equal(h(-3 + t) + h(-6 + t) + h(-9 + t), evs)
+    # make some beta symbols
+    b = [Symbol('b%d' % i, dummy=True) for i in range(3)]
+    a = Symbol('a')
+    p = b[0] + b[1]*a + b[2]*a**2
+    evs = events([3,6,9], amplitudes=[2,1,-1], g=p)
+    yield assert_equal((2*b[1] + 4*b[2] + b[0])*DiracDelta(-3 + t) +
+                       (-b[1] + b[0] + b[2])*DiracDelta(-9 + t) +
+                       (b[0] + b[1] + b[2])*DiracDelta(-6 + t),
+                       evs)
+    evs = events([3,6,9], amplitudes=[2,1,-1], g=p, f=h)
+    yield assert_equal((2*b[1] + 4*b[2] + b[0])*h(-3 + t) +
+                       (-b[1] + b[0] + b[2])*h(-9 + t) +
+                       (b[0] + b[1] + b[2])*h(-6 + t),
+                       evs)
     # test no error for numpy int arrays
     onsets = np.array([30, 70, 100], dtype=np.int64)
     evs = events(onsets, f=mfhrf.glover)
 
 
 @parametric
-def test_linear_inter():
+def test_interp():
     times = [0,4,5.]
     values = [2.,4,6]
-    s = linear_interp(times, values, bounds_error=False)
-    tval = np.array([-0.1,0.1,3.9,4.1,5.1])
-    res = lambdify(t, s)(tval)
-    yield assert_array_equal(np.isnan(res),
-                             [True, False, False, False, True])
-    yield assert_array_almost_equal(res[1:-1],
-                                    [2.05, 3.95, 4.2])
-    # specifying kind as linear is OK
-    s = linear_interp(times, values, kind='linear')
+    for int_func in (interp, linear_interp):
+        s = int_func(times, values, bounds_error=False)
+        tval = np.array([-0.1,0.1,3.9,4.1,5.1])
+        res = lambdify(t, s)(tval)
+        yield assert_array_equal(np.isnan(res),
+                                 [True, False, False, False, True])
+        yield assert_array_almost_equal(res[1:-1],
+                                        [2.05, 3.95, 4.2])
+        # specifying kind as linear is OK
+        s = linear_interp(times, values, kind='linear')
 
 
 @raises(ValueError)
@@ -84,10 +102,13 @@ def test_blocks():
     b = blocks(on_off, amplitudes=[3,5])
     lam = lambdify(t, b)
     yield assert_array_equal(lam(tval),
-                             [0, 3, 0, 6])
-    a = Symbol('a')
-    b = blocks(on_off, amplitudes=[3,5], g=a+1)
-    lam = lambdify(t, b)
-    yield assert_array_equal(lam(tval),
-                             [0, 4, 0, 6])
+                             [0, 3, 0, 5])
 
+
+@parametric
+def test_convolve_functions():
+    # square wave
+    expr = (t > 0) * (t < 1)
+    # convolve with 1
+    cf = convolve_functions(expr, 1, [-1, 2], 0.1)
+    lam = lambdify(t, cf)
--- nipy/modalities/fmri/utils.py
+++ nipy/modalities/fmri/utils.py
@@ -17,7 +17,7 @@ fourier_basis : a convenience function to generate a Fourier basis
 
 """
 
-__docformat__ = 'restructuredtext'
+import itertools
 
 import numpy as np
 import numpy.fft as FFT
@@ -29,15 +29,17 @@ from sympy import cos as sympy_cos
 from sympy import pi as sympy_pi
 
 from . import formula
+from . import aliased
 from .aliased import aliased_function, vectorize, lambdify
 
 t = formula.Term('t')
 
 
 def fourier_basis(freq):
-    """
-    Formula for Fourier drift, consisting of sine and
-    cosine waves of given frequencies.
+    """ sin and cos Formula for Fourier drift
+
+    The Fourier basis consists of sine and cosine waves of given
+    frequencies.
 
     Parameters
     ----------
@@ -64,6 +66,62 @@ def fourier_basis(freq):
     return formula.Formula(r)
 
 
+def interp(times, values, fill=0, name=None, **kw):
+    """ Generic interpolation function of t given `times` and `values`
+
+    Imterpolator such that:
+    
+    f(times[i]) = values[i]
+
+    if t < times[0]:
+        f(t) = fill
+
+    See ``scipy.interpolate.interp1d`` for details of interpolation
+    types and other keyword arguments.  Default is 'kind' is linear,
+    making this function, by default, have the same behavior as
+    ``linear_interp``. 
+
+    Parameters
+    ----------
+    times : array-like
+        Increasing sequence of times
+    values : array-like
+        Values at the specified times
+    fill : float, optional
+        Value on the interval (-np.inf, times[0]). Default 0.
+    name : None or str, optional
+        Name of symbolic expression to use. If None, a default is used.
+    **kw : keyword args, optional
+        passed to ``interp1d``
+        
+    Returns
+    -------
+    f : sympy expression 
+        A Function of t.
+
+    Examples
+    --------
+    >>> s = interp([0,4,5.],[2.,4,6], bounds_error=False)
+    >>> tval = np.array([-0.1,0.1,3.9,4.1,5.1])
+    >>> res = aliased.lambdify(t, s)(tval)
+    >>> # nans outside bounds
+    >>> np.isnan(res)
+    array([ True, False, False, False,  True], dtype=bool)
+    >>> # interpolated values otherwise
+    >>> np.allclose(res[1:-1], [2.05, 3.95, 4.2])
+    True
+    """
+    interpolator = interp1d(times, values, **kw)
+    # make a new name if none provided
+    if name is None:
+        name = 'interp%d' % interp.counter
+        interp.counter += 1
+    s = aliased_function(name, interpolator)
+    return s(t)
+
+interp.counter = 0
+
+
 def linear_interp(times, values, fill=0, name=None, **kw):
     """ Linear interpolation function of t given `times` and `values`
 
@@ -73,6 +131,9 @@ def linear_interp(times, values, fill=0, name=None, **kw):
 
     if t < times[0]:
         f(t) = fill
+
+    This version of the function enforces the 'linear' kind of
+    interpolation (argument to ``scipy.interpolate.interp1d``). 
 
     Parameters
     ----------
@@ -96,7 +157,7 @@ def linear_interp(times, values, fill=0, name=None, **kw):
     --------
     >>> s = linear_interp([0,4,5.],[2.,4,6], bounds_error=False)
     >>> tval = np.array([-0.1,0.1,3.9,4.1,5.1])
-    >>> res = lambdify(t, s)(tval)
+    >>> res = aliased.lambdify(t, s)(tval)
     >>> # nans outside bounds
     >>> np.isnan(res)
     array([ True, False, False, False,  True], dtype=bool)
@@ -104,21 +165,12 @@ def linear_interp(times, values, fill=0, name=None, **kw):
     >>> np.allclose(res[1:-1], [2.05, 3.95, 4.2])
     True
     """
-    # XXX - does interpolation have to be linear?
     kind = kw.get('kind')
     if kind is None:
         kw['kind'] = 'linear'
     elif kind != 'linear':
         raise ValueError('Only linear interpolation supported')
-    interp = interp1d(times, values, **kw)
-    # make a new name if none provided
-    if name is None:
-        name = 'interp%d' % linear_interp.counter
-        linear_interp.counter += 1
-    s = aliased_function(name, interp)
-    return s(t)
-
-linear_interp.counter = 0
+    return interp(times, values, fill, name, **kw)
 
 
 def step_function(times, values, name=None, fill=0):
@@ -134,14 +186,13 @@ def step_function(times, values, name=None, fill=0):
     Parameters
     ----------
     times : (N,) sequence
-        Increasing sequence of times
+       Increasing sequence of times
     values : (N,) sequence
-        Values at the specified times
+       Values at the specified times
     fill : float
-        Value on the interval (-np.inf, times[0])
+       Value on the interval (-np.inf, times[0])
     name : str
-        Name of symbolic expression to use. If None,
-        a default is used.
+       Name of symbolic expression to use. If None, a default is used.
 
     Returns
     -------
@@ -154,9 +205,9 @@ def step_function(times, values, name=None, fill=0):
     --------
     >>> s = step_function([0,4,5],[2,4,6])
     >>> tval = np.array([-0.1,3.9,4.1,5.1])
-    >>> lam = lambdify(t, s)
+    >>> lam = aliased.lambdify(t, s)
     >>> lam(tval)
-    array([0, 2, 4, 6])
+    array([ 0.,  2.,  4.,  6.])
     """
     if name is None:
         name = 'step%d' % step_function.counter
@@ -184,24 +235,23 @@ def events(times, amplitudes=None, f=DiracDelta, g=Symbol('a')):
     times : sequence
        vector of onsets length $N$
     amplitudes : None or sequence length $N$, optional
-        Optional sequence of amplitudes. None (default) results in
-        sequence length $N$ of 1s
+       Optional sequence of amplitudes. None (default) results in
+       sequence length $N$ of 1s
     f : sympy.Function, optional
-        Optional function. Defaults to DiracDelta, can be replaced with
-        another function, f, in which case the result is the convolution
-        with f.
+       Optional function. Defaults to DiracDelta, can be replaced with
+       another function, f, in which case the result is the convolution
+       with f.
     g : sympy.Basic, optional
-        Optional sympy expression function of amplitudes.  The
-        amplitudes, should be represented by the symbol 'a', which
-        will be substituted, by the corresponding value in
-        `amplitudes`. .
+       Optional sympy expression function of amplitudes.  The
+       amplitudes, should be represented by the symbol 'a', which will
+       be substituted, by the corresponding value in `amplitudes`.
 
     Returns
     -------
     sum_expression : Sympy.Add
-        Sympy expression of time $t$, where onsets, as a function of
-        $t$, have been symbolically convolved with function `f`, and any
-        function `g` of corresponding amplitudes.
+       Sympy expression of time $t$, where onsets, as a function of $t$,
+       have been symbolically convolved with function `f`, and any
+       function `g` of corresponding amplitudes.
 
     Examples
     --------
@@ -210,45 +260,28 @@ def events(times, amplitudes=None, f=DiracDelta, g=Symbol('a')):
     >>> h = Symbol('hrf')
     >>> events([3,6,9], f=h)
     hrf(-9 + t) + hrf(-6 + t) + hrf(-3 + t)
-
     >>> events([3,6,9], amplitudes=[2,1,-1])
     -DiracDelta(-9 + t) + 2*DiracDelta(-3 + t) + DiracDelta(-6 + t)
-
-    >>> b = [Symbol('b%d' % i, dummy=True) for i in range(3)]
-    >>> a = Symbol('a')
-    >>> p = b[0] + b[1]*a + b[2]*a**2
-    >>> events([3,6,9], amplitudes=[2,1,-1], g=p)
-    (2*_b1 + 4*_b2 + _b0)*DiracDelta(-3 + t) + (-_b1 + _b0 + _b2)*DiracDelta(-9 + t) + (_b0 + _b1 + _b2)*DiracDelta(-6 + t)
-
-    >>> h = Symbol('hrf')
-    >>> events([3,6,9], amplitudes=[2,1,-1], g=p, f=h)
-    (2*_b1 + 4*_b2 + _b0)*hrf(-3 + t) + (-_b1 + _b0 + _b2)*hrf(-9 + t) + (_b0 + _b1 + _b2)*hrf(-6 + t)
     """
     e = 0
     asymb = Symbol('a')
     if amplitudes is None:
-        def _amplitudes():
-            while True:
-                yield 1
-        amplitudes = _amplitudes()
+        amplitudes = itertools.cycle([1])
     for _t, a in zip(times, amplitudes):
         e = e + g.subs(asymb, a) * f(t-_t)
     return e
 
 
-def blocks(intervals, amplitudes=None, g=Symbol('a')):
-    """  Step function based on a sequence of intervals.
+def blocks(intervals, amplitudes=None):
+    """ Step function based on a sequence of intervals.
 
     Parameters
     ----------
     intervals : (S,) sequence of (2,) sequences
-       Seqence (S0, S1, ... S(N-1)) of sequences, where S0 (etc) are
+       Sequence (S0, S1, ... S(N-1)) of sequences, where S0 (etc) are
        sequences of length 2, giving 'on' and 'off' times of block
     amplitudes : (S,) sequence of float, optional
        Optional amplitudes for each block. Defaults to 1.
-    g : sympy.Basic, optional
-        Optional sympy expression function involving 'a', which
-        will be substituted for 'a' in the generator.
 
     Returns
     -------
@@ -258,63 +291,54 @@ def blocks(intervals, amplitudes=None, g=Symbol('a')):
 
     Examples
     --------
-    >>> tval = np.array([0.4,1.4,2.4,3.4]).view(np.dtype([('t', np.float)]))
-    >>> b = blocks([[1,2],[3,4]])
-    >>> b.design(tval)
-    array([(0.0,), (1.0,), (0.0,), (1.0,)], 
-          dtype=[('step0(t)', '<f8')])
-
-    >>> b = blocks([[1,2],[3,4]], amplitudes=[3,5])
-    >>> b.design(tval)
-    array([(0.0,), (3.0,), (0.0,), (5.0,)], 
-          dtype=[('step1(t)', '<f8')])
-
-    >>> a = Symbol('a')
-    >>> b = blocks([[1,2],[3,4]], amplitudes=[3,5], g=a+1)
-    >>> b.design(tval)
-    array([(0.0,), (4.0,), (0.0,), (6.0,)], 
-          dtype=[('step2(t)', '<f8')])
+    >>> on_off = [[1,2],[3,4]]
+    >>> tval = np.array([0.4,1.4,2.4,3.4])
+    >>> b = blocks(on_off)
+    >>> lam = aliased.lambdify(t, b)
+    >>> lam(tval)
+    array([ 0.,  1.,  0.,  1.])
+    >>> b = blocks(on_off, amplitudes=[3,5])
+    >>> lam = aliased.lambdify(t, b)
+    >>> lam(tval)
+    array([ 0.,  3.,  0.,  5.])
     """
     t = [-np.inf]
     v = [0]
-    asymb = Symbol('a')
     if amplitudes is None:
-        def _amplitudes():
-            while True:
-                yield 1
-        amplitudes = _amplitudes()
+        amplitudes = itertools.cycle([1])
     for _t, a in zip(intervals, amplitudes):
         t += list(_t)
-        v += [g.subs(asymb, a), 0]
+        v += [a, 0]
     t.append(np.inf)
     v.append(0)
     return step_function(t, v)
 
 
 def convolve_functions(fn1, fn2, interval, dt, padding_f=0.1, name=None):
-    """
-    Convolve fn1 with fn2.
+    """ Expression containing numerical convolution of `fn1` with `fn2`
     
     Parameters
     ----------
     fn1 : sympy expr
-        An expression that is a function of t only.
+       An expression that is a function of t only.
     fn2 : sympy expr
-        An expression that is a function of t only.
-    interval : [float, float]
-        The interval over which to convolve the two functions.
+       An expression that is a function of t only.
+    interval : (2,) sequence of float
+       The interval over which to convolve the two functions.
     dt : float
-        Time step for discretization 
+       Time step for discretization.  We use this for creating the
+       interpolator to form the numerical implementation
     padding_f : float, optional
-        Padding added to the left and right in the convolution.
+       Padding added to the left and right in the convolution.  Padding
+       value is fraction of the length given by `interval` 
     name : None or str, optional
-        Name of the convolved function in the resulting expression. 
-        Defaults to one created by linear_interp.
+       Name of the convolved function in the resulting expression. 
+       Defaults to one created by linear_interp.
             
     Returns
     -------
     f : sympy expr
-            An expression that is a function of t only.
+       An expression that is a function of t only.
 
     Examples
     --------
@@ -322,20 +346,25 @@ def convolve_functions(fn1, fn2, interval, dt, padding_f=0.1, name=None):
     >>> t = sympy.Symbol('t')
     >>> # This is a square wave on [0,1]
     >>> f1 = (t > 0) * (t < 1)
-    >>> # The convolution of with itself is a triangular wave on [0,2], peaking at 1 with height 1
-    >>> tri = convolve_functions(f1, f1, [0,2], 1.0e-03, name='conv')
+
+    The convolution of ``f1`` with itself is a triangular wave on [0,2],
+    peaking at 1 with height 1
+    
+    >>> tri = convolve_functions(f1, f1, [1,2], 0.001, name='conv')
     >>> print tri
     conv(t)
-    >>> ftri = vectorize(tri)
-    >>> x = np.linspace(0,2,11)
+    >>> ftri = aliased.lambdify(t, tri)
+    >>> x = np.linspace(1,2,11)
     >>> y = ftri(x)
-    >>> # This is the resulting y-value (which seem to be numerically off by dt
+
+    This is the resulting y-value (which seem to be numerically off by
+    dt)
+
     >>> y
     array([ -3.90255908e-16,   1.99000000e-01,   3.99000000e-01,
            5.99000000e-01,   7.99000000e-01,   9.99000000e-01,
            7.99000000e-01,   5.99000000e-01,   3.99000000e-01,
            1.99000000e-01,   6.74679706e-16])
-    >>> 
     """
 
     max_interval, min_interval = max(interval), min(interval)
