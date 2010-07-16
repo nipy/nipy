46ab509b7893ef886648f9ffe687f80dd54438b3
Yaroslav Halchenko <debian@onerussian.com>
2010-07-16 11:57:11 +0800

    BF - missing import of assert_almost_equal in test_utils

--- nipy/modalities/fmri/tests/test_utils.py
+++ nipy/modalities/fmri/tests/test_utils.py
@@ -21,7 +21,8 @@ import nipy.modalities.fmri.hrf as mfhrf
 from nose.tools import (assert_true, assert_false, 
                         assert_equal, assert_raises, raises)
 
-from numpy.testing import assert_array_equal, assert_array_almost_equal
+from numpy.testing import (assert_array_equal, assert_array_almost_equal,
+                           assert_almost_equal)
 
 from nipy.testing import parametric
 
