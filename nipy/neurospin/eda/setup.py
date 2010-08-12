6dbe7f7ac68df176101b0d8ba9cff59e1b37086e
Gael varoquaux <gael.varoquaux@normalesup.org>
2010-08-12 08:28:25 +0200

    BUG: Fix tab/space mixture

--- nipy/neurospin/eda/setup.py
+++ nipy/neurospin/eda/setup.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 
 def configuration(parent_package='',top_path=None):
-	
-	from numpy.distutils.misc_util import Configuration
-	
-	config = Configuration('eda', parent_package, top_path)
+    from numpy.distutils.misc_util import Configuration
+    
+    config = Configuration('eda', parent_package, top_path)
     config.add_subpackage('tests')
-		
-	return config
-	
+    return config
 
 if __name__ == '__main__':
-	print 'This is the wrong setup.py file to run'
+    print 'This is the wrong setup.py file to run'
 
 
