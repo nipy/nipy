fixed a bug in masking script

nipy/neurospin/spatial_models/parcel_io.py
@@ -456,8 +456,14 @@ def one_subj_parcellation(MaskImage, betas, nbparcel, nn=6, method='ward',
         lbeta = rbeta.get_data()
         lbeta = lbeta[lmask>0]
         beta.append(lbeta)
-	
-    beta = np.array(beta).T
+
+    beta = np.array(beta)
+    if len(beta.shape)>2:
+        beta = np.squeeze(beta)
+        
+    if beta.shape[0]!=nvox:
+        beta = beta.T
+
 
     #step 2: parcel the data ---------------------------
     feature = np.hstack((beta, mu*coord/np.std(coord)))