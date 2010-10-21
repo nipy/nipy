49ad3dde243075d5643d6e5b2167032211b4b6cf
Matthew Brett <matthew.brett@gmail.com>
2010-10-21 19:32:45 +0100

    NF - largely untested draft of general transforms interface

--- nipy/neurospin/registration/affine.py
+++ nipy/neurospin/registration/affine.py
@@ -125,7 +125,62 @@ def subgrid_affine(affine, slices):
     return np.dot(affine, t)
 
 
-class Affine(object): 
+class Transform(object):
+    """ A default transformation class
+    """
+    def __init__(self, func):
+        self.func = func
+
+    def apply(self, pts):
+        return self.func(pts)
+
+    def compose(self, other):
+        return self.__class__(
+            lambda pts : self.func(other.apply(pts)))
+
+    @property
+    def param(self):
+        raise AttributeError('No param for generic transform')
+
+
+class SplineTransform(Transform):
+    def __init__(self, control_points, kernel, param=None):
+        """ Create spline transform
+
+        Parameters
+        ----------
+        control_points : (K,3) array-like
+            spline control points
+        kernel : callable
+            kernel G where G(x - x_k) for point x and control point k is the
+            basis for the parameters
+        param : None or (K,3) array, optional
+            coefficients for each control point.  None gives array of zeros as
+            starting default
+        """
+        self.control_points = np.asarray(control_points)
+        K = control_points.shape[0]
+        self.kernel = kernel
+        if param is None:
+            self.param = np.zeros((n_pts,3))
+        else:
+            self.param = np.asarray(param)
+        self._cache = {'last_pts': None, 'Gik': None}
+
+    def apply(self, pts):
+        # logic for applying transform to points
+        if pts is self._cache['last_pts']:
+            Gik = self._cache['Gik']
+        else: # calculate and cache Gik matrix
+            # Gik is an N by K matrix where G is the kernel, i indexes input
+            # points, k indexes control points, and Gik[i, k] is G(x_i - x_k)
+            Gik = None
+            self._cache['last_pts'] = pts
+            self._cache['Gik'] = Gik
+        return np.dot(Gik, self.param)
+
+
+class Affine(Transform): 
 
     def __init__(self, array=None, radius=_radius, flag2d=_flag2d):
         subtype = id_affine
@@ -206,9 +261,11 @@ class Affine(object):
         composed_transform : Transform
             a transform implementing the composition of self on `other`
         """
+        try:
+            other_aff = other.as_affine()
+        except AttributeError:
+            return Transform(self.apply).compose(other)
         aff = self.as_affine()
-        # Deliberately raise error in non-affine case for now
-        other_aff = other.as_affine()
         return self.__class__(np.dot(aff, other_aff))
 
     def __str__(self): 
--- nipy/neurospin/registration/tests/test_chain_transforms.py
+++ nipy/neurospin/registration/tests/test_chain_transforms.py
@@ -40,55 +40,65 @@ AFF3 = np.eye(4)
 AFF3[:3,3] = np.random.normal(size=(3,))
 tmp = np.random.normal(size=(3,3))
 AFF3[:3,:3] = np.sign(npl.det(tmp))*tmp 
+POINTS = np.arange(12).reshape(4,3)
+# Make affine objects
+AFF1_OBJ, AFF2_OBJ, AFF3_OBJ = [Affine(a) for a in [AFF1, AFF2.copy(), AFF3]]
 
 
 def test_creation():
-    # Make affine objects
-    aff1_obj, aff2_obj, aff3_obj = [Affine(a) for a in [AFF1, AFF2.copy(), AFF3]]
-    pts = np.arange(12).reshape(4,3)
     # This is the simplest possible example, where there is a thing we are
     # optimizing, and an optional pre and post transform
+    # Reset the aff2 object
+    aff2_obj = Affine(AFF2.copy())
     ct = ChainTransform(aff2_obj)
     # Check apply gives expected result
-    assert_array_equal(ct.apply(pts),
-                       apply_affine(AFF2, pts))
+    assert_array_equal(ct.apply(POINTS),
+                       apply_affine(AFF2, POINTS))
     # Check that result is changed by setting params
     assert_array_equal(ct.param, aff2_obj.param)
     ct.param = np.zeros((12,))
-    assert_array_equal(ct.apply(pts), pts)
+    assert_array_equal(ct.apply(POINTS), POINTS)
     # Does changing params in chain object change components passed in?
     assert_array_equal(aff2_obj.param, np.zeros((12,)))
     # Reset the aff2 object
     aff2_obj = Affine(AFF2.copy())
     # Check apply gives the expected results
-    ct = ChainTransform(aff2_obj, pre=aff1_obj)
-    assert_array_almost_equal(aff1_obj.as_affine(), AFF1)
+    ct = ChainTransform(aff2_obj, pre=AFF1_OBJ)
+    assert_array_almost_equal(AFF1_OBJ.as_affine(), AFF1)
     assert_array_almost_equal(aff2_obj.as_affine(), AFF2)
     tmp = np.dot(AFF2, AFF1)
-    assert_array_almost_equal(ct.apply(pts),
-                       apply_affine(np.dot(AFF2, AFF1), pts))
+    assert_array_almost_equal(ct.apply(POINTS),
+                       apply_affine(np.dot(AFF2, AFF1), POINTS))
     # Check that result is changed by setting params
     assert_array_equal(ct.param, aff2_obj.param)
     ct.param = np.zeros((12,))
-    assert_array_almost_equal(ct.apply(pts), apply_affine(AFF1, pts))
+    assert_array_almost_equal(ct.apply(POINTS), apply_affine(AFF1, POINTS))
     # Does changing params in chain object change components passed in?
     assert_array_equal(aff2_obj.param, np.zeros((12,)))
     # Reset the aff2 object
     aff2_obj = Affine(AFF2.copy())
-    ct = ChainTransform(aff2_obj, pre=aff1_obj, post=aff3_obj)
-    assert_array_almost_equal(ct.apply(pts),
-                       apply_affine(np.dot(AFF3, np.dot(AFF2, AFF1)), pts))
+    ct = ChainTransform(aff2_obj, pre=AFF1_OBJ, post=AFF3_OBJ)
+    assert_array_almost_equal(ct.apply(POINTS),
+                       apply_affine(np.dot(AFF3, np.dot(AFF2, AFF1)), POINTS))
     # Check that result is changed by setting params
     assert_array_equal(ct.param, aff2_obj.param)
     ct.param = np.zeros((12,))
-    assert_array_almost_equal(ct.apply(pts), apply_affine(np.dot(AFF3, AFF1), pts))
+    assert_array_almost_equal(ct.apply(POINTS),
+                              apply_affine(np.dot(AFF3, AFF1), POINTS))
     # Does changing params in chain object change components passed in?
     assert_array_equal(aff2_obj.param, np.zeros((12,)))
 
 
 def test_inputs():
     # Check that we can pass arrays or None as pre and post
-    ct = ChainTransform(AFF2.copy())
-    ct = ChainTransform(AFF2.copy(), pre=AFF1)
-    ct = ChainTransform(AFF2.copy(), pre=AFF1, post=AFF3)
+    assert_array_equal(ChainTransform(AFF2).apply(POINTS),
+                       ChainTransform(AFF2_OBJ).apply(POINTS))
+    assert_array_equal(ChainTransform(AFF2, pre=AFF1).apply(POINTS),
+                       ChainTransform(AFF2_OBJ, pre=AFF1_OBJ).apply(POINTS))
+    assert_array_equal(ChainTransform(AFF2, pre=AFF1, post=AFF3).apply(POINTS),
+                       ChainTransform(AFF2_OBJ, pre=AFF1_OBJ, post=AFF3_OBJ).apply(POINTS))
+    assert_array_equal(ChainTransform(AFF2, pre=None).apply(POINTS),
+                       ChainTransform(AFF2_OBJ).apply(POINTS))
+    assert_array_equal(ChainTransform(AFF2, pre=None, post=None).apply(POINTS),
+                       ChainTransform(AFF2_OBJ).apply(POINTS))
 
