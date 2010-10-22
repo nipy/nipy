6b5de48c49e206fa2cd8e6de4ad5973b2cb50148
Matthew Brett <matthew.brett@gmail.com>
2010-10-22 01:46:08 +0100

    RF - major and possibly dangerous refactoring of affine transform classes cython->python

--- nipy/neurospin/registration/affine.py
+++ nipy/neurospin/registration/affine.py
@@ -1,20 +1,16 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 import numpy as np
+import scipy.linalg as spl
 
 from nipy.externals.transforms3d.quaternions import mat2quat, quat2axangle
-from ._registration import param_to_vector12, matrix44, affines, _affines
-
-# Globals 
-naffines = 3
-id_rigid = affines.index('rigid')
-id_similarity = affines.index('similarity')
-id_affine = affines.index('affine')
 
 # Defaults 
 _radius = 100
 _flag2d = False
 
+# Smallest possible scaling
+TINY = 1e-100
 
 def rotation_mat2vec(R):
     """ Rotation vector from rotation matrix `R`
@@ -34,39 +30,71 @@ def rotation_mat2vec(R):
     return ax * angle
 
 
-def vector12(mat, subtype=id_affine):
+def rotation_vec2mat(r):
     """
-    Return a 12-sized vector of natural affine parameters:
-    translation, rotation, log-scale, pre-rotation (to allow for
-    shearing when combined with non-unitary scales).
+    R = rotation_vec2mat(r)
 
-    a better naming is
-    vec12=[translation, post-rot, logscaling, pre-rot]
-
+    The rotation matrix is given by the Rodrigues formula:
+    
+    R = Id + sin(theta)*Sn + (1-cos(theta))*Sn^2  
+    
+    with:
+    
+           0  -nz  ny
+    Sn =   nz   0 -nx
+          -ny  nx   0
+    
+    where n = r / ||r||
+    
+    In case the angle ||r|| is very small, the above formula may lead
+    to numerical instabilities. We instead use a Taylor expansion
+    around theta=0:
+    
+    R = I + sin(theta)/tetha Sr + (1-cos(theta))/teta2 Sr^2
+    
+    leading to:
+    
+    R = I + (1-theta2/6)*Sr + (1/2-theta2/24)*Sr^2
     """
-    TINY = 1e-100
-    vec12 = np.zeros(12)
-    vec12[0:3] = mat[0:3,3]
-    A = mat[0:3,0:3]
-    if subtype == id_rigid: 
-        vec12[3:6] = rotation_mat2vec(A)
-        vec12[6:9] = 0.0
-    elif subtype == id_similarity:
-        ## A = s R ==> det A = (s)**3 ==> s = (det A)**(1/3)
-        s = np.linalg.det(A)**(1/3.)
-        vec12[3:6] = rotation_mat2vec(A/s)
-        vec12[6:9] = np.log(np.maximum(s, TINY))
-    else: 
-        R, s, Q = np.linalg.svd(mat[0:3,0:3]) # mat == R*diag(s)*Q
-        if np.linalg.det(R) < 0: 
-            R = -R
-            Q = -Q
-        r = rotation_mat2vec(R)
-        q = rotation_mat2vec(Q)
-        vec12[3:6] = r
-        vec12[6:9] = np.log(np.maximum(s, TINY))
-        vec12[9:12] = q
-    return vec12
+    theta = spl.norm(r)
+    if theta > 1e-30:
+        n = r/theta
+        Sn = np.array([[0,-n[2],n[1]],[n[2],0,-n[0]],[-n[1],n[0],0]])
+        R = np.eye(3) + np.sin(theta)*Sn + (1-np.cos(theta))*np.dot(Sn,Sn)
+    else:
+        Sr = np.array([[0,-r[2],r[1]],[r[2],0,-r[0]],[-r[1],r[0],0]])
+        theta2 = theta*theta
+        R = np.eye(3) + (1-theta2/6.)*Sr + (.5-theta2/24.)*np.dot(Sr,Sr)
+    return R
+
+
+def matrix44(t, dtype=np.float):
+    """
+    T = matrix44(t)
+
+    t is a vector of of affine transformation parameters with size at
+    least 6.
+
+    size < 6 ==> error
+    size == 6 ==> t is interpreted as translation + rotation
+    size == 7 ==> t is interpreted as translation + rotation + isotropic scaling
+    7 < size < 12 ==> error
+    size >= 12 ==> t is interpreted as translation + rotation + scaling + shearing 
+    """
+    size = t.size
+    T = np.eye(4, dtype=dtype)
+    R = rotation_vec2mat(t[3:6])
+    if size == 6:
+        T[0:3,0:3] = R
+    elif size == 7:
+        T[0:3,0:3] = t[6]*R
+    else:
+        S = np.diag(np.exp(t[6:9])) 
+        Q = rotation_vec2mat(t[9:12]) 
+        # Beware: R*s*Q
+        T[0:3,0:3] = np.dot(R,np.dot(S,Q))
+    T[0:3,3] = t[0:3] 
+    return T 
 
 
 def preconditioner(radius):
@@ -89,7 +117,7 @@ def preconditioner(radius):
 
 
 def inverse_affine(affine):
-    return np.linalg.inv(affine)
+    return spl.inv(affine)
 
 
 def apply_affine(aff, pts):
@@ -162,7 +190,7 @@ class SplineTransform(Transform):
         K = control_points.shape[0]
         self.kernel = kernel
         if param is None:
-            self.param = np.zeros((n_pts,3))
+            self.param = np.zeros((K,3))
         else:
             self.param = np.asarray(param)
         self._cache = {'last_pts': None, 'Gik': None}
@@ -181,36 +209,60 @@ class SplineTransform(Transform):
 
 
 class Affine(Transform): 
+    _param_inds_3d = range(12)
+    _param_inds_2d = [0,1,5,6,7,11]
 
     def __init__(self, array=None, radius=_radius, flag2d=_flag2d):
-        subtype = id_affine
-        self._generic_init(array, radius, subtype, flag2d)
-    
-    def _generic_init(self, array, radius, subtype, flag2d): 
         if array == None: 
             self._vec12 = np.zeros(12)
         elif array.shape == (4,4):
-            self._vec12 = vector12(array)
+            self._vec12 = self._vector12(array)
         elif array.size == 12: 
             self._vec12 = array.ravel()
         else: 
             raise ValueError('Invalid array')
         self._precond = preconditioner(radius)
-        self._subtype = subtype
         self._flag2d = flag2d
-        self._stamp = subtype + naffines*(not flag2d)
+
+    def _param_inds(self):
+        if self._flag2d:
+            return self._param_inds_2d
+        return self._param_inds_3d
+
+    def _vector12(self, aff):
+        """
+        Return a 12-sized vector of natural affine parameters:
+        translation, rotation, log-scale, pre-rotation (to allow for
+        shearing when combined with non-unitary scales).
+
+        a better naming is
+        vec12=[translation, post-rot, logscaling, pre-rot]
+        """
+        vec12 = np.zeros((12,))
+        vec12[0:3] = aff[:3,3]
+        R, s, Q = spl.svd(aff[0:3,0:3]) # mat == R*diag(s)*Q
+        if spl.det(R) < 0:
+            R = -R
+            Q = -Q
+        r = rotation_mat2vec(R)
+        q = rotation_mat2vec(Q)
+        vec12[3:6] = r
+        vec12[6:9] = np.log(np.maximum(s, TINY))
+        vec12[9:12] = q
+        return vec12
 
     def apply(self, xyz): 
         return apply_affine(self.as_affine(), xyz)
 
     def _get_param(self): 
         param = self._vec12/self._precond
-        return param[_affines[self._stamp]]
+        return param[self._param_inds()]
 
-    def _set_param(self, p): 
-        self._vec12 = param_to_vector12(np.asarray(p), self._vec12, 
-                                        self._precond, self._stamp)
-        
+    def _set_param(self, p):
+        p = np.asarray(p)
+        inds = self._param_inds()
+        self._vec12[inds] = p * self._precond[inds]
+
     def _get_translation(self): 
         return self._vec12[0:3]
 
@@ -234,16 +286,16 @@ class Affine(Transform):
 
     def _set_shearing(self, x): 
         self._vec12[9:12] = x
-                
+
     def _get_precond(self): 
         return self._precond 
 
-    param = property(_get_param, _set_param)
     translation = property(_get_translation, _set_translation)
     rotation = property(_get_rotation, _set_rotation)
     scaling = property(_get_scaling, _set_scaling)
     shearing = property(_get_shearing, _set_shearing)
     precond = property(_get_precond)
+    param = property(_get_param, _set_param)
 
     def as_affine(self, dtype='double'): 
         return matrix44(self._vec12, dtype=dtype)
@@ -279,46 +331,89 @@ class Affine(Transform):
         """
         Affine composition: T1oT2(x)
         """
-        subtype = max(self._subtype, other._subtype)
-        a = affine_classes[subtype]()
+        # Choose more capable of input types as output type
+        self_inds = set(self._param_inds())
+        other_inds = set(other._param_inds())
+        if self_inds.issubset(other_inds):
+            klass = other.__class__
+        elif other_inds.isssubset(self_inds):
+            klass = self.__class__
+        else: # neither one contains capabilities of the other
+            klass = Affine
+        a = klass()
         a._precond = self._precond
-        a._vec12 = vector12(np.dot(self.as_affine(), other.as_affine()), a._subtype)
+        a._vec12 = a._vector12(np.dot(self.as_affine(), other.as_affine()))
         return a
 
     def inv(self):
         """
         Return the inverse affine transform. 
         """
-        a = affine_classes[self._subtype]()
+        a = self.__class__()
         a._precond = self._precond
-        a._vec12 = vector12(np.linalg.inv(self.as_affine()), self._subtype)
+        a._vec12 = a._vector12(spl.inv(self.as_affine()))
         return a
-        
+
 
 class Rigid(Affine):
+    _param_inds_2d = [0, 1, 5]
+    _param_inds_3d = range(6)
 
-    def __init__(self, array=None, radius=_radius, flag2d=_flag2d):
-        subtype = id_rigid
-        self._generic_init(array, radius, subtype, flag2d)
-        
-    def __str__(self): 
+    def _vector12(self, aff):
+        """
+        Return a 12-sized vector of natural affine parameters:
+        translation, rotation, log-scale, pre-rotation (to allow for
+        shearing when combined with non-unitary scales).
+
+        a better naming is
+        vec12=[translation, post-rot, logscaling, pre-rot]
+        """
+        vec12 = np.zeros((12,))
+        vec12[:3] = aff[:3,3]
+        vec12[3:6] = rotation_mat2vec(aff[:3,:3])
+        vec12[6:9] = 0.0
+        return vec12
+
+    def __str__(self):
         string  = 'translation : %s\n' % str(self.translation)
         string += 'rotation    : %s\n' % str(self.rotation)
         return string
 
-class Similarity(Affine):
 
-    def __init__(self, array=None, radius=_radius, flag2d=_flag2d):
-        subtype = id_similarity
-        self._generic_init(array, radius, subtype, flag2d)
+class Similarity(Affine):
+    _param_inds_2d = [0, 1, 5, 6]
+    _param_inds_3d = range(7)
+
+    def _vector12(self, aff):
+        """
+        Return a 12-sized vector of natural affine parameters:
+        translation, rotation, log-scale, pre-rotation (to allow for
+        shearing when combined with non-unitary scales).
+
+        a better naming is
+        vec12=[translation, post-rot, logscaling, pre-rot]
+        """
+        vec12 = np.zeros((12,))
+        vec12[:3] = aff[:3,3]
+        ## A = s R ==> det A = (s)**3 ==> s = (det A)**(1/3)
+        A = aff[:3,:3]
+        s = spl.det(A)**(1/3.)
+        vec12[3:6] = rotation_mat2vec(A/s)
+        vec12[6:9] = np.log(np.maximum(s, TINY))
+
+    def _set_param(self, p):
+        p = np.asarray(p)
+        if self._flag2d:
+            self._vec12[[0,1,5,6,7]] = (p[[0,1,2,3,3]] *
+                                        self._precond[[0,1,5,6,7]])
+        else:
+            self._vec12[range(9)] = (p[[0,1,2,3,4,5,6,6,6]] *
+                                     self._precond[range(9)])
+
+    param = property(Affine._get_param, _set_param)
 
     def __str__(self): 
         string  = 'translation : %s\n' % str(self.translation)
         string += 'rotation    : %s\n' % str(self.rotation)
         string += 'scaling     : %s\n' % str(self.scaling[0])
         return string
-    
-
-# List of 
-affine_classes = [Rigid, Similarity, Affine]
-
