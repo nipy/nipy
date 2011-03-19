f03be8ec8f4716bbc718ad8cc73db186d5809763
Alexis Roche <alexis.roche@gmail.com>
2011-03-19 01:05:37 +0100

    clean segmentation example script

--- examples/tissue_classification.py
+++ examples/tissue_classification.py
@@ -4,10 +4,17 @@
 """
 Perform brain tissue classification from skull stripped T1 image in
 CSF, GM and WM.
+
+Usage: 
+  python tissue_classification img_file [niters][mask_file]
+
+If no mask image is provided, the mask is defined by thresholding the
+input image above zero (strictly). 
 """
 
-from os.path import join 
-
+from os.path import join, split
+import sys
+import tempfile 
 import numpy as np 
 
 from nipy import load_image, save_image 
@@ -51,32 +58,43 @@ def fuzzy_dice(gt, ppm, labels, mask):
         dices[k] = 2*PQ/float(P+Q)
     return dices
 
-
-
 def hard_classification(im, mask, ppm): 
     tmp = np.zeros(im.shape, dtype='uint8')
     tmp[mask] = ppm[mask].argmax(1) + 1
     return AffineImage(tmp, im.affine, 'scanner')
 
 
-
-prefix = join('/home/alexis/E/Data/brainweb')
-im = load_image(join(prefix, 'brainweb_SS.nii'))
-mask = np.where(im.get_data()>0)
-
-gt_csf = load_image(join(prefix, 'phantom_1.0mm_normal_csf_.nii'))
-gt_gm = load_image(join(prefix, 'phantom_1.0mm_normal_gry_.nii'))
-gt_wm = load_image(join(prefix, 'phantom_1.0mm_normal_wht_.nii'))
-gt = [gt_csf, gt_gm, gt_wm]
+if len(sys.argv)<2:
+    raise ValueError('Missing input image.') 
+im = load_image(sys.argv[1])
+if len(sys.argv)>2: 
+    niters = int(sys.argv[2])
+    magic = niters<0
+    niters = abs(niters) 
+if len(sys.argv)>3:
+    mask_im = load_image(sys.argv[3])
+    mask = np.where(mask_im.get_data()>0)
+else:
+    mask = np.where(im.get_data()>0)
 
 mu, sigma = moment_matching(im, mask)
-
 vem = VEM(im.get_data(), 3, mask=mask, labels=LABELS)
-vem.run(mu=mu, sigma=sigma, prop=[.20, .47, .33], freeze_prop=False, niters=25, beta=1.2)
-
-d = fuzzy_dice(gt, vem.ppm, LABELS, mask)
-print(d) 
-
+vem.run(mu=mu, sigma=sigma, prop=[.20, .47, .33], freeze_prop=False, niters=niters, beta=1.2)
 classif = hard_classification(im, mask, vem.ppm) 
-save_image(classif, 'classif.nii')
+
+outfile = join(tempfile.mkdtemp(), 'classif.nii')
+save_image(classif, outfile)
+print('Label image saved in: %s' % outfile) 
+
+############################################################
+if magic: 
+    print('Computing Dice index') 
+    datadir, _ = split(sys.argv[1]) 
+    prefix = 'phantom_1.0mm_normal_'
+    fuzzy_model = [prefix+t+'_.nii' for t in ('csf','gry','wht')]
+    gt = [load_image(join(datadir, f)) for f in fuzzy_model]
+    d = fuzzy_dice(gt, vem.ppm, LABELS, mask)
+    print('Fuzzy Dice indices: %s' % d) 
+
+
 
