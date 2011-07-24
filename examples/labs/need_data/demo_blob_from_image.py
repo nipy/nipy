6345e96496eb0295b48a1985f195ac6cb06045d1
bthirion <bertrand.thirion@inria.fr>
2011-07-24 23:37:34 +0200

    STYLE: more pep8

--- examples/labs/need_data/demo_blob_from_image.py
+++ examples/labs/need_data/demo_blob_from_image.py
@@ -40,7 +40,7 @@ mask_image = Nifti1Image((nim.get_data() ** 2 > 0).astype('u8'),
                          nim.get_affine())
 domain = domain_from_image(mask_image)
 data = nim.get_data()
-values = data[data!=0]
+values = data[data != 0]
 
 # compute the  nested roi object
 nroi = hroi.HROI_as_discrete_domain_blobs(domain, values,
@@ -49,34 +49,34 @@ nroi = hroi.HROI_as_discrete_domain_blobs(domain, values,
 # compute region-level activation averages
 nroi.make_feature('activation', values)
 average_activation = nroi.representative_feature('activation')
-bmap = -np.zeros(domain.size)
+bmap = - np.zeros(domain.size)
 for k in range(nroi.k):
-    bmap[nroi.label==k] = average_activation[k]
+    bmap[nroi.label == k] = average_activation[k]
 
-# saving the blob image,i. e. a label image 
-wlabel = -2*np.ones(nim.get_shape())
-wlabel[data!=0] = nroi.label
+# saving the blob image, i. e. a label image
+wlabel = -2 * np.ones(nim.get_shape())
+wlabel[data != 0] = nroi.label
 wim = Nifti1Image(wlabel, nim.get_affine())
-wim.get_header()['descrip'] = 'blob image extracted from %s'%input_image 
-save(wim,op.join(swd,"blob.nii"))
+wim.get_header()['descrip'] = 'blob image extracted from %s' % input_image
+save(wim, op.join(swd, "blob.nii"))
 
 # saving the image of the average-signal-per-blob
 wlabel = np.zeros(nim.get_shape())
-wlabel[data!=0] = bmap
+wlabel[data != 0] = bmap
 wim = Nifti1Image(wlabel, nim.get_affine())
 wim.get_header()['descrip'] = 'blob average signal extracted from %s' \
-                              %input_image 
-save(wim,op.join(swd,"bmap.nii"))
+                              % input_image
+save(wim, op.join(swd, "bmap.nii"))
 
 # saving the image of the end blobs or leaves
 lroi = nroi.reduce_to_leaves()
 
-wlabel = -2*np.ones(nim.get_shape())
-wlabel[data!=0] = lroi.label
+wlabel = - 2 * np.ones(nim.get_shape())
+wlabel[data != 0] = lroi.label
 wim = Nifti1Image(wlabel, nim.get_affine())
-wim.get_header()['descrip'] = 'blob image extracted from %s'%input_image  
-save(wim,op.join(swd,"leaves.nii"))
+wim.get_header()['descrip'] = 'blob image extracted from %s' % input_image
+save(wim, op.join(swd, "leaves.nii"))
 
-print "Wrote the blob image in %s" %op.join(swd,"blob.nii")
-print "Wrote the blob-average signal image in %s" %op.join(swd,"bmap.nii")
-print "Wrote the end-blob image in %s" %op.join(swd,"leaves.nii")
+print "Wrote the blob image in %s" % op.join(swd, "blob.nii")
+print "Wrote the blob-average signal image in %s" % op.join(swd, "bmap.nii")
+print "Wrote the end-blob image in %s" % op.join(swd, "leaves.nii")
