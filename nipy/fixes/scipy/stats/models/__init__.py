9db6b1b90f53300bd0c6128b709b87389f7c3b62
Matthew Brett <matthew.brett@gmail.com>
2011-08-04 12:17:29 -0700

    RF+BF - fix confusion of glm module and class
    We were importing the 'glm.Model' class as 'glm', so the class looked
    like the module, leading to confusing test failures

--- nipy/fixes/scipy/stats/models/__init__.py
+++ nipy/fixes/scipy/stats/models/__init__.py
@@ -10,7 +10,7 @@ from nipy.fixes.scipy.stats.models.info import __doc__
 
 import nipy.fixes.scipy.stats.models.model
 import nipy.fixes.scipy.stats.models.regression
-from nipy.fixes.scipy.stats.models.glm import Model as glm
+import nipy.fixes.scipy.stats.models.glm
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
 
