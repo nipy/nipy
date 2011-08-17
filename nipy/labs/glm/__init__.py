6b4493593b1eecdec06e218fa8c962e0b2c3c5e2
Matthew Brett <matthew.brett@gmail.com>
2011-08-17 15:42:46 -0700

    RF - use nipy tester instead of numpy version
    Move __init__s to use relative imports

--- nipy/labs/glm/__init__.py
+++ nipy/labs/glm/__init__.py
@@ -1,8 +1,7 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-from glm import *
+from .glm import *
 
-from numpy.testing import Tester
-
+from nipy.testing import Tester
 test = Tester().test
 bench = Tester().bench
