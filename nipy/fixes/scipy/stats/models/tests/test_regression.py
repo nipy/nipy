3d0941c402580e61ad430e7f948f77a9638e136d
Matthew Brett <matthew.brett@gmail.com>
2011-08-19 12:35:32 -0700

    TST - expand Yule Walker tests to sigma2
    JB and I found why R and our routine differ on the estimate of sigma2.
    
    Use older np.dot(X, Y) instead of X.dot(Y) for compatibility

--- nipy/fixes/scipy/stats/models/tests/test_regression.py
+++ nipy/fixes/scipy/stats/models/tests/test_regression.py
@@ -51,21 +51,28 @@ def test_AR_degenerate():
     assert_equal(results.df_resid, 31)
 
 
-def test_yule_walker():
+def test_yule_walker_R():
     # Test YW implementation against R results
     Y = np.array([1,3,4,5,8,9,10])
     N = len(Y)
     X = np.ones((N, 2))
     X[:,0] = np.arange(1,8)
     pX = spl.pinv(X)
-    betas = pX.dot(Y)
-    Yhat = Y - X.dot(betas)
-    # R results from saving Yhat, then:
-    # ar.yw(yhat$V1, aic=FALSE, order.max=2)
+    betas = np.dot(pX, Y)
+    Yhat = Y - np.dot(X, betas)
+    # R results obtained from:
+    # >>> np.savetxt('yhat.csv', Yhat)
+    # > yhat = read.table('yhat.csv')
+    # > ar.yw(yhat$V1, aic=FALSE, order.max=2)
+    def r_fudge(sigma, order):
+        # Reverse fudge in ar.R calculation labeled as splus compatibility fix
+        return sigma **2 * N / (N-order-1)
     rhos, sd = yule_walker(Yhat, 1, 'mle')
     assert_array_almost_equal(rhos, [-0.3004], 4)
+    assert_array_almost_equal(r_fudge(sd, 1), 0.2534, 4)
     rhos, sd = yule_walker(Yhat, 2, 'mle')
     assert_array_almost_equal(rhos, [-0.5113, -0.7021], 4)
+    assert_array_almost_equal(r_fudge(sd, 2), 0.1606, 4)
     rhos, sd = yule_walker(Yhat, 3, 'mle')
     assert_array_almost_equal(rhos, [-0.6737, -0.8204, -0.2313], 4)
-
+    assert_array_almost_equal(r_fudge(sd, 3), 0.2027, 4)
