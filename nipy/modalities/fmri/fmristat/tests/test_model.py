fd84306626774bc5fba3abf8fc41b7dd420298fd
Matthew Brett <matthew.brett@gmail.com>
2011-08-30 12:13:16 -0700

    RF+BRK - rewriting of skipped FMRI model tests

--- nipy/modalities/fmri/fmristat/tests/test_model.py
+++ nipy/modalities/fmri/fmristat/tests/test_model.py
@@ -1,79 +1,43 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-import os
 import warnings
-from shutil import rmtree
-from tempfile import mkstemp, mkdtemp
-
-from nipy.testing import TestCase, funcfile, dec
 
 from nipy.io.api import load_image
 
-import nipy.modalities.fmri.fmristat.model as model
-from nipy.modalities.fmri.api import FmriImageList
+from  .. import model
+from ...api import FmriImageList
+from ...formula import Formula, Term, make_recarray
+from nibabel.tmpdirs import InTemporaryDirectory
 
-# FIXME: these things are obsolete
-# from nipy.modalities.fmri.formula import \
-#     ExperimentalQuantitative
-# from nipy.fixes.scipy.stats.models.contrast import Contrast
-
-def setup():
-    # Suppress warnings during tests to reduce noise
-    warnings.simplefilter("ignore")
-
-def teardown():
-    # Clear list of warning filters
-    warnings.resetwarnings()
+from nipy.testing import funcfile
 
 
-class test_fMRIstat_model(TestCase):
+# FIXME: This does many things, but it does not test any values
+# with asserts.
+def test_run():
+    ar1_fname = 'ar1_out.nii'
+    funcim = load_image(funcfile)
+    fmriims = FmriImageList.from_image(funcim, volume_start_times=2.)
+    # Formula - with no intercept
+    t = Term('t')
+    f = Formula([t, t**2, t**3])
+    # Design matrix and contrasts
+    time_vector = make_recarray(fmriims.volume_start_times, 't')
+    con_defs = dict(c=t, c2=t+t**2)
+    desmtx, cmatrices = f.design(time_vector, contrasts=con_defs)
 
-    def setUp(self):
-        # Using mkstemp instead of NamedTemporaryFile.  MS Windows
-        # cannot reopen files created with NamedTemporaryFile.
-        _, self.ar1 = mkstemp(prefix='ar1_', suffix='.nii')
-        _, self.resid_OLS = mkstemp(prefix='resid_OSL_', suffix='.nii')
-        _, self.F = mkstemp(prefix='F_', suffix='.nii')
-        _, self.resid = mkstemp(prefix='resid_', suffix='.nii')
-        # Use a temp directory for the model.output_T images
-        self.out_dir = mkdtemp()
-
-    def tearDown(self):
-        os.remove(self.ar1)
-        os.remove(self.resid_OLS)
-        os.remove(self.F)
-        os.remove(self.resid)
-        rmtree(self.out_dir)
-
-    # FIXME: This does many things, but it does not test any values
-    # with asserts.
-    @dec.skipif(True)
-    def testrun(self):
-        funcim = load_image(funcfile)
-        fmriims = FmriImageList.from_image(funcim, volume_start_times=2.)
-
-        f1 = ExperimentalQuantitative("f1", lambda t:t)
-        f2 = ExperimentalQuantitative("f1", lambda t:t**2)
-        f3 = ExperimentalQuantitative("f1", lambda t:t**3)
-
-        f = f1 + f2 + f3
-        c = Contrast(f1, f)
-        c.compute_matrix(fmriims.volume_start_times)
-        c2 = Contrast(f1 + f2, f)
-        c2.compute_matrix(fmriims.volume_start_times)
-
+    with InTemporaryDirectory():
+        # Run OLS model
         outputs = []
-        outputs.append(model.output_AR1(self.ar1, fmriims, clobber=True))
-        outputs.append(model.output_resid(self.resid_OLS, fmriims, 
-                                          clobber=True))
+        outputs.append(model.output_AR1(ar1_fname, fmriims))
+        outputs.append(model.output_resid('resid_OLS_out.nii', fmriims))
         ols = model.OLS(fmriims, f, outputs)
         ols.execute()
 
         outputs = []
-        out_fn = os.path.join(self.out_dir, 'out.nii')
-        outputs.append(model.output_T(out_fn, c, fmriims, clobber=True))
-        outputs.append(model.output_F(self.F, c2, fmriims, clobber=True))
-        outputs.append(model.output_resid(self.resid, fmriims, clobber=True))
-        rho = load_image(self.ar1)
+        outputs.append(model.output_T('T_out.nii', cmatrices['c'], fmriims))
+        outputs.append(model.output_F('F_out.nii', cmatrices['c2'], fmriims))
+        outputs.append(model.output_resid('resid_AR_out.nii', fmriims))
+        rho = load_image(ar1_fname)
         ar = model.AR1(fmriims, f, rho, outputs)
         ar.execute()
