019720e5a4cee9eb1477a1576627c1d9330d7243
Matthew Brett <matthew.brett@gmail.com>
2011-09-10 23:52:58 -0700

    BF+RF - update examples to formula API
    The examples are not fully updated and need more work.
    This updates them to use the current imports for formula.
    Cleanup to the FIAC example
    Porting the FIR example to use the current formula API.
    create_fmri_model example still broken

--- examples/formula/fir.py
+++ examples/formula/fir.py
@@ -1,51 +1,47 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 import numpy as np
-import sympy
-from nipy.modalities.fmri import formula, utils, hrf
+import matplotlib.pyplot as plt
 
-import pylab
+from nipy.fixes.sympy.utilities.lambdify import implemented_function
+from nipy.algorithms.statistics.api import Formula
+from nipy.modalities.fmri import utils
 
-t = formula.Term('t')
-
-def linBspline(t, knots):
+def linBspline(knots):
     """ Create a linear B spline that is zero outside [knots[0],
     knots[-1]] (knots is assumed to be sorted).
     """
-    fns = []; symbols=[]
+    fns = []
     knots = np.array(knots)
     for i in range(knots.shape[0]-2):
-        n = 'bs_%s' % i
-        s = sympy.Function(n)
+        name = 'bs_%s' % i
         k1, k2, k3 = knots[i:i+3]
         d1 = k2-k1
-        d2 = k3-k2
         def anon(x,k1=k1,k2=k2,k3=k3):
-            return ((x-k1) / d1 * np.greater(x, k1) * np.less_equal(x, k2) + 
+            return ((x-k1) / d1 * np.greater(x, k1) * np.less_equal(x, k2) +
                     (k3-x) / d1 * np.greater(x, k2) * np.less(x, k3))
-        fns.append((n, anon))
-        symbols.append(s(t))
+        fns.append(implemented_function(name, anon))
+    return fns
 
-    ff = formula.Formula(symbols)
-    for n, l in fns:
-        ff.aliases[n] = l
-    return ff
 
-t = formula.Term('t')
-bsp = linBspline(t, np.arange(0,10,2))
+# The splines are functions of t (time)
+bsp_fns = linBspline(np.arange(0,10,2))
+
+# We're going to evaluate at these specific values of time
 tt = np.linspace(0,50,101)
-tval = tt.view(np.dtype([('t', np.float)]))
+tvals= tt.view(np.dtype([('t', np.float)]))
 
-e = np.random.uniform(low=0, high=3, size=(20,)) + 20.
-e = np.cumsum(e)
-f = formula.Formula([utils.events(e, f=hrf.symbolic(term)) for term in bsp.design])
-for k, v in bsp.aliases.items():
-    f.aliases[k] = v
+# Some interstimulus intervals
+isis = np.random.uniform(low=0, high=3, size=(4,)) + 10.
+# Made into event onset times
+e = np.cumsum(isis)
 
-d = formula.Design(f, return_float=True)
-X = d(tval)
+f = Formula([utils.events(e, f=fn) for fn in bsp_fns])
 
-pylab.plot(X[:,0])
-pylab.plot(X[:,1])
-pylab.plot(X[:,2])
-pylab.show()
+# The design matrix
+X = f.design(tvals, return_float=True)
+
+plt.plot(X[:,0])
+plt.plot(X[:,1])
+plt.plot(X[:,2])
+plt.show()
--- examples/formula/simple_contrast.py
+++ examples/formula/simple_contrast.py
@@ -1,9 +1,9 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 import numpy as np
-import nipy.testing as niptest
-import sympy
-from nipy.modalities.fmri import formula, utils, hrf
+
+from nipy.algorithms.statistics.api import Formula, make_recarray
+from nipy.modalities.fmri import utils, hrf
 from nipy.modalities.fmri.fmristat import hrf as delay
 
 c1 = utils.events([3,7,10], f=hrf.glover) # Symbolic function of time
@@ -11,16 +11,16 @@ c2 = utils.events([1,3,9], f=hrf.glover) # Symbolic function of time
 c3 = utils.events([3,4,6], f=delay.spectral[0])
 d = utils.fourier_basis([3,5,7]) # Formula
 
-f = formula.Formula([c1,c2,c3]) + d
-contrast = formula.Formula([c1-c2, c1-c3])
+f = Formula([c1,c2,c3]) + d
+contrast = Formula([c1-c2, c1-c3])
 
-t = formula.make_recarray(np.linspace(0,20,50), 't')
+t = make_recarray(np.linspace(0,20,50), 't')
 
 X, c = f.design(t, return_float=True, contrasts={'C':contrast})
 preC = contrast.design(t, return_float=True)
 
 C = np.dot(np.linalg.pinv(X), preC).T
-niptest.assert_almost_equal(C, c['C'])
+np.testing.assert_almost_equal(C, c['C'])
 
 print C
 
