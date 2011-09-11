0ff2906c4dfb45f3ae6098463cfa371d92c3afea
Matthew Brett <matthew.brett@gmail.com>
2011-09-10 20:31:21 -0700

    BF - fix broken import of OLSModel
    Trying to get OLSModel out of the statistics package instead of
    statistics.models

--- nipy/algorithms/statistics/tests/test_regression.py
+++ nipy/algorithms/statistics/tests/test_regression.py
@@ -3,7 +3,8 @@
 
 import numpy as np
 
-from ..regression import output_T, output_F, OLSModel
+from ..models.regression import OLSModel
+from ..regression import output_T, output_F
 
 from nose.tools import assert_true, assert_equal, assert_raises
 
