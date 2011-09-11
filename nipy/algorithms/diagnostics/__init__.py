3cdc936373215278e8df71447e4f3588e63d3a53
Matthew Brett <matthew.brett@gmail.com>
2011-09-10 22:10:20 -0700

    RF - moved PCA into algorithms.utils
    PCA was in modalilities.fmri, but the routine is general

--- nipy/algorithms/diagnostics/__init__.py
+++ nipy/algorithms/diagnostics/__init__.py
@@ -5,4 +5,4 @@
 from .timediff import time_slice_diffs
 from .tsdiffplot import plot_tsdiffs, plot_tsdiffs_image
 from .screens import screen
-from ...modalities.fmri import pca
+from ..utils import pca
