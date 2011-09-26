d3fa102a0cffc579b61d8d1bf4baee489862ee7e
VirgileFritsch <virgile.fritsch@gmail.com>
2011-09-26 15:00:39 +0200

    Refactoring of the MultiROI and HierarchicalROI classes.
    Tests have been added, examples have been checked, and doc has been
    written.
    - We no longer rely on the voxels order to manipulate the objects.
    - Instead, a special feature, `id` is introduced and should be used
      to construct feature arrays/lists, or to access them.
    - We now control the merge strategy in HROI's merge_ascending method.
    - set_feature and make_feature methods have been combined into one.
    - Providing the id of a ROI, one can access individual feature/ROI feature
      for this ROI.
    - We cannot have a feature and a ROI feature having the same name.
    - to_image method easier to use, and can write every feature/ROI feature
      at will.

--- examples/labs/blob_extraction.py
+++ examples/labs/blob_extraction.py
@@ -16,6 +16,7 @@ import matplotlib
 import nipy.labs.utils.simul_multisubject_fmri_dataset as simul
 import nipy.labs.spatial_models.hroi as hroi
 from nipy.labs.spatial_models.discrete_domain import domain_from_array
+from nipy.labs.spatial_models.mroi import subdomain_from_array
 
 # ---------------------------------------------------------
 # simulate an activation image
@@ -31,21 +32,20 @@ values = dataset.ravel()
 
 #-------------------------------------------------------
 # Computations
-#------------------------------------------------------- 
+#-------------------------------------------------------
 
 # create a domain descriptor associated with this
 domain = domain_from_array(dataset ** 2 > 0)
 
 nroi = hroi.HROI_as_discrete_domain_blobs(domain, dataset.ravel(),
                                           threshold=2.0, smin=3)
-label = np.reshape(nroi.label, shape)
-
 # create an average activaion image
-nroi.make_feature('activation', dataset.ravel())
-mean_activation = nroi.representative_feature('activation')
-bmap = - np.ones(shape)
-for k in range(nroi.k):
-    bmap[label == k] = mean_activation[k]
+flat_dataset = dataset.ravel()
+activation = [flat_dataset[nroi.select_id(id, roi=False)]
+              for id in nroi.get_id()]
+nroi.set_feature('activation', activation)
+bmap = nroi.feature_to_voxel_map(
+    'activation', roi=True, method="mean").reshape((dimx, dimy))
 
 #--------------------------------------------------------
 # Result display
@@ -73,13 +73,14 @@ pl.imshow(dataset, interpolation='nearest', cmap=my_cmap)
 cb = pl.colorbar()
 for t in cb.ax.get_yticklabels():
     t.set_fontsize(16)
-    
+
 pl.axis('off')
 pl.title('Thresholded data')
 
 # plot the blob label image
 pl.subplot(1, 3, 2)
-pl.imshow(label, interpolation='nearest')
+pl.imshow(nroi.feature_to_voxel_map('id', roi=True).reshape((dimx, dimy)),
+          interpolation='nearest')
 pl.colorbar()
 pl.title('Blob labels')
 
--- examples/labs/hierarchical_rois.py
+++ examples/labs/hierarchical_rois.py
@@ -39,10 +39,12 @@ root = np.argmax(td)
 lv = n1.make_forest().get_descendents(root)
 u = nroi.make_graph().cc()
 
-nroi.make_feature('activation', dataset.ravel())
-nroi.representative_feature('activation')
+flat_data = dataset.ravel()
+activation = [flat_data[nroi.select_id(id, roi=False)]
+              for id in nroi.get_id()]
+nroi.set_feature('activation', activation)
 
-label = np.reshape(n1.label, shape)
+label = np.reshape(n1.feature_to_voxel_map('id', roi=True), shape)
 
 # make a figure
 import matplotlib.pylab as mp
