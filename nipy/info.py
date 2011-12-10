f1ee694f2070a99fb8b0bb5ecd2cdbd5f8a48159
Yaroslav Halchenko <debian@onerussian.com>
2011-12-10 09:44:52 -0500

    ENH: starting development toward 0.2 (or 0.1.9999)

--- nipy/info.py
+++ nipy/info.py
@@ -8,8 +8,8 @@ docs.  In setup.py in particular, we exec this file, so it cannot import nipy
 # version
 _version_major = 0
 _version_minor = 1
-_version_micro = 999
-_version_extra = ''
+_version_micro = 9999
+_version_extra = '.dev'
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 __version__ = "%s.%s.%s%s" % (_version_major,
