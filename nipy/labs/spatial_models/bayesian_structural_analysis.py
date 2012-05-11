8f436aa6a3726a411071590f3608d333361a99a6
bthirion <bertrand.thirion@inria.fr>
2012-05-11 22:48:05 +0200

    BF: fixed the matplotlib import in bayesian_structural_analysis

--- nipy/labs/spatial_models/bayesian_structural_analysis.py
+++ nipy/labs/spatial_models/bayesian_structural_analysis.py
@@ -277,14 +277,17 @@ def bsa_dpmm(bf, gf0, sub, gfc, dmax, thq, ths, verbose=0):
     if verbose:
         h1, c1 = np.histogram((1 - gf0), bins=100)
         h2, c2 = np.histogram(q, bins=100)
-        import pylab
-        pylab.figure()
-        pylab.plot(1 - gf0, q, '.')
-        pylab.figure()
-        pylab.bar(c1[:len(h1)], h1, width=0.005)
-        pylab.bar(c2[:len(h2)] + 0.003, h2, width=0.005, color='r')
+        try:
+            import matplotlib.pylab as pl
+            pl.figure()
+            pl.plot(1 - gf0, q, '.')
+            pl.figure()
+            pl.bar(c1[:len(h1)], h1, width=0.005)
+            pl.bar(c2[:len(h2)] + 0.003, h2, width=0.005, color='r')
+        except ImportError:
+            pass
         print 'Number of candidate regions %i, regions found %i' % (
-                    np.size(q), q.sum())
+            np.size(q), q.sum())
 
     Fbeta = field_from_coo_matrix_and_data(dom.topology, p)
     _, label = Fbeta.custom_watershed(0, g0)
