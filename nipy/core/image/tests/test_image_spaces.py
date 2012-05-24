bbc4c281b595ac8a29b11455bc71d6db06e59ee0
Matthew Brett <matthew.brett@gmail.com>
2012-05-24 16:56:05 -0400

    ENH: neuro_image image creation function
    Allow creation of images in standard neuroimaging space conventions from
    a utility function.  This allows us to do stuff like this::
    
        >>> data = np.arange(24).reshape((2, 3, 4))
        >>> aff = np.diag([4, 5, 6, 1])
        >>> img = neuro_image(data, aff, 'mni')
    
    where ``aff`` here is a standard neuroimaging xyz affine.

--- nipy/core/image/tests/test_image_spaces.py
+++ nipy/core/image/tests/test_image_spaces.py
@@ -7,10 +7,14 @@ import nibabel as nib
 from nibabel.affines import from_matvec
 
 from ..image import Image, rollaxis as img_rollaxis
-from ..image_spaces import is_xyz_affable, as_xyz_affable, xyz_affine
+from ..image_spaces import (is_xyz_affable, as_xyz_affable, xyz_affine,
+                            neuro_image)
 from ...reference.coordinate_system import CoordinateSystem as CS
 from ...reference.coordinate_map import AffineTransform
-from ...reference.spaces import (vox2mni, AffineError, AxesError)
+from ...reference.spaces import (vox2mni, vox2talairach, voxel_csm,
+                                 mni_space, talairach_space,
+                                 AffineError, AxesError,
+                                 XYZSpace, SpaceError)
 
 from numpy.testing import (assert_array_almost_equal,
                            assert_array_equal)
@@ -102,3 +106,44 @@ def test_image_xyza_slices():
     assert_raises(AxesError, as_xyz_affable, img0)
     img0r = img0.reordered_reference([1,0,2,3]).reordered_axes([2,0,1])
     assert_raises(AxesError, as_xyz_affable, img0r)
+
+
+def test_neuro_image():
+    # Standard neuro image creator
+    arr = np.arange(24).reshape((1,2,3,4))
+    aff = np.diag([2,3,4,1])
+    img = neuro_image(arr, aff, 'mni')
+    assert_equal(img.coordmap, vox2mni(aff, 1.0))
+    assert_array_equal(img.get_data(), arr)
+    img = neuro_image(arr, aff, 'talairach')
+    assert_equal(img.coordmap, vox2talairach(aff, 1.0))
+    assert_array_equal(img.get_data(), arr)
+    img = neuro_image(arr, aff, talairach_space)
+    assert_equal(img.coordmap, vox2talairach(aff, 1.0))
+    # Unknown space as string raises SpaceError
+    assert_raises(SpaceError, neuro_image, arr, aff, 'unlikely space name')
+    funky_space = XYZSpace('hija')
+    img = neuro_image(arr, aff, funky_space)
+    csm = funky_space.to_coordsys_maker('t')
+    in_cs = CS('ijkl', 'array')
+    exp_cmap = AffineTransform(in_cs, csm(4), np.diag([2, 3, 4, 1, 1]))
+    assert_equal(img.coordmap, exp_cmap)
+    # Affine must be 4, 4
+    bad_aff = np.diag([2,3,4,2,1])
+    assert_raises(ValueError, neuro_image, arr, bad_aff, 'mni')
+    # Can pass added zooms
+    img = neuro_image(arr, (aff, (2.,)), 'mni')
+    assert_equal(img.coordmap, vox2mni(aff, 2.0))
+    # Also as scalar
+    img = neuro_image(arr, (aff, 2.), 'mni')
+    assert_equal(img.coordmap, vox2mni(aff, 2.0))
+    # Must match length of needed zooms
+    arr5 = arr[...,None]
+    assert_raises(ValueError, neuro_image, arr5, (aff, 2.), 'mni')
+    img = neuro_image(arr5, (aff, (2., 3.)), 'mni')
+    assert_equal(img.coordmap, vox2mni(aff, (2.0, 3.0)))
+    # Always xyz affable after creation
+    assert_array_equal(xyz_affine(img), aff)
+    assert_true(is_xyz_affable(img))
+    # Need at least 3 dimensions in data
+    assert_raises(ValueError, neuro_image, np.zeros((2,3)), aff, 'mni')
