e39d607170dc981dfa5043af4f61da449da25403
Matthew Brett <matthew.brett@gmail.com>
2012-06-04 15:02:15 -0400

    TEST: re-enable load / save tests for reordering
    Jonathan disabled these tests when the nifti save did not reorder the
    data axes according to x, y, z.  It looks like the old code left the
    input axes as (x, y, z) in the first 3, but in any order.  The current
    code does reorder to (x, y, z) exactly, and I adapted the tests to this.
    Also, we now use the nifti ``toffset`` for time offset.

--- nipy/io/tests/test_save.py
+++ nipy/io/tests/test_save.py
@@ -2,14 +2,24 @@
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 import numpy as np
 
+from nibabel.affines import from_matvec
+
 from nipy.io.api import load_image, save_image
 from nipy.core import api
+from nipy.core.reference.coordinate_map import (
+    AffineTransform as AT)
+from nipy.core.reference.coordinate_system import (
+    CoordinateSystem as CS)
+from nipy.core.reference.spaces import mni_csm
 
 from nibabel.tmpdirs import InTemporaryDirectory
 
-from nipy.testing import (assert_true, assert_false, assert_equal,
-                          assert_array_almost_equal, funcfile)
+from nose.tools import (assert_true, assert_false, assert_equal,
+                        assert_not_equal)
 
+from numpy.testing import assert_array_almost_equal
+
+from nipy.testing import funcfile
 
 TMP_FNAME = 'afile.nii'
 
@@ -67,78 +77,76 @@ def test_save2b():
         assert_array_almost_equal(img2.get_data(), img.get_data())
         del img2
 
-# JT: nifti_ref doesn't reorder axes anymore so these tests
-# are no longer expected to work
-#
-# def test_save3():
-#     # A test to ensure that when a file is saved, the affine
-#     # and the data agree. In this case, things don't agree:
-#     # i) the pixdim is off
-#     # ii) makes the affine off
 
-#     step = np.array([3.45,2.3,4.5,6.9])
-#     shape = (13,5,7,3)
-#     cmap = api.AffineTransform.from_start_step('jkli', 'tzyx', [0,3,5,1], step)
-
-#     data = np.random.standard_normal(shape)
-#     img = api.Image(data, cmap)
-#     save_image(img, TMP_FNAME)
-#     img2 = load_image(TMP_FNAME)
-
-#     yield assert_equal, tuple([img.shape[l] for l in [3,0,1,2]]), img2.shape
-#     a = np.transpose(np.asarray(img), [3,0,1,2])
-#     yield assert_false, np.allclose(img.affine, img2.affine)
-#     yield assert_true, np.allclose(a, np.asarray(img2))
-
-
-# def test_save4():
-#     # Same as test_save3 except we have reordered the 'ijk' input axes.
-#     shape = (13,5,7,3)
-#     step = np.array([3.45,2.3,4.5,6.9])
-#     # When the input coords are in the 'ljki' order, the affines get
-#     # rearranged.  Note that the 'start' below, must be 0 for
-#     # non-spatial dimensions, because we have no way to store them in
-#     # most cases.  For example, a 'start' of [1,5,3,1] would be lost on
-#     # reload
-#     cmap = api.AffineTransform.from_start_step('lkji', 'tzyx', [2,5,3,1], step)
-#     data = np.random.standard_normal(shape)
-#     img = api.Image(data, cmap)
-#     save_image(img, TMP_FNAME)
-#     img2 = load_image(TMP_FNAME)
-#     P = np.array([[0,0,0,1,0],
-#                   [0,0,1,0,0],
-#                   [0,1,0,0,0],
-#                   [1,0,0,0,0],
-#                   [0,0,0,0,1]])
-#     res = np.dot(P, np.dot(img.affine, P.T))
-
-#     # the step part of the affine should be set correctly
-#     yield assert_array_almost_equal, res[:4,:4], img2.affine[:4,:4]
-
-#     # start in the spatial dimensions should be set correctly
-#     yield assert_array_almost_equal, res[:3,-1], img2.affine[:3,-1]
-
-#     # start in the time dimension should not be 2 as in img, but 0
-#     # because NIFTI dosen't have a time start
-
-#     yield assert_false, (res[3,-1] == img2.affine[3,-1])
-#     yield assert_true, (res[3,-1] == 2)
-#     yield assert_true, (img2.affine[3,-1] == 0)
-
-#     # shapes should be reversed because img has coordinates reversed
+def test_save3():
+    # A test to ensure that when a file is saved, the affine
+    # and the data agree. In this case, things don't agree:
+    # i) the pixdim is off
+    # ii) makes the affine off
+    step = np.array([3.45,2.3,4.5,6.9])
+    shape = (13,5,7,3)
+    mni_xyz = mni_csm(3).coord_names
+    cmap = AT(CS('jkli'),
+              CS(('t',) + mni_xyz[::-1]),
+              from_matvec(np.diag([0,3,5,1]), step))
+    data = np.random.standard_normal(shape)
+    img = api.Image(data, cmap)
+    # with InTemporaryDirectory():
+    with InTemporaryDirectory():
+        save_image(img, TMP_FNAME)
+        tmp = load_image(TMP_FNAME)
+        # Detach image from file so we can delete it
+        img2 = api.Image(tmp.get_data(), tmp.coordmap, tmp.metadata)
+        del tmp
+    assert_equal(tuple([img.shape[l] for l in [3,2,1,0]]), img2.shape)
+    a = np.transpose(np.asarray(img), [3,2,1,0])
+    assert_false(np.allclose(img.affine, img2.affine))
+    assert_true(np.allclose(a, img2.get_data()))
 
 
-#     yield assert_equal, img.shape[::-1], img2.shape
-
-#     # data should be transposed because coordinates are reversed
-
-#     yield (assert_array_almost_equal, 
-#            np.transpose(np.asarray(img2),[3,2,1,0]),
-#            np.asarray(img))
-
-#     # coordinate names should be reversed as well
-
-#     yield assert_equal, img2.coordmap.function_domain.coord_names, \
-#         img.coordmap.function_domain.coord_names[::-1]
-#     yield assert_equal, img2.coordmap.function_domain.coord_names, \
-#         ['i', 'j', 'k', 'l']
+def test_save4():
+    # Same as test_save3 except we have reordered the 'ijk' input axes.
+    shape = (13,5,7,3)
+    step = np.array([3.45,2.3,4.5,6.9])
+    # When the input coords are in the 'ljki' order, the affines get
+    # rearranged.  Note that the 'start' below, must be 0 for
+    # non-spatial dimensions, because we have no way to store them in
+    # most cases.  For example, a 'start' of [1,5,3,1] would be lost on
+    # reload
+    mni_xyz = mni_csm(3).coord_names
+    cmap = AT(CS('tkji'),
+              CS((('t',) + mni_xyz[::-1])),
+              from_matvec(np.diag([2., 3, 5, 1]), step))
+    data = np.random.standard_normal(shape)
+    img = api.Image(data, cmap)
+    with InTemporaryDirectory():
+        save_image(img, TMP_FNAME)
+        tmp = load_image(TMP_FNAME)
+        # Detach image from file so we can delete it
+        img2 = api.Image(tmp.get_data(), tmp.coordmap, tmp.metadata)
+        del tmp
+    P = np.array([[0,0,0,1,0],
+                  [0,0,1,0,0],
+                  [0,1,0,0,0],
+                  [1,0,0,0,0],
+                  [0,0,0,0,1]])
+    res = np.dot(P, np.dot(img.affine, P.T))
+    # the step part of the affine should be set correctly
+    assert_array_almost_equal(res[:4,:4], img2.affine[:4,:4])
+    # start in the spatial dimensions should be set correctly
+    assert_array_almost_equal(res[:3,-1], img2.affine[:3,-1])
+    # start in the time dimension should be 3.45 as in img, because NIFTI stores
+    # the time offset in hdr[``toffset``]
+    assert_not_equal(res[3,-1], img2.affine[3,-1])
+    assert_equal(res[3,-1], 3.45)
+    # shapes should be reversed because img has coordinates reversed
+    assert_equal(img.shape[::-1], img2.shape)
+    # data should be transposed because coordinates are reversed
+    assert_array_almost_equal(
+           np.transpose(np.asarray(img2),[3,2,1,0]),
+           np.asarray(img))
+    # coordinate names should be reversed as well
+    assert_equal(img2.coordmap.function_domain.coord_names,
+                 img.coordmap.function_domain.coord_names[::-1])
+    assert_equal(img2.coordmap.function_domain.coord_names,
+                 ('i', 'j', 'k', 't'))
