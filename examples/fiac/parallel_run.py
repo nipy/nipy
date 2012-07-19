ac0f0b20129f3cea9def16b212cfedf6458463da
Matthew Brett <matthew.brett@gmail.com>
2012-07-19 16:37:51 -0700

    BF: fix parallel example to run permutation test
    returns were preventing run of both block and event fixed effects
    analysese for each subject.
    Some typos in the variable names.
    np.concatenate does not allow generators (require len(x) to work)

--- examples/fiac/parallel_run.py
+++ examples/fiac/parallel_run.py
@@ -1,6 +1,8 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 """Script to run the main analyses in parallel, using the IPython machinery.
+
+See ``fiac_example.py``.
 """
 #-----------------------------------------------------------------------------
 # Imports
@@ -11,9 +13,6 @@ import os
 import numpy as np
 
 from IPython import parallel
-
-# Local imports
-from fiac_example import GROUP_MASK
 
 #-----------------------------------------------------------------------------
 # Utility functions
@@ -62,14 +61,14 @@ def fitruns():
     rc = setup_client()
     view = rc.load_balanced_view()
     i_s, j_s = zip(*getruns())
-    
+
     def _fit(subj, run):
         import fiac_example
         try:
             return fiac_example.run_model(subj, run)
         except IOError:
             pass
-    
+
     return view.map(_fit, i_s, j_s)
 
 
@@ -78,19 +77,19 @@ def fitfixed():
     rc = setup_client()
     view = rc.load_balanced_view()
     subjects = range(16)
-    
+
     def _fit(subject):
         import fiac_example
         try:
-            return fiac_example.fixed_effects(subject, "block")
+            fiac_example.fixed_effects(subject, "block")
         except IOError:
             pass
         try:
-            return fiac_example.fixed_effects(subject, "event")
+            fiac_example.fixed_effects(subject, "event")
         except IOError:
             pass
-    
-    return view.map(_fit, subject)
+
+    return view.map(_fit, subjects)
 
 
 def fitgroup():
@@ -98,33 +97,33 @@ def fitgroup():
     rc = setup_client()
     view = rc.load_balanced_view()
     d_s, c_s = zip(*getvals())
-    
+
     def _fit(d, c):
         import fiac_example
         return fiac_example.group_analysis(d, c)
-    
+
     return view.map(_fit, d_s, c_s)
 
 
 def run_permute_test(design, contrast, nsample=1000):
     rc = setup_client()
     dview = rc[:]
-    nnod = len(view)
-    ns_nod = nsample/nnod
-    
+    nnod = len(dview)
+    # Samples per node.  Round up
+    ns_nod = np.ceil(nsample / float(nnod))
+
     def _run_test(n, des, con):
         import fiac_example
         from fiac_example import GROUP_MASK
         min_vals, max_vals = fiac_example.permutation_test(des, con,
                                        GROUP_MASK, n)
         return min_vals, max_vals
-        
-    ar = view.apply_async(_run_test, nnod, design, contrast)
-    min_vals = numpy.concatenate(mins for mins,maxes in ar)
-    max_vals = numpy.concatenate(maxes for mins,maxes in ar)
-    return min_vals, max_vals
 
-    
+    ar = dview.apply_async(_run_test, ns_nod, design, contrast)
+    min_vals, max_vals = zip(*[r for r in ar])
+    return np.concatenate(min_vals), np.concatenate(max_vals)
+
+
 #-----------------------------------------------------------------------------
 # Script entry point
 #-----------------------------------------------------------------------------
