BF: Improved testing and fixed a bug on mean computation

examples/labs/need_data/localizer_glm_ar.py
@@ -125,7 +125,7 @@ contrasts['effects_of_interest'] = np.eye(25)[:20:2]
 print 'Fitting a GLM (this takes time)...'
 fmri_glm = FMRILinearModel(data_path, design_matrix.matrix, 
                            mask='compute')
-fmri_glm.fit()
+fmri_glm.fit(do_scaling=True, model='ar1')
 
 #########################################
 # Estimate the contrasts

nipy/modalities/fmri/glm.py
@@ -475,16 +475,17 @@ class FMRILinearModel(object):
                in case of an ar1, discrteization of the ar1 parameter
         """
         from nibabel import Nifti1Image
-        # mask the data
+        # get the mask as an array
         mask = self.mask.get_data().astype(np.bool)
         
+        self.glms, self.means = [], []
         for fmri, design_matrix in zip(self.fmri_data, self.design_matrices):
             if do_scaling:
                 # scale the data
                 data, mean = data_scaling(fmri.get_data()[mask].T)
             else:
                 data, mean = (fmri.get_data()[mask].T, 
-                              fmri.get_data()[mask].T.mean[0])
+                              fmri.get_data()[mask].T.mean(0))
             mean_data = mask.astype(np.int16)
             mean_data[mask] = mean
             self.means.append(Nifti1Image(mean_data, self.affine))