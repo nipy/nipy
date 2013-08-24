35936407569b50a31771163ec38188af40cd9f72
Matthew Brett <matthew.brett@gmail.com>
2013-08-24 14:17:47 -0700

    NF+TST: add slicetiming utility functions
    Functions for returning slice times given number of slices and TR.
    
    Keep dictionary of allowable names for lookup of function name and
    aliases as string.

---
+++ nipy/algorithms/slicetiming/__init__.py
@@ -0,0 +1,9 @@
+# Init for slicetiming subpackage
+""" Slicetiming subpackage
+
+The standard nipy method of slice timing is implemented in
+:mod:`nipy.algorithms.registration.groupwise_registration`.
+
+This subpackage is a placeholder for other slice timing methods, and for utility
+functions for slice timing
+"""
---
+++ nipy/algorithms/slicetiming/setup.py
@@ -0,0 +1,17 @@
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+import os
+
+
+def configuration(parent_package='', top_path=None):
+
+    from numpy.distutils.misc_util import Configuration
+
+    config = Configuration('slicetiming', parent_package, top_path)
+    config.add_subpackage('tests')
+    config.add_include_dirs(config.name.replace('.', os.sep))
+    return config
+
+
+if __name__ == '__main__':
+    print('This is the wrong setup.py file to run')
