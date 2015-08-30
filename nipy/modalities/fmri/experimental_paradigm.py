ca0d09f0e72a05e4eb7e3683b3a9176bf3876553
bthirion <bertrand.thirion@inria.fr>
2015-08-30 23:03:24 +0200

    Rebased and included Mattews suggestions

--- nipy/modalities/fmri/experimental_paradigm.py
+++ nipy/modalities/fmri/experimental_paradigm.py
@@ -49,19 +49,7 @@ class Paradigm(object):
         self.amplitude = amplitude
         if con_id is not None:
             self.n_events = len(con_id)
-            try:
-                # this is only for backward compatibility:
-                #if con_id were integers, they become a string
-                warnings.warn(
-                    'The behavior will change,' +
-                    'prepending by a c will disappear in the future')
-                self.con_id = np.array(['c' + str(int(float(c)))
-                                         for c in con_id])
-            except:
-                self.con_id = np.ravel(np.array(con_id)).astype('str')
-            # the following line should replace the try.. except statement
-            # self.con_id = np.ravel(np.array(con_id)).astype('str')
-            
+            self.con_id = np.ravel(np.array(con_id)).astype('str')
         if onset is not None:
             if len(onset) != self.n_events:
                 raise ValueError(
--- nipy/modalities/fmri/tests/test_paradigm.py
+++ nipy/modalities/fmri/tests/test_paradigm.py
@@ -11,6 +11,7 @@ import numpy as np
 
 from ..experimental_paradigm import (EventRelatedParadigm, BlockParadigm,
                                      load_paradigm_from_csv_file)
+from nose.tools import assert_true
 
 
 def basic_paradigm():
@@ -80,10 +81,8 @@ def test_read_paradigm():
 def test_paradigm_with_int_condition_ids():
     paradigm1 = basic_paradigm()
     conditions = [0, 0, 0, 1, 1, 1, 2, 2, 2]
-    paradigm2 =  EventRelatedParadigm(conditions, paradigm1.onset)
-    #assert (paradigm2.con_id == np.array(conditions).astype('str')).all()
-    ccon = np.array(['c0', 'c0', 'c0', 'c1', 'c1', 'c1', 'c2', 'c2', 'c2'])
-    assert (paradigm2.con_id == ccon).all()
+    paradigm2 = EventRelatedParadigm(conditions, paradigm1.onset)
+    assert_true((paradigm2.con_id == np.array(conditions).astype('str')).all())
 
 
 if __name__ == "__main__":
