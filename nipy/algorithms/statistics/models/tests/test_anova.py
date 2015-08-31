8d23362d55ec10776d136f397279924bdfdfae2f
Matthew Brett <matthew.brett@gmail.com>
2015-08-31 17:13:35 +0100

    RF: use io to import StringIO
    Use io module for StringIO import.

--- nipy/algorithms/statistics/models/tests/test_anova.py
+++ nipy/algorithms/statistics/models/tests/test_anova.py
@@ -1,6 +1,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-from StringIO import StringIO
+from io import StringIO
 
 import numpy as np
 
