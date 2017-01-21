79cb7f651e85d7009fc067ad580c0e2f95bb4988
Matthew Brett <matthew.brett@gmail.com>
2017-01-21 15:22:08 -0800

    BF: force integers for heirarchichal clustering
    Numpy 1.12 more strict about enforcing integers for indices.

--- nipy/algorithms/clustering/tests/test_hierarchical_clustering.py
+++ nipy/algorithms/clustering/tests/test_hierarchical_clustering.py
@@ -9,6 +9,8 @@ Author : Bertrand Thirion, 2008-2009
 """
 from __future__ import absolute_import
 
+import math
+
 import numpy as np
 from numpy.random import randn
 from ..hierarchical_clustering import (average_link_graph,
@@ -157,7 +159,7 @@ def ward_test_more(n=100, k=5, verbose=0):
     # Check that two implementations give the same result
     np.random.seed(0)
     X = randn(n,2)
-    X[:np.ceil(n/3)] += 5
+    X[:math.ceil(n/3)] += 5
     G = knn(X, 5)
     u,c = ward_segment(G, X, stop=-1, qmax=1, verbose=verbose)
     u1,c = ward_segment(G, X, stop=-1, qmax=k, verbose=verbose)
