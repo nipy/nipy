271f928d9477de7a3676b6909e8449f1683aa928
Matthew Brett <matthew.brett@gmail.com>
2017-03-07 15:26:30 -0800

    MAINT: update gitwash
    Update gitwash from upstream.

--- doc/devel/guidelines/gitwash/forking_hell.rst
+++ doc/devel/guidelines/gitwash/forking_hell.rst
@@ -1,33 +1,34 @@
+.. highlight:: bash
+
 .. _forking:
 
-==========================================
+======================================================
 Making your own copy (fork) of nipy
-==========================================
+======================================================
 
 You need to do this only once.  The instructions here are very similar
-to the instructions at http://help.github.com/forking/ - please see that
-page for more detail.  We're repeating some of it here just to give the
-specifics for the nipy_ project, and to suggest some default names.
+to the instructions at https://help.github.com/forking/ |emdash| please see
+that page for more detail.  We're repeating some of it here just to give the
+specifics for the `nipy`_ project, and to suggest some default names.
 
-Set up and configure a github_ account
-======================================
+Set up and configure a github account
+=====================================
 
-If you don't have a github_ account, go to the github_ page, and make one.
+If you don't have a github account, go to the github page, and make one.
 
-You then need to configure your account to allow write access - see the
-``Generating SSH keys`` help on `github help`_.
+You then need to configure your account to allow write access |emdash| see
+the ``Generating SSH keys`` help on `github help`_.
 
-Create your own forked copy of nipy_
-=========================================
+Create your own forked copy of `nipy`_
+======================================================
 
-#. Log into your github_ account.
-#. Go to the nipy_ github home at `nipy github`_.
+#. Log into your github account.
+#. Go to the `nipy`_ github home at `nipy github`_.
 #. Click on the *fork* button:
 
    .. image:: forking_button.png
 
-   Now, after a short pause and some 'Hardcore forking action', you
-   should find yourself at the home page for your own forked copy of nipy_.
+   Now, after a short pause, you should find yourself at the home page for
+   your own forked copy of `nipy`_.
 
-.. include:: git_links.inc
-
+.. include:: links.inc
--- tools/gitwash_dumper.py
+++ tools/gitwash_dumper.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 ''' Checkout gitwash repo into directory and do search replace on name '''
 
+from __future__ import (absolute_import, division, print_function)
+
 import os
 from os.path import join as pjoin
 import shutil
@@ -10,7 +12,7 @@ import glob
 import fnmatch
 import tempfile
 from subprocess import call
-
+from optparse import OptionParser
 
 verbose = False
 
@@ -54,37 +56,35 @@ def filename_search_replace(sr_pairs, filename, backup=False):
     ''' Search and replace for expressions in files
 
     '''
-    in_txt = open(filename, 'rt').read(-1)
+    with open(filename, 'rt') as in_fh:
+        in_txt = in_fh.read(-1)
     out_txt = in_txt[:]
     for in_exp, out_exp in sr_pairs:
         in_exp = re.compile(in_exp)
         out_txt = in_exp.sub(out_exp, out_txt)
     if in_txt == out_txt:
         return False
-    open(filename, 'wt').write(out_txt)
+    with open(filename, 'wt') as out_fh:
+        out_fh.write(out_txt)
     if backup:
-        open(filename + '.bak', 'wt').write(in_txt)
+        with open(filename + '.bak', 'wt') as bak_fh:
+            bak_fh.write(in_txt)
     return True
 
-        
+
 def copy_replace(replace_pairs,
+                 repo_path,
                  out_path,
-                 repo_url,
-                 repo_branch = 'master',
                  cp_globs=('*',),
                  rep_globs=('*',),
                  renames = ()):
-    repo_path = clone_repo(repo_url, repo_branch)
-    try:
-        out_fnames = cp_files(repo_path, cp_globs, out_path)
-    finally:
-        shutil.rmtree(repo_path)
+    out_fnames = cp_files(repo_path, cp_globs, out_path)
     renames = [(re.compile(in_exp), out_exp) for in_exp, out_exp in renames]
     fnames = []
     for rep_glob in rep_globs:
         fnames += fnmatch.filter(out_fnames, rep_glob)
     if verbose:
-        print '\n'.join(fnames)
+        print('\n'.join(fnames))
     for fname in fnames:
         filename_search_replace(replace_pairs, fname, False)
         for in_exp, out_exp in renames:
@@ -93,7 +93,70 @@ def copy_replace(replace_pairs,
                 os.rename(fname, new_fname)
                 break
 
-            
+
+def make_link_targets(proj_name,
+                      user_name,
+                      repo_name,
+                      known_link_fname,
+                      out_link_fname,
+                      url=None,
+                      ml_url=None):
+    """ Check and make link targets
+
+    If url is None or ml_url is None, check if there are links present for these
+    in `known_link_fname`.  If not, raise error.  The check is:
+
+    Look for a target `proj_name`.
+    Look for a target `proj_name` + ' mailing list'
+
+    Also, look for a target `proj_name` + 'github'.  If this exists, don't write
+    this target into the new file below.
+
+    If we are writing any of the url, ml_url, or github address, then write new
+    file with these links, of form:
+
+    .. _`proj_name`
+    .. _`proj_name`: url
+    .. _`proj_name` mailing list: url
+    """
+    with open(known_link_fname, 'rt') as link_fh:
+        link_contents = link_fh.readlines()
+    have_url = not url is None
+    have_ml_url = not ml_url is None
+    have_gh_url = None
+    for line in link_contents:
+        if not have_url:
+            match = re.match(r'..\s+_`%s`:\s+' % proj_name, line)
+            if match:
+                have_url = True
+        if not have_ml_url:
+            match = re.match(r'..\s+_`%s mailing list`:\s+' % proj_name, line)
+            if match:
+                have_ml_url = True
+        if not have_gh_url:
+            match = re.match(r'..\s+_`%s github`:\s+' % proj_name, line)
+            if match:
+                have_gh_url = True
+    if not have_url or not have_ml_url:
+        raise RuntimeError('Need command line or known project '
+                           'and / or mailing list URLs')
+    lines = []
+    if not url is None:
+        lines.append('.. _`%s`: %s\n' % (proj_name, url))
+    if not have_gh_url:
+        gh_url = 'https://github.com/%s/%s\n' % (user_name, repo_name)
+        lines.append('.. _`%s github`: %s\n' % (proj_name, gh_url))
+    if not ml_url is None:
+        lines.append('.. _`%s mailing list`: %s\n' % (proj_name, ml_url))
+    if len(lines) == 0:
+        # Nothing to do
+        return
+    # A neat little header line
+    lines = ['.. %s\n' % proj_name] + lines
+    with open(out_link_fname, 'wt') as out_links:
+        out_links.writelines(lines)
+
+
 USAGE = ''' <output_directory> <project_name>
 
 If not set with options, the repository name is the same as the <project
@@ -107,8 +170,7 @@ GITWASH_CENTRAL = 'git://github.com/matthew-brett/gitwash.git'
 GITWASH_BRANCH = 'master'
 
 
-if __name__ == '__main__':
-    from optparse import OptionParser
+def main():
     parser = OptionParser()
     parser.set_usage(parser.get_usage().strip() + USAGE)
     parser.add_option("--repo-name", dest="repo_name",
@@ -131,6 +193,14 @@ if __name__ == '__main__':
                       help="suffix of ReST source files - default '.rst'",
                       default='.rst',
                       metavar="SOURCE_SUFFIX")
+    parser.add_option("--project-url", dest="project_url",
+                      help="URL for project web pages",
+                      default=None,
+                      metavar="PROJECT_URL")
+    parser.add_option("--project-ml-url", dest="project_ml_url",
+                      help="URL for project mailing list",
+                      default=None,
+                      metavar="PROJECT_ML_URL")
     (options, args) = parser.parse_args()
     if len(args) < 2:
         parser.print_help()
@@ -140,12 +210,26 @@ if __name__ == '__main__':
         options.repo_name = project_name
     if options.main_gh_user is None:
         options.main_gh_user = options.repo_name
-    copy_replace((('PROJECTNAME', project_name),
-                  ('REPONAME', options.repo_name),
-                  ('MAIN_GH_USER', options.main_gh_user)),
-                 out_path,
-                 options.gitwash_url,
-                 options.gitwash_branch,
-                 cp_globs=(pjoin('gitwash', '*'),),
-                 rep_globs=('*.rst',),
-                 renames=(('\.rst$', options.source_suffix),))
+    repo_path = clone_repo(options.gitwash_url, options.gitwash_branch)
+    try:
+        copy_replace((('PROJECTNAME', project_name),
+                      ('REPONAME', options.repo_name),
+                      ('MAIN_GH_USER', options.main_gh_user)),
+                     repo_path,
+                     out_path,
+                     cp_globs=(pjoin('gitwash', '*'),),
+                     rep_globs=('*.rst',),
+                     renames=(('\.rst$', options.source_suffix),))
+        make_link_targets(project_name,
+                          options.main_gh_user,
+                          options.repo_name,
+                          pjoin(out_path, 'gitwash', 'known_projects.inc'),
+                          pjoin(out_path, 'gitwash', 'this_project.inc'),
+                          options.project_url,
+                          options.project_ml_url)
+    finally:
+        shutil.rmtree(repo_path)
+
+
+if __name__ == '__main__':
+    main()
