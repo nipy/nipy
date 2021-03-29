d16d268938dcd5c15748ca051532c21f57cf8a22
Matthew Brett <matthew.brett@gmail.com>
2021-03-29 17:56:48 +0100

    Oops - set version_extra

--- nipy/info.py
+++ nipy/info.py
@@ -10,7 +10,7 @@ docs.  In setup.py in particular, we exec this file, so it cannot import nipy
 _version_major = 0
 _version_minor = 5
 _version_micro = 1
-# _version_extra = '.dev1' # For development
+_version_extra = '.dev1' # For development
 # _version_extra = 'rc1' # For release candidate
 # _version_extra = '' # For release
 
