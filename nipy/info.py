91c22042eb02cd6b14bdef62a876ff6d14c60e2a
Matthew Brett <matthew.brett@gmail.com>
2021-03-29 17:54:16 +0100

    Maintenance, 0.5.1 dev1

--- nipy/info.py
+++ nipy/info.py
@@ -9,10 +9,10 @@ docs.  In setup.py in particular, we exec this file, so it cannot import nipy
 # See: https://www.python.org/dev/peps/pep-0440
 _version_major = 0
 _version_minor = 5
-_version_micro = 0
-# _version_extra = '.dev2' # For development
+_version_micro = 1
+# _version_extra = '.dev1' # For development
 # _version_extra = 'rc1' # For release candidate
-_version_extra = '' # For release
+# _version_extra = '' # For release
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 __version__ = "%s.%s.%s%s" % (_version_major,
