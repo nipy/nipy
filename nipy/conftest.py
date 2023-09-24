0e10c2b8069991861b3b8e44bbec2a7b5b3556aa
Matthew Brett <matthew.brett@gmail.com>
2023-09-23 19:55:40 -0700

    Try adding matplotlib import to testing env

--- nipy/conftest.py
+++ nipy/conftest.py
@@ -206,3 +206,15 @@ doctest.OutputChecker = NipyOutputChecker
 def add_np(doctest_namespace):
     numpy.set_printoptions(legacy="1.13")
     doctest_namespace["np"] = numpy
+
+
+@pytest.fixture
+def mpl_imports():
+    """ Force matplotlib to use agg backend for tests
+    """
+    try:
+        import matplotlib as mpl
+    except ImportError:
+        pass
+    else:
+        mpl.use('agg')
