4121bebe61e0f1bbe569e0b48cbbe7bde63fce44
Matthew Brett <matthew.brett@gmail.com>
2023-10-03 20:46:24 +0100

    Add benchmark testing

--- nipy/labs/bindings/benchmarks/bench_numpy.py
+++ nipy/labs/bindings/benchmarks/bench_numpy.py
@@ -15,24 +15,24 @@ def time_ratio(t0,t1):
         return t0/t1
 
 def time_copy_vector(x):
-    t0 = time.clock()
+    t0 = time.process_time()
     y0 = copy_vector(x, 0)
-    dt0 = time.clock()-t0
-    t1 = time.clock()
+    dt0 = time.process_time()-t0
+    t1 = time.process_time()
     y1 = copy_vector(x, 1)
-    dt1 = time.clock()-t1
+    dt1 = time.process_time()-t1
     ratio = time_ratio(dt0,dt1)
     print(f'  using fff_array: {dt0:f} sec')
     print(f'  using numpy C API: {dt1:f} sec')
     print(f'  ratio: {ratio:f}')
 
 def bench_copy_vector_contiguous():
-    x = (1000*np.random.rand(1e6)).astype('int32')
+    x = (1000*np.random.rand(1_000_000)).astype('int32')
     print('Contiguous buffer copy (int32-->double)')
     time_copy_vector(x)
 
 def bench_copy_vector_strided():
-    x0 = (1000*np.random.rand(2e6)).astype('int32')
+    x0 = (1000*np.random.rand(2_000_000)).astype('int32')
     x = x0[::2]
     print('Non-contiguous buffer copy (int32-->double)')
     time_copy_vector(x)
