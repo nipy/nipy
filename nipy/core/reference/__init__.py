f7115d92f3105f8557da361842a82881a2166280
Matthew Brett <matthew.brett@gmail.com>
2023-10-04 10:56:36 +0100

    Raw docstring to allow \phi in LaTeX

--- nipy/core/reference/__init__.py
+++ nipy/core/reference/__init__.py
@@ -1,6 +1,6 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
-"""
+r"""
 Frame of reference/coordinates package.
 
 Mathematical model
