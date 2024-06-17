8c4a8a2829995dfd476709e54eee0b71d2f350ee
Matthew Brett <matthew.brett@gmail.com>
2024-06-17 16:16:24 +0100

    Numpy int_t gone, switch to intp_t

--- nipy/algorithms/graph/_graph.pyx
+++ nipy/algorithms/graph/_graph.pyx
@@ -1,22 +1,21 @@
-import numpy as np
-cimport numpy as np
+cimport numpy as cnp
 cimport cython
-ctypedef np.float64_t DOUBLE
-ctypedef np.int_t INT
+ctypedef cnp.float64_t DOUBLE
+ctypedef cnp.int_t INT
 
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
 
-def dilation(np.ndarray[DOUBLE, ndim=2] field,\
-             np.ndarray[INT, ndim=1] idx,\
-             np.ndarray[INT, ndim=1] neighb):
+def dilation(cnp.ndarray[DOUBLE, ndim=2] field,\
+             cnp.ndarray[INT, ndim=1] idx,\
+             cnp.ndarray[INT, ndim=1] neighb):
     cdef int size_max = field.shape[0]
     cdef int dim = field.shape[1]
     cdef int i, j, d
     cdef DOUBLE fmax
-    cdef np.ndarray[DOUBLE, ndim=1] res = 0 * field[:, 0]
+    cdef cnp.ndarray[DOUBLE, ndim=1] res = 0 * field[:, 0]
     for d in range(dim):
         for i in range(size_max):
             fmax = field[i, d]
